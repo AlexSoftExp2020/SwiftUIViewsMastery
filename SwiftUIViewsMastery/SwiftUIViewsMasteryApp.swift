09dd23a6659f30c7aacea31957ef8cf1a736533f
Alex <oleksii.pidhornyi@gmail.com>
2025-03-16 01:07:50 +0100

    Initial Commit

---
+++ SwiftUIViewsMastery/SwiftUIViewsMasteryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIViewsMasteryApp.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 16.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIViewsMasteryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

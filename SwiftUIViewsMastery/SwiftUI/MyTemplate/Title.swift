14637871e08310a658b6abb7495f46f2e65edccb
Alex <oleksii.pidhornyi@gmail.com>
2025-03-17 22:13:45 +0100

    add title

---
+++ SwiftUIViewsMastery/SwiftUI/MyTemplate/Title.swift
@@ -0,0 +1,19 @@
+//
+//  Title.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 17.03.2025.
+//
+
+import SwiftUI
+
+struct Title: View {
+    var body: some View {
+        Text("Title")
+            .font(.largeTitle)
+    }
+}
+
+#Preview {
+    Title()
+}

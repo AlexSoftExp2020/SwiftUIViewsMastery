aeaa60a99d026f45c18af4a296000c8754f63a65
Alex <oleksii.pidhornyi@gmail.com>
2025-03-19 21:09:49 +0100

    extend symbols intro

--- SwiftUIViewsMastery/SwiftUI/SwiftUIBasics/SymbolsIntro.swift
+++ SwiftUIViewsMastery/SwiftUI/SwiftUIBasics/SymbolsIntro.swift
@@ -10,8 +10,25 @@ import SwiftUI
 struct SymbolsIntro: View {
     var body: some View {
         VStack(spacing: 20) {
-            <#code#>
+            Text("Images")
+                .font(.largeTitle)
+            
+            Text("Using SF Symbols")
+                .foregroundStyle(.gray)
+            
+            Text("You will see I use icons or symbols to add clarify to what I'm demonstrating. These come from Apple's new symbol font library which you can browse using an app called 'SF Symbols'.")
+                .frame(maxWidth: .infinity)
+                .padding()
+                .background(.blue)
+                .foregroundStyle(.white)
+            
+            // Use "systemName" when you want to use "SF Symbols"
+            Image(systemName: "hand.thumbsup.fill")
+                .font(.largeTitle) // Make the symbol larger
+            
+            Image("SF Symols") // Regular image from Assets.xsassets
         }
+        .font(.title)
     }
 }
 

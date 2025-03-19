4c24b6ecda2bc4e1353b476971b8f139777cf972
Alex <oleksii.pidhornyi@gmail.com>
2025-03-20 00:20:01 +0100

    extend shapes

--- SwiftUIViewsMastery/SwiftUI/SwiftUIBasics/Shapes.swift
+++ SwiftUIViewsMastery/SwiftUI/SwiftUIBasics/Shapes.swift
@@ -9,7 +9,29 @@ import SwiftUI
 
 struct Shapes: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 15) {
+            Text("Shapes")
+                .font(.largeTitle)
+            
+            Text("Short Introduction")
+                .foregroundStyle(Color.gray)
+            
+            Text("I'll make shapes, give them color and put them behind other views just for decoration.")
+                .frame(maxWidth: .infinity)
+                .padding()
+                .background(Color.blue)
+                .foregroundStyle(Color.white)
+            
+            Text("This text has a rounded rectangle behind it")
+                .foregroundStyle(Color.white)
+                .padding()
+                .background {
+                    RoundedRectangle(cornerRadius: 20) // Create the shape
+                        .fill(Color.blue) // Make the shape blue
+                }
+                .padding()
+        }
+        .font(.title)
     }
 }
 

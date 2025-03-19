51537ac840ef28a4adee455cd7d8fb6621fa4e0f
Alex <oleksii.pidhornyi@gmail.com>
2025-03-20 00:12:13 +0100

    extend layers

--- SwiftUIViewsMastery/SwiftUI/SwiftUIBasics/Layers.swift
+++ SwiftUIViewsMastery/SwiftUI/SwiftUIBasics/Layers.swift
@@ -9,7 +9,29 @@ import SwiftUI
 
 struct Layers: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 40) {
+            Text("Layers")
+                .font(.largeTitle)
+            
+            Text("The Basics")
+                .foregroundStyle(.gray)
+            
+            Text("With SwiftUI views, you can add layers on top (.overlay) and behind (.background) the view.")
+                .frame(maxWidth: .infinity)
+                .padding()
+                .background(Color.blue)
+                .foregroundStyle(Color.white)
+            
+            Image("yosemite") // Show an image from Assets.xsassets
+                .opacity(0.7) // Make image onlu 70% solid
+                .background(Color.red.opacity(0.3)) // Layer behind image
+                .background(Color.yellow.opacity(0.3)) // Layer behind red
+                .background(Color.blue.opacity(0.3)) // Layer behind yellow
+                .overlay(Text("Yosemite")) // Layer on top of image
+            
+            Image("Layers")
+        }
+        .font(.title)
     }
 }
 

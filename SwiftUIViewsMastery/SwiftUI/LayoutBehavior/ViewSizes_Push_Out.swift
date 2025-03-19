0e231eaf6c0c618341b98d02e499e2b4fe83acd5
Alex <oleksii.pidhornyi@gmail.com>
2025-03-20 00:39:42 +0100

    add view sizes push out

---
+++ SwiftUIViewsMastery/SwiftUI/LayoutBehavior/ViewSizes_Push_Out.swift
@@ -0,0 +1,18 @@
+//
+//  ViewSizes_Push_Out.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 20.03.2025.
+//
+
+import SwiftUI
+
+struct ViewSizes_Push_Out: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ViewSizes_Push_Out()
+}

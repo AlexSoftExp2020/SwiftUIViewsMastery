f4fccceacb3c847fa90b5d832b58401934519d0e
Alex <oleksii.pidhornyi@gmail.com>
2025-03-19 20:58:21 +0100

    add symbols intro

---
+++ SwiftUIViewsMastery/SwiftUI/SwiftUIBasics/SymbolsIntro.swift
@@ -0,0 +1,20 @@
+//
+//  SymbolsIntro.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 19.03.2025.
+//
+
+import SwiftUI
+
+struct SymbolsIntro: View {
+    var body: some View {
+        VStack(spacing: 20) {
+            <#code#>
+        }
+    }
+}
+
+#Preview {
+    SymbolsIntro()
+}

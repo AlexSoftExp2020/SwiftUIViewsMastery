0934bc30ddb360626129c71defb6e54b382b299b
Alex <oleksii.pidhornyi@gmail.com>
2025-03-20 21:54:39 +0100

    add hstack alignment

---
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/HStack/HStack_Alignment.swift
@@ -0,0 +1,18 @@
+//
+//  HStack_Alignment.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 20.03.2025.
+//
+
+import SwiftUI
+
+struct HStack_Alignment: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HStack_Alignment()
+}

8b23c64994f279ba179aa618d9e9466da24ec8b6
Alex <oleksii.pidhornyi@gmail.com>
2025-03-20 11:34:34 +0100

    extend lazy vstack intro

--- SwiftUIViewsMastery/SwiftUI/LayoutViews/LazyVStack/LazyVStack_Intro.swift
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/LazyVStack/LazyVStack_Intro.swift
@@ -9,7 +9,31 @@ import SwiftUI
 
 struct LazyVStack_Intro: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 10.0) {
+            HeaderView("LazyVStack",
+                       subtitle: "Introduction",
+                       desc: "When using the LazyVStack by itself, you won't notice much of a difference from the VStack.")
+            .layoutPriority(1)
+            
+            Text("LazyVStack")
+            LazyVStack(spacing: 10) {
+                Image(systemName: "1.circle")
+                Image(systemName: "2.circle")
+                Image(systemName: "3.circle")
+            }
+            .border(Color.red, width: 2) // Draws a border around the frame of the view
+            
+            Text("VStack")
+            VStack(spacing: 10) {
+                Image(systemName: "1.circle")
+                Image(systemName: "2.circle")
+                Image(systemName: "3.circle")
+            }
+            .border(Color.red, width: 2)
+            
+            Text("Notice the LazyVStack pushes out horizontally. (No spacers being used here.)")
+        }
+        .font(.title)
     }
 }
 

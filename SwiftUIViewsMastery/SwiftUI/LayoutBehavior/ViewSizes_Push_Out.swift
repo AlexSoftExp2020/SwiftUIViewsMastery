66ead4642b44ce4be97a10c674d8d2a7de711b4c
Alex <oleksii.pidhornyi@gmail.com>
2025-03-20 01:49:40 +0100

    extend view sizes push out

--- SwiftUIViewsMastery/SwiftUI/LayoutBehavior/ViewSizes_Push_Out.swift
+++ SwiftUIViewsMastery/SwiftUI/LayoutBehavior/ViewSizes_Push_Out.swift
@@ -9,7 +9,38 @@ import SwiftUI
 
 struct ViewSizes_Push_Out: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            Text("Layout Behavior")
+            Text("Views that Push Out")
+                .font(.title)
+                .foregroundStyle(.gray)
+            Text("Some views will push out to fill up all available space within their parent.")
+                .frame(maxWidth: .infinity)
+                .padding()
+                .font(.title)
+                .background(Color.purple)
+            
+            Color.purple
+            // Add 5 layers on top of the color view
+                .overlay(
+                    Image(systemName: "arrow.up.left")
+                        .padding(), // Add spacing around the symbol
+                    alignment: .topLeading)
+                .overlay(
+                    Image(systemName: "arrow.up.right")
+                        .padding(),
+                    alignment: .topTrailing)
+                .overlay(
+                    Image(systemName: "arrow.down.left")
+                        .padding(),
+                    alignment: .bottomLeading)
+                .overlay(
+                    Image(systemName: "arrow.down.right")
+                        .padding(),
+                    alignment: .bottomTrailing)
+                .overlay(Text("Colors are Push-Out views"))
+                
+        }.font(.largeTitle) // Make all text and symbols larger
     }
 }
 

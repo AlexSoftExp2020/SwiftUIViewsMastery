e5c7a7c2cde07c5fbff6a2085e9341a9aa21ede4
Alex <oleksii.pidhornyi@gmail.com>
2025-03-20 11:16:53 +0100

    extend alignment

--- SwiftUIViewsMastery/SwiftUI/LayoutViews/VStack/Alignment.swift
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/VStack/Alignment.swift
@@ -9,7 +9,38 @@ import SwiftUI
 
 struct Alignment: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            Text("VStack")
+                .font(.largeTitle)
+            Text("Alignment")
+                .font(.title)
+                .foregroundStyle(.gray)
+            Text("By default, views in a VStack are center aligned.")
+            
+            VStack(alignment: .leading, spacing: 40) {
+                Text("Leading Alignment")
+                    .font(.title)
+                Divider() // Creates a thin line (Push-out view)
+                Image(systemName: "arrow.left")
+            }
+            .padding()
+            .foregroundStyle(Color.white)
+            .background(RoundedRectangle(cornerRadius: 10))
+            .foregroundStyle(.blue)
+            .padding()
+            
+            VStack(alignment: .trailing, spacing: 40) {
+                Text("Trailing Alignment")
+                    .font(.title)
+                Divider()
+                Image(systemName: "arrow.right")
+            }
+            .padding()
+            .foregroundStyle(Color.white)
+            .background(RoundedRectangle(cornerRadius: 10)
+            .foregroundStyle(.blue))
+            .padding()
+        }
     }
 }
 

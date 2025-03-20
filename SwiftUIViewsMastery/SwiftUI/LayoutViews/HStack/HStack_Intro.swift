8ef9933e5019180ddea7e830affa204faf087633
Alex <oleksii.pidhornyi@gmail.com>
2025-03-20 16:10:28 +0100

    add hstack intro

---
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/HStack/HStack_Intro.swift
@@ -0,0 +1,18 @@
+//
+//  HStack_Intro.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 20.03.2025.
+//
+
+import SwiftUI
+
+struct HStack_Intro: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HStack_Intro()
+}

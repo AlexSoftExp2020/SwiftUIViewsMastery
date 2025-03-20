2d9bb57eff334c398e2a81bd985a7b7f746f2458
Alex <oleksii.pidhornyi@gmail.com>
2025-03-20 10:52:55 +0100

    extend vstack nesting

--- SwiftUIViewsMastery/SwiftUI/LayoutViews/VStack/VStack_Nesting.swift
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/VStack/VStack_Nesting.swift
@@ -9,7 +9,28 @@ import SwiftUI
 
 struct VStack_Nesting: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            HeaderView("VStack",
+                       subtitle: "Nesting",
+                       desc: "A VStack can be nested within another VStack when laying out views.",
+                       back: .blue,
+                       textColor: .white)
+            VStack {
+                Text("VStack inside another VStack")
+                Divider()
+                Text("This can be helpful. Why?")
+                Divider()
+                Text("More than 10 views creates an error.")
+            }
+            .padding()
+            .foregroundStyle(Color.white)
+            .background(
+                // Use a blue rectangle as the background
+                RoundedRectangle(cornerRadius: 10)
+                    .foregroundStyle(.blue))
+            .padding()
+        }
+        .font(.title)
     }
 }
 

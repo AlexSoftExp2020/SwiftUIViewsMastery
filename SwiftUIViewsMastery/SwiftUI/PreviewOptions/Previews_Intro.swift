5000413afe708cc89836ea8d7705c3d12fbe79d4
Alex <oleksii.pidhornyi@gmail.com>
2025-03-20 01:59:14 +0100

    extend previews intro

--- SwiftUIViewsMastery/SwiftUI/PreviewOptions/Previews_Intro.swift
+++ SwiftUIViewsMastery/SwiftUI/PreviewOptions/Previews_Intro.swift
@@ -9,7 +9,17 @@ import SwiftUI
 
 struct Previews_Intro: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            Text("Previews")
+                .font(.largeTitle)
+            Text("Introduction")
+                .foregroundStyle(.gray)
+            Text("Xcode looks for a struct that conforms to the PreviewProvider protocol ans accesses its previews property to display a view on the Canvas.")
+                .frame(maxWidth: .infinity)
+                .padding()
+                .background(Color.red)
+                .foregroundStyle(.white)
+        }.font(.title)
     }
 }
 

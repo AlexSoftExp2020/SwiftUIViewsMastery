dbd5896d439e150d3cffb072b0f1be5c38e368ea
Alex <oleksii.pidhornyi@gmail.com>
2025-03-20 01:52:50 +0100

    add previews intro

---
+++ SwiftUIViewsMastery/SwiftUI/PreviewOptions/Previews_Intro.swift
@@ -0,0 +1,18 @@
+//
+//  Previews_Intro.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 20.03.2025.
+//
+
+import SwiftUI
+
+struct Previews_Intro: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Previews_Intro()
+}

0a867076ed6e082291856cb5ce60387a9b4a1f1f
Alex <oleksii.pidhornyi@gmail.com>
2025-03-20 15:36:08 +0100

    add team vertical footer view

---
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/LazyVStack/TeamVerticalFooterView.swift
@@ -0,0 +1,18 @@
+//
+//  TeamVerticalFooterView.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 20.03.2025.
+//
+
+import SwiftUI
+
+struct TeamVerticalFooterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TeamVerticalFooterView()
+}

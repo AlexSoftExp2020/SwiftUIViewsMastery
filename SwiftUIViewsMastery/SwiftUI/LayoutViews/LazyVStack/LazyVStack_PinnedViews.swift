9082cbf53d8c462acd773faa561fcb05b686cb6e
Alex <oleksii.pidhornyi@gmail.com>
2025-03-20 16:07:03 +0100

    extend lazy v stack pinned views

--- SwiftUIViewsMastery/SwiftUI/LayoutViews/LazyVStack/LazyVStack_PinnedViews.swift
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/LazyVStack/LazyVStack_PinnedViews.swift
@@ -8,8 +8,34 @@
 import SwiftUI
 
 struct LazyVStack_PinnedViews: View {
+    @State private var teams = Data.getTeams()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            HeaderView("LazyVStack",
+                       subtitle: "Pinned Views",
+                       desc: "LazyVStacks can also have section headers and section footers that can be pinned so they only scroll when the next header/footer comes.")
+            
+            ScrollView {
+                LazyVStack(spacing: 5, pinnedViews: [.sectionHeaders, .sectionFooters]) {
+                    ForEach(teams) { team in
+                        Section {
+                            ForEach(team.people) { person in
+                                Image("\(person.imageName)")
+                                    .resizable()
+                                    .frame(width: 300, height: 300)
+                            }
+                        } header: {
+                            TeamVerticalHeaderView(team: team)
+                        } footer: {
+                            TeamVerticalFooterView(team: team)
+                        }
+                    }
+                }
+            }
+            Spacer()
+        }
+        .font(.title)
     }
 }
 

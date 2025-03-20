44b189774ed4f9e4aa42bc3b73bc09948cf110a2
Alex <oleksii.pidhornyi@gmail.com>
2025-03-20 02:21:44 +0100

    add vstack intro

---
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/VStack/VStack_Intro.swift
@@ -0,0 +1,18 @@
+//
+//  VStack_Intro.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 20.03.2025.
+//
+
+import SwiftUI
+
+struct VStack_Intro: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VStack_Intro()
+}

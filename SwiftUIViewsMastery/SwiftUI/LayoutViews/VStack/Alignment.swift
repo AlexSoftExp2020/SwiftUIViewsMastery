c1595863b7542528466d49ac21094194cb30646b
Alex <oleksii.pidhornyi@gmail.com>
2025-03-20 11:02:26 +0100

    add alignment

---
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/VStack/Alignment.swift
@@ -0,0 +1,18 @@
+//
+//  Alignment.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 20.03.2025.
+//
+
+import SwiftUI
+
+struct Alignment: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Alignment()
+}

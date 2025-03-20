76f81f6b3a5d746d188fec7139f258e89690f03b
Alex <oleksii.pidhornyi@gmail.com>
2025-03-20 02:39:18 +0100

    extend vstack intro

--- SwiftUIViewsMastery/SwiftUI/LayoutViews/VStack/VStack_Intro.swift
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/VStack/VStack_Intro.swift
@@ -9,7 +9,23 @@ import SwiftUI
 
 struct VStack_Intro: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            HeaderView("VStack",
+                       subtitle: "Introduction",
+                       desc: "A VStack will vertically arrange other views within it.",
+                       back: .blue,
+                       textColor: .white)
+            Text("View 1")
+            Text("View 2")
+            Text("View 3")
+            Text("View 4")
+            Text("View 5")
+            Text("View 6")
+            Text("View 7")
+            Text("View 8")
+            Text("View 9")
+        }
+        .font(.title)
     }
 }
 

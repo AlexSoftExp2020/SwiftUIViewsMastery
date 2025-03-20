f876e78e2817b5597faca49a302d52b3eb99e3b0
Alex <oleksii.pidhornyi@gmail.com>
2025-03-20 11:48:08 +0100

    add lazy vstack with scrolling

---
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/LazyVStack/LazyVStack_WithScrolling.swift
@@ -0,0 +1,18 @@
+//
+//  LazyVStack_WithScrolling.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 20.03.2025.
+//
+
+import SwiftUI
+
+struct LazyVStack_WithScrolling: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LazyVStack_WithScrolling()
+}

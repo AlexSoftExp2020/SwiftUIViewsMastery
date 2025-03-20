8897059c4ecd1ee262414e37e8b6d7ebbfd5c211
Alex <oleksii.pidhornyi@gmail.com>
2025-03-20 11:01:21 +0100

    extend spacing

--- SwiftUIViewsMastery/SwiftUI/LayoutViews/VStack/Spacing.swift
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/VStack/Spacing.swift
@@ -9,7 +9,27 @@ import SwiftUI
 
 struct Spacing: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 80) {
+            Text("VStack")
+                .font(.largeTitle)
+            
+            Text("Spacing")
+                .font(.title)
+                .foregroundStyle(.gray)
+            
+            Text("The VStack initializer allows you to set the spacing between all the views inside the VStack")
+                .frame(maxWidth: .infinity)
+                .padding()
+                .background(Color.blue)
+                .font(.title)
+                .foregroundStyle(.white)
+            
+            Image(systemName: "arrow.up.and.down.circle.fill")
+                .font(.largeTitle)
+            
+            Text("The spacing herebetween all of these views is 80")
+                .font(.title)
+        }
     }
 }
 

ef2d19d582203fe9f139efbf1c731f82b013fca3
Alex <oleksii.pidhornyi@gmail.com>
2025-03-20 12:10:04 +0100

    extend lazy v stack with scrolling

--- SwiftUIViewsMastery/SwiftUI/LayoutViews/LazyVStack/LazyVStack_WithScrolling.swift
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/LazyVStack/LazyVStack_WithScrolling.swift
@@ -8,8 +8,33 @@
 import SwiftUI
 
 struct LazyVStack_WithScrolling: View {
+    @State private var teams = Data.getTeams()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 10) {
+            HeaderView("LazyVStack",
+                       subtitle: "With ScrollView",
+                       desc: "The LazyVStack is best used with many views that scroll off the scree. \"Lazy\" means views off the screen are not created unless shown. This increases performance.")
+            ScrollView {
+                LazyVStack(spacing: 5) {
+                    ForEach(teams) { team in
+                        Section {
+                            ForEach(team.people) { person in
+                                Image("\(person.imageName)")
+                                    .resizable()
+                                    .frame(width: 300, height: 300)
+                            }
+                        } header: {
+                            TeamVerticalHeaderView(team: team)
+                        } footer: {
+                            TeamVerticalFooterView(team: team)
+                        }
+                    }
+                }
+            }
+            Spacer()
+        }
+        .font(.title)
     }
 }
 

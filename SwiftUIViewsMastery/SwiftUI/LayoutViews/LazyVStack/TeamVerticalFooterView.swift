0a7e4c213ef17fae82fe63ad9350761db174d02c
Alex <oleksii.pidhornyi@gmail.com>
2025-03-20 15:41:00 +0100

    extend team vertical footer view

--- SwiftUIViewsMastery/SwiftUI/LayoutViews/LazyVStack/TeamVerticalFooterView.swift
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/LazyVStack/TeamVerticalFooterView.swift
@@ -8,8 +8,19 @@
 import SwiftUI
 
 struct TeamVerticalFooterView: View {
+    var team: Data.Team
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Text("Team Total: ")
+                .font(.title)
+            Text("\(team.people.count)")
+                .font(.title)
+                .fontWeight(.bold)
+        }
+        .frame(width: 300, height: 50)
+        .background(Rectangle()
+            .fill(Color.yellow)
+            .opacity(0.9))
     }
 }
 

7c08e63bd8821eba49a31df015c7cb864935d45a
Alex <oleksii.pidhornyi@gmail.com>
2025-03-21 10:47:25 +0100

    add hstack customizing

---
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/HStack/HStack_Customizing.swift
@@ -0,0 +1,18 @@
+//
+//  HStack_Customizing.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 21.03.2025.
+//
+
+import SwiftUI
+
+struct HStack_Customizing: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HStack_Customizing()
+}

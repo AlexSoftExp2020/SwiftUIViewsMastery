d856848a997c75c1475d962c374126b2ad55b7e9
Alex <oleksii.pidhornyi@gmail.com>
2025-03-21 10:25:48 +0100

    add hstack text alignment first last

---
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/HStack/HStack_TextAlignment_FirstLast.swift
@@ -0,0 +1,18 @@
+//
+//  HStack_TextAlignment_FirstLast.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 21.03.2025.
+//
+
+import SwiftUI
+
+struct HStack_TextAlignment_FirstLast: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HStack_TextAlignment_FirstLast()
+}

f1bac50df00a063538dc725b6127be791f38ce70
Alex <oleksii.pidhornyi@gmail.com>
2025-03-21 10:19:00 +0100

    extend hstack text alignment

--- SwiftUIViewsMastery/SwiftUI/LayoutViews/HStack/HStack_TextAlignment.swift
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/HStack/HStack_TextAlignment.swift
@@ -9,7 +9,31 @@ import SwiftUI
 
 struct HStack_TextAlignment: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            HeaderView("HStack",
+                       subtitle: "Text Alignment",
+                       desc: "HStacks have another alignment option to help better align the bottom of text.",
+                       back: .orange)
+            
+            HStack(alignment: .bottom) {
+                Text("Hello")
+                Text("amazing")
+                    .font(.largeTitle)
+                Text("developer!")
+            }
+            .font(.body)
+            
+            DescView("Notice the bottom of the text isn't really aligned above. Use firstTextBaseLine", back: .orange)
+            
+            HStack(alignment: .firstTextBaseline) {
+                Text("Hello")
+                Text("amazing")
+                    .font(.largeTitle)
+                Text("developer!")
+            }
+            .font(.body)
+        }
+        .font(.title)
     }
 }
 

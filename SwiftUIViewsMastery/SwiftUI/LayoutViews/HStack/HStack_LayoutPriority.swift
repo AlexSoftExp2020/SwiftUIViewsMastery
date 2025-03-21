dc58868f3d7109dbf9951469e559f01aafab35ec
Alex <oleksii.pidhornyi@gmail.com>
2025-03-21 19:52:16 +0100

    extend hstack layout priority

--- SwiftUIViewsMastery/SwiftUI/LayoutViews/HStack/HStack_LayoutPriority.swift
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/HStack/HStack_LayoutPriority.swift
@@ -9,7 +9,30 @@ import SwiftUI
 
 struct HStack_LayoutPriority: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Text("SwiftUI")
+                .font(.largeTitle)
+                .lineLimit(1) // Don't let text wrap
+            Image("SwiftUI")
+                .resizable()
+                .frame(width: 80, height: 80)
+            Text("Bring Balance")
+                .font(.largeTitle)
+                .layoutPriority(1)
+        }
+        .padding([.horizontal])
+        Divider()
+        HStack {
+            Text("SwiftUI")
+                .font(.largeTitle)
+                .layoutPriority(1) // Truncate last
+            Image("SwiftUI")
+                .resizable()
+                .frame(width: 80, height: 80)
+            Text("Brings Balance")
+                .font(.largeTitle)
+                .lineLimit(1) // Don't let text wrap
+        }
     }
 }
 

14f16f98cbaedd1b1003a672ec80147013db6401
Alex <oleksii.pidhornyi@gmail.com>
2025-03-21 10:45:52 +0100

    extend hstack text alignment first last

--- SwiftUIViewsMastery/SwiftUI/LayoutViews/HStack/HStack_TextAlignment_FirstLast.swift
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/HStack/HStack_TextAlignment_FirstLast.swift
@@ -9,7 +9,29 @@ import SwiftUI
 
 struct HStack_TextAlignment_FirstLast: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            HeaderView("HStack",
+                       subtitle: "First & Last Text Alignment",
+                       desc: "The firstTextBaseline will align the bottom of the text on the first lines (\"Amazing\" and \"Rally\").",
+                       back: .orange)
+            
+            HStack(alignment: .firstTextBaseline) {
+                Text("Amazing developer")
+                    .font(.title3)
+                Text("Really amazing developer")
+            }
+            .frame(width: 250)
+            
+            DescView("The lastTextBaseline will align the bottom of the text on the last lines (\"developer\" and \"developer\").", back: .orange)
+            
+            HStack(alignment: .lastTextBaseline) {
+                Text("Amazing developer")
+                    .font(.title3)
+                Text("Really amazing developer")
+            }
+            .frame(width: 250)
+        }
+        .font(.title)
     }
 }
 

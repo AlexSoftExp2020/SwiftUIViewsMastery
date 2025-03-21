2a9a5f9fd5f99f83fc17920a9032266c7a631862
Alex <oleksii.pidhornyi@gmail.com>
2025-03-21 10:59:23 +0100

    add hstack layout priority

---
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/HStack/HStack_LayoutPriority.swift
@@ -0,0 +1,18 @@
+//
+//  HStack_LayoutPriority.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 21.03.2025.
+//
+
+import SwiftUI
+
+struct HStack_LayoutPriority: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HStack_LayoutPriority()
+}

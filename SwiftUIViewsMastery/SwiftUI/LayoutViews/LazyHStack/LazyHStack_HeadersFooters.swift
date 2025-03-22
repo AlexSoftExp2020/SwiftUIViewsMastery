2284c94ba75d182bd2636f827545cd51238379a4
Alex <oleksii.pidhornyi@gmail.com>
2025-03-23 00:12:42 +0100

    extend lazy h stack headers footers

--- SwiftUIViewsMastery/SwiftUI/LayoutViews/LazyHStack/LazyHStack_HeadersFooters.swift
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/LazyHStack/LazyHStack_HeadersFooters.swift
@@ -8,8 +8,36 @@
 import SwiftUI
 
 struct LazyHStack_HeadersFooters: View {
+    @State private var teams = Data.getTeams()
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 10.0) {
+            HeaderView("LazyHStack",
+                       subtitle: "Headers & Footers",
+                       desc: "Using the Section view, you can add a header and footer inside a LazyHStack.")
+            Spacer()
+            ScrollView(.horizontal) {
+                LazyHStack(spacing: 5) {
+                    ForEach(teams) { team in
+                        Section {
+                            ForEach(team.people) { person in
+                                Image("\(person.imageName)")
+                                    .resizable()
+                                    .frame(width: 100)
+                                    .padding(.bottom, 8)
+                            }
+                        } header: {
+                            TeamHeaderView(team: team)
+                        } footer: {
+                            TeamFooterView(team: team)
+                        }
+                    }
+                }
+            }
+            .frame(height: 108)
+            
+            Spacer()
+        }
+        .font(.title)
     }
 }
 

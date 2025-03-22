b780e63c3e3f87a99d9812c1c41deb7456966abf
Alex <oleksii.pidhornyi@gmail.com>
2025-03-23 00:20:48 +0100

    add team header view

---
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/LazyHStack/TeamHeaderView.swift
@@ -0,0 +1,30 @@
+//
+//  TeamHeaderView.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 23.03.2025.
+//
+
+import SwiftUI
+
+struct TeamHeaderView: View {
+    var team: Data.Team
+    
+    var body: some View {
+        VStack(spacing: 8) {
+            Text("Team")
+                .font(.title2)
+            Image(systemName: team.imageName)
+                .font(.system(size: 50))
+        }
+        .frame(width: 75, height: 100)
+        .background(Rectangle()
+            .fill(Color.yellow)
+            .opacity(0.9))
+        .padding(.bottom, 8)
+    }
+}
+
+#Preview {
+    TeamHeaderView()
+}

23252a7d2da9df26bab7e2f9c813773ed668d082
Alex <oleksii.pidhornyi@gmail.com>
2025-03-23 00:13:39 +0100

    comment lazy h stack headers footers

--- SwiftUIViewsMastery/SwiftUI/LayoutViews/LazyHStack/LazyHStack_HeadersFooters.swift
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/LazyHStack/LazyHStack_HeadersFooters.swift
@@ -7,40 +7,40 @@
 
 import SwiftUI
 
-struct LazyHStack_HeadersFooters: View {
-    @State private var teams = Data.getTeams()
-    var body: some View {
-        VStack(spacing: 10.0) {
-            HeaderView("LazyHStack",
-                       subtitle: "Headers & Footers",
-                       desc: "Using the Section view, you can add a header and footer inside a LazyHStack.")
-            Spacer()
-            ScrollView(.horizontal) {
-                LazyHStack(spacing: 5) {
-                    ForEach(teams) { team in
-                        Section {
-                            ForEach(team.people) { person in
-                                Image("\(person.imageName)")
-                                    .resizable()
-                                    .frame(width: 100)
-                                    .padding(.bottom, 8)
-                            }
-                        } header: {
-                            TeamHeaderView(team: team)
-                        } footer: {
-                            TeamFooterView(team: team)
-                        }
-                    }
-                }
-            }
-            .frame(height: 108)
-            
-            Spacer()
-        }
-        .font(.title)
-    }
-}
-
-#Preview {
-    LazyHStack_HeadersFooters()
-}
+//struct LazyHStack_HeadersFooters: View {
+//    @State private var teams = Data.getTeams()
+//    var body: some View {
+//        VStack(spacing: 10.0) {
+//            HeaderView("LazyHStack",
+//                       subtitle: "Headers & Footers",
+//                       desc: "Using the Section view, you can add a header and footer inside a LazyHStack.")
+//            Spacer()
+//            ScrollView(.horizontal) {
+//                LazyHStack(spacing: 5) {
+//                    ForEach(teams) { team in
+//                        Section {
+//                            ForEach(team.people) { person in
+//                                Image("\(person.imageName)")
+//                                    .resizable()
+//                                    .frame(width: 100)
+//                                    .padding(.bottom, 8)
+//                            }
+//                        } header: {
+//                            TeamHeaderView(team: team)
+//                        } footer: {
+//                            TeamFooterView(team: team)
+//                        }
+//                    }
+//                }
+//            }
+//            .frame(height: 108)
+//            
+//            Spacer()
+//        }
+//        .font(.title)
+//    }
+//}
+//
+//#Preview {
+//    LazyHStack_HeadersFooters()
+//}

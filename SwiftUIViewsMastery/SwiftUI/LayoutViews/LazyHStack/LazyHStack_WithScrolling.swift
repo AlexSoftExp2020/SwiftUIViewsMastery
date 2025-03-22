af34acfa3f5e134eb78439f0242cd57085d3d5a3
Alex <oleksii.pidhornyi@gmail.com>
2025-03-22 10:18:06 +0100

    add lazy h stack with scrolling

---
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/LazyHStack/LazyHStack_WithScrolling.swift
@@ -0,0 +1,18 @@
+//
+//  LazyHStack_WithScrolling.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 22.03.2025.
+//
+
+import SwiftUI
+
+struct LazyHStack_WithScrolling: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LazyHStack_WithScrolling()
+}

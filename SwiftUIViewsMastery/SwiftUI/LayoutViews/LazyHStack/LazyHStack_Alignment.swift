fc9fe025a4e36c7ada60eb5e0c3f1dcfd6c6870f
Alex <oleksii.pidhornyi@gmail.com>
2025-03-22 09:42:08 +0100

    add lazy h stack alignment

---
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/LazyHStack/LazyHStack_Alignment.swift
@@ -0,0 +1,18 @@
+//
+//  LazyHStack_Alignment.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 22.03.2025.
+//
+
+import SwiftUI
+
+struct LazyHStack_Alignment: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LazyHStack_Alignment()
+}

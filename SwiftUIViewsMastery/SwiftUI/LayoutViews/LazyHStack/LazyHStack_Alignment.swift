ce2b8a5e717e6484a8479131f8c87b153f62fe0e
Alex <oleksii.pidhornyi@gmail.com>
2025-03-22 10:06:08 +0100

    extend lazy h stack alignment

--- SwiftUIViewsMastery/SwiftUI/LayoutViews/LazyHStack/LazyHStack_Alignment.swift
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/LazyHStack/LazyHStack_Alignment.swift
@@ -9,7 +9,31 @@ import SwiftUI
 
 struct LazyHStack_Alignment: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20.0) {
+            HeaderView("LazyHStack",
+                       subtitle: "Alignment",
+                       desc: "Since LazyHStacks are push-out views (vertically) the alignment parameter could be useful.")
+            .layoutPriority(1)
+            
+            Text("Top")
+            
+            LazyHStack(alignment: .top, spacing: 40) {
+                Image(systemName: "1.circle")
+                Image(systemName: "2.circle")
+                Image(systemName: "3.circle")
+            }
+            .border(Color.red, width: 2) // Draws a border around the frame of the view
+            
+            Text("Bottom")
+            
+            LazyHStack(alignment: .bottom, spacing: 40) {
+                Image(systemName: "1.circle")
+                Image(systemName: "2.circle")
+                Image(systemName: "3.circle")
+            }
+            .border(Color.red, width: 2)
+        }
+        .font(.title)
     }
 }
 

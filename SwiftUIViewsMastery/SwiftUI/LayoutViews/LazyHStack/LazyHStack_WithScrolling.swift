da822773ce72402c62c2942c8566bd252b4c0a59
Alex <oleksii.pidhornyi@gmail.com>
2025-03-22 11:12:26 +0100

    extend lazy h stack with scrolling

--- SwiftUIViewsMastery/SwiftUI/LayoutViews/LazyHStack/LazyHStack_WithScrolling.swift
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/LazyHStack/LazyHStack_WithScrolling.swift
@@ -8,8 +8,32 @@
 import SwiftUI
 
 struct LazyHStack_WithScrolling: View {
+    @State private var whatAppeared = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 10.0) {
+            HeaderView("LazyHStack",
+                       subtitle: "With ScrollView",
+                       desc: "The LazyHStack is best used with many views that scroll off the screen. \"Lazy\" means views off the screen are not created unless shown. THis increases performance.")
+            Spacer()
+            
+            ScrollView(.horizontal) {
+                LazyHStack(spacing: 20) {
+                    ForEach(0..<51) { item in
+                        Image(systemName: "\(item).circle")
+                            .onAppear {
+                                whatAppeared = "\(item).circle"
+                            }
+                    }
+                }
+            }
+            .font(.largeTitle)
+            .padding()
+            
+            Text("\(whatAppeared)")
+            
+            Spacer()
+        }
+        .font(.title)
     }
 }
 

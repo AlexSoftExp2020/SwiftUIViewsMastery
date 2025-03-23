3382cea8bdb2d1130e0b4238719ce9ce47c61317
Alex <oleksii.pidhornyi@gmail.com>
2025-03-23 01:13:04 +0100

    comment team header view

--- SwiftUIViewsMastery/SwiftUI/LayoutViews/LazyHStack/TeamHeaderView.swift
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/LazyHStack/TeamHeaderView.swift
@@ -7,24 +7,24 @@
 
 import SwiftUI
 
-struct TeamHeaderView: View {
-    var team: Data.Team
-    
-    var body: some View {
-        VStack(spacing: 8) {
-            Text("Team")
-                .font(.title2)
-            Image(systemName: team.imageName)
-                .font(.system(size: 50))
-        }
-        .frame(width: 75, height: 100)
-        .background(Rectangle()
-            .fill(Color.yellow)
-            .opacity(0.9))
-        .padding(.bottom, 8)
-    }
-}
-
-#Preview {
-    TeamHeaderView()
-}
+//struct TeamHeaderView: View {
+//    var team: Data.Team
+//    
+//    var body: some View {
+//        VStack(spacing: 8) {
+//            Text("Team")
+//                .font(.title2)
+//            Image(systemName: team.imageName)
+//                .font(.system(size: 50))
+//        }
+//        .frame(width: 75, height: 100)
+//        .background(Rectangle()
+//            .fill(Color.yellow)
+//            .opacity(0.9))
+//        .padding(.bottom, 8)
+//    }
+//}
+//
+//#Preview {
+//    TeamHeaderView()
+//}

042e9ffe3ec76a43777e6daa39da95f8ca84d27f
Alex <oleksii.pidhornyi@gmail.com>
2025-03-23 20:18:57 +0100

    extend grid no grid row

--- SwiftUIViewsMastery/SwiftUI/LayoutViews/Grid/Grid_NoGridRow.swift
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/Grid/Grid_NoGridRow.swift
@@ -9,7 +9,29 @@ import SwiftUI
 
 struct Grid_NoGridRow: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Grid {
+            GridRow {
+                Text("Column 1")
+                Text("Column 2")
+            }
+            
+            GridRow {
+                RoundedRectangle(cornerRadius: 16)
+                    .fill(Color.blue.opacity(0.5))
+                RoundedRectangle(cornerRadius: 16)
+                    .fill(Color.orange.opacity(0.5))
+                    .frame(width: 150)
+            }
+            
+            RoundedRectangle(cornerRadius: 16)
+                .fill(Color.pink.opacity(0.5))
+            
+            GridRow {
+                RoundedRectangle(cornerRadius: 16)
+                    .fill(Color.blue.opacity(0.5))
+            }
+        }
+        .font(.title)
     }
 }
 

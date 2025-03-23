b3a0e4672c9e5c15228920f96e1397f10abcc89c
Alex <oleksii.pidhornyi@gmail.com>
2025-03-23 02:24:46 +0100

    extend z stack background color problem

--- SwiftUIViewsMastery/SwiftUI/LayoutViews/ZStack/ZStack_BackgroundColor_Problem.swift
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/ZStack/ZStack_BackgroundColor_Problem.swift
@@ -9,7 +9,26 @@ import SwiftUI
 
 struct ZStack_BackgroundColor_Problem: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.gray
+            
+            VStack(spacing: 20) {
+                Text("ZStack") // This view is undet the notch
+                    .font(.largeTitle)
+                
+                Text("Ignores Safe Area Edges")
+                    .foregroundStyle(.white)
+                
+                Text("Having the ZStack edges ignoring the safe area edges might be a mistake. \nYou notice that the top Text view is completely under the notch.")
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .background(Color.green)
+                
+                Spacer() // Added a spacer to push the views up.
+            }
+            .font(.title)
+        }
+        .ignoresSafeArea()
     }
 }
 

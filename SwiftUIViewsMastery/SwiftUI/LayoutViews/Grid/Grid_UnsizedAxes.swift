fc60fbb209072a5424d1c320f7b719119ab6e4ab
Alex <oleksii.pidhornyi@gmail.com>
2025-03-23 20:29:19 +0100

    add grid unsized axes

---
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/Grid/Grid_UnsizedAxes.swift
@@ -0,0 +1,18 @@
+//
+//  Grid_UnsizedAxes.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 23.03.2025.
+//
+
+import SwiftUI
+
+struct Grid_UnsizedAxes: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Grid_UnsizedAxes()
+}

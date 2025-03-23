d03742ff780552e3880e465b7b01327bdcb75275
Alex <oleksii.pidhornyi@gmail.com>
2025-03-23 03:13:59 +0100

    extend z stack layering

--- SwiftUIViewsMastery/SwiftUI/LayoutViews/ZStack/ZStack_Layering.swift
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/ZStack/ZStack_Layering.swift
@@ -9,7 +9,31 @@ import SwiftUI
 
 struct ZStack_Layering: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            HeaderView("ZStack",
+                       subtitle: "Layering & Aligning",
+                       desc: "ZStacks are great for layering views. For example, putting text on top of an image.",
+                       back: .green,
+                       textColor: .white)
+            
+            ZStack {
+                Image("yosemite_large")
+                    .resizable() // Allows image to change size
+                    .scaledToFit() // Keeps image the same aspect ratio when resizing
+                
+                Rectangle()
+                    .fill(Color.white.opacity(0.6))
+                    .frame(maxWidth: .infinity, maxHeight: 50)
+                
+                Text("Yosemite National Park")
+                    .font(.title)
+                    .padding()
+            }
+            DescView("But what if you wanted to have all the views align to the bottom?",
+                     back: .green,
+                     textColor: .white)
+        }
+        .font(.title)
     }
 }
 

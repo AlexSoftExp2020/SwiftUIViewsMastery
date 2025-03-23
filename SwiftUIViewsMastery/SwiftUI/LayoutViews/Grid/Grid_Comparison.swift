69ca03c7deb4b5325000bd33a0b992b6e310ea7e
Alex <oleksii.pidhornyi@gmail.com>
2025-03-23 19:20:25 +0100

    add grid comparison

---
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/Grid/Grid_Comparison.swift
@@ -0,0 +1,18 @@
+//
+//  Grid_Comparison.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 23.03.2025.
+//
+
+import SwiftUI
+
+struct Grid_Comparison: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Grid_Comparison()
+}

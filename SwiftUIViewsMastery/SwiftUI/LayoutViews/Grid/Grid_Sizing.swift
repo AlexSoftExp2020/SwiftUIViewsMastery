23e111cf84d48bdfcc7fdb66e3be83429018bd93
Alex <oleksii.pidhornyi@gmail.com>
2025-03-23 20:19:42 +0100

    add grid sizing

---
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/Grid/Grid_Sizing.swift
@@ -0,0 +1,18 @@
+//
+//  Grid_Sizing.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 23.03.2025.
+//
+
+import SwiftUI
+
+struct Grid_Sizing: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Grid_Sizing()
+}

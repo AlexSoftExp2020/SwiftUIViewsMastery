6c02b7ee71d49af00eb474e03e75f73ee04a4ab5
Alex <oleksii.pidhornyi@gmail.com>
2025-03-23 02:14:42 +0100

    add zstack background color problem

---
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/ZStack/ZStack_BackgroundColor_Problem.swift
@@ -0,0 +1,18 @@
+//
+//  ZStack_BackgroundColor_Problem.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 23.03.2025.
+//
+
+import SwiftUI
+
+struct ZStack_BackgroundColor_Problem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ZStack_BackgroundColor_Problem()
+}

b304b993f4f08b434fb6b9b2d00cd810d53a72d9
Alex <oleksii.pidhornyi@gmail.com>
2025-03-23 21:18:01 +0100

    add grid spacing

---
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/Grid/GridSpacing.swift
@@ -0,0 +1,18 @@
+//
+//  GridSpacing.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 23.03.2025.
+//
+
+import SwiftUI
+
+struct GridSpacing: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GridSpacing()
+}

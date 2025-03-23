1cccd8198b832f62fb7827fe344d7705ff434e20
Alex <oleksii.pidhornyi@gmail.com>
2025-03-23 21:34:01 +0100

    extend grid spacing

--- SwiftUIViewsMastery/SwiftUI/LayoutViews/Grid/GridSpacing.swift
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/Grid/GridSpacing.swift
@@ -9,7 +9,32 @@ import SwiftUI
 
 struct GridSpacing: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Grid(horizontalSpacing: 24, verticalSpacing: 24) {
+            
+            GridRow {
+                Color.green.opacity(0.5)
+                Color.green.opacity(0.5)
+                Color.green.opacity(0.5)
+            }
+            
+            GridRow {
+                Color.blue.opacity(0.5)
+                Color.orange.opacity(0.5)
+                Color.red.opacity(0.5)
+            }
+            
+            GridRow {
+                Color.blue.opacity(0.5)
+                Color.orange.opacity(0.5)
+                Color.red.opacity(0.5)
+            }
+            
+            GridRow {
+                Color.orange.opacity(0.5)
+                Color.orange.opacity(0.5)
+                Color.red.opacity(0.5)
+            }
+        }
     }
 }
 

cdbd30b7dfbdf33343f7765e018db7fea3583c1a
Alex <oleksii.pidhornyi@gmail.com>
2025-03-23 19:12:42 +0100

    add grd intro

---
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/Grid/Grid_Intro.swift
@@ -0,0 +1,18 @@
+//
+//  Grid_Intro.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 23.03.2025.
+//
+
+import SwiftUI
+
+struct Grid_Intro: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Grid_Intro()
+}

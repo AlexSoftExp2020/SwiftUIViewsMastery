9b23bdfa34c21fedc99559286918dc3e219db88f
Alex <oleksii.pidhornyi@gmail.com>
2025-03-23 19:40:14 +0100

    add grid no grid row

---
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/Grid/Grid_NoGridRow.swift
@@ -0,0 +1,18 @@
+//
+//  Grid_NoGridRow.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 23.03.2025.
+//
+
+import SwiftUI
+
+struct Grid_NoGridRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Grid_NoGridRow()
+}

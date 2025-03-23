3b11d2a2c27654d94b42b8e21c92c0073c440dd7
Alex <oleksii.pidhornyi@gmail.com>
2025-03-23 20:45:09 +0100

    add grid alignment

---
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/Grid/Grid_Alignment.swift
@@ -0,0 +1,18 @@
+//
+//  Grid_Alignment.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 23.03.2025.
+//
+
+import SwiftUI
+
+struct Grid_Alignment: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Grid_Alignment()
+}

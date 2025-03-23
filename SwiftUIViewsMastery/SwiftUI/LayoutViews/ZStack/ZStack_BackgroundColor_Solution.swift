6e0c5ea281d30946561384ff2884ddf4def5c236
Alex <oleksii.pidhornyi@gmail.com>
2025-03-23 03:02:49 +0100

    extend z stack background color solution

--- SwiftUIViewsMastery/SwiftUI/LayoutViews/ZStack/ZStack_BackgroundColor_Solution.swift
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/ZStack/ZStack_BackgroundColor_Solution.swift
@@ -9,7 +9,25 @@ import SwiftUI
 
 struct ZStack_BackgroundColor_Solution: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.gray
+                .ignoresSafeArea() // Have JUST the color ignore the safe areas edges, not the VStack.
+            
+            VStack {
+                Text("ZStack")
+                    .font(.largeTitle)
+                
+                Text("Color Ignores Safe Area Edges")
+                    .foregroundStyle(.white)
+                
+                Text("To solve the problem, you want just the color (bottom layer) to ignore the safe area edges and fill the screen. Other layers above it will stay within the Safe Area.")
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .background(Color.green)
+                Spacer()
+            }
+            .font(.title)
+        }
     }
 }
 

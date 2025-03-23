ee67555545deb5dce35e7f6909d216a8f441ff10
Alex <oleksii.pidhornyi@gmail.com>
2025-03-23 01:26:27 +0100

    comment lazy h stack pinned views

--- SwiftUIViewsMastery/SwiftUI/LayoutViews/LazyHStack/LazyHStack_PinnedViews.swift
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/LazyHStack/LazyHStack_PinnedViews.swift
@@ -7,43 +7,43 @@
 
 import SwiftUI
 
-struct LazyHStack_PinnedViews: View {
-    @State private var teams = Data.getTeams()
-    
-    var body: some View {
-        VStack(spacing: 20) {
-            HeaderView("LazyHStack",
-                       subtitle: "Pinned Views",
-                       desc: "LazyHStacks can also have section headers and section footers that can be pinned so they only scroll when the next header/footer comes.")
-            
-            Spacer()
-            
-            ScrollView(.horizontal) {
-                LazyHStack(spacing: 5, pinnedViews: [.sectionHeaders, .sectionFooters]) {
-                    ForEach(teams) { team in
-                        Section {
-                            ForEach(team.people) { person in
-                                Image("\(person.imageName)")
-                                    .resizable()
-                                    .frame(width: 100)
-                                    .padding(.bottom, 8)
-                            }
-                        } header: {
-                            TeamHeaderView(team: team)
-                        } footer: {
-                            TeamFooterView(team: team)
-                        }
-                    }
-                }
-            }
-            .frame(height: 108)
-            
-            Spacer()
-        }
-        .font(.title)
-    }
-}
-
-#Preview {
-    LazyHStack_PinnedViews()
-}
+//struct LazyHStack_PinnedViews: View {
+//    @State private var teams = Data.getTeams()
+//    
+//    var body: some View {
+//        VStack(spacing: 20) {
+//            HeaderView("LazyHStack",
+//                       subtitle: "Pinned Views",
+//                       desc: "LazyHStacks can also have section headers and section footers that can be pinned so they only scroll when the next header/footer comes.")
+//            
+//            Spacer()
+//            
+//            ScrollView(.horizontal) {
+//                LazyHStack(spacing: 5, pinnedViews: [.sectionHeaders, .sectionFooters]) {
+//                    ForEach(teams) { team in
+//                        Section {
+//                            ForEach(team.people) { person in
+//                                Image("\(person.imageName)")
+//                                    .resizable()
+//                                    .frame(width: 100)
+//                                    .padding(.bottom, 8)
+//                            }
+//                        } header: {
+//                            TeamHeaderView(team: team)
+//                        } footer: {
+//                            TeamFooterView(team: team)
+//                        }
+//                    }
+//                }
+//            }
+//            .frame(height: 108)
+//            
+//            Spacer()
+//        }
+//        .font(.title)
+//    }
+//}
+//
+//#Preview {
+//    LazyHStack_PinnedViews()
+//}

43c5a3ba34486bc609b226ef09384965cfa1477a
Alex <oleksii.pidhornyi@gmail.com>
2025-03-23 01:12:38 +0100

    comment team footer view

--- SwiftUIViewsMastery/SwiftUI/LayoutViews/LazyHStack/TeamFooterView.swift
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/LazyHStack/TeamFooterView.swift
@@ -7,25 +7,25 @@
 
 import SwiftUI
 
-struct TeamFooterView: View {
-    var team: Data.Team
-    
-    var body: some View {
-        VStack(spacing: 0) {
-            Text("Total")
-                .font(.title2)
-            Text("\(team.people.count)")
-                .font(.largeTitle)
-                .fontWeight(.bold)
-        }
-        .frame(width: 60, height: 100)
-        .background(Rectangle()
-            .fill(Color.yellow)
-            .opacity(0.9))
-        .padding(.bottom, 8)
-    }
-}
-
-#Preview {
-    TeamFooterView()
-}
+//struct TeamFooterView: View {
+//    var team: Data.Team
+//    
+//    var body: some View {
+//        VStack(spacing: 0) {
+//            Text("Total")
+//                .font(.title2)
+//            Text("\(team.people.count)")
+//                .font(.largeTitle)
+//                .fontWeight(.bold)
+//        }
+//        .frame(width: 60, height: 100)
+//        .background(Rectangle()
+//            .fill(Color.yellow)
+//            .opacity(0.9))
+//        .padding(.bottom, 8)
+//    }
+//}
+//
+//#Preview {
+//    TeamFooterView()
+//}

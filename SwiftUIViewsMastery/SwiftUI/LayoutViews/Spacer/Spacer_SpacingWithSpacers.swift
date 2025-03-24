1f58958ef529def713bc7bd0736c4913f9ae2e21
Alex <oleksii.pidhornyi@gmail.com>
2025-03-24 20:06:02 +0100

    add spacer spacing with spacers

---
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/Spacer/Spacer_SpacingWithSpacers.swift
@@ -0,0 +1,18 @@
+//
+//  Spacer_SpacingWithSpacers.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 24.03.2025.
+//
+
+import SwiftUI
+
+struct Spacer_SpacingWithSpacers: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Spacer_SpacingWithSpacers()
+}

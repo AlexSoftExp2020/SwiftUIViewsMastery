766f9ad5b5e7e4bd514f99302347b36c8b4b1143
Alex <oleksii.pidhornyi@gmail.com>
2025-03-24 21:37:08 +0100

    add geometry reader getting coordinates

---
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/GeometryReader/GeometryReader_GettingCoordinates.swift
@@ -0,0 +1,18 @@
+//
+//  GeometryReader_GettingCoordinates.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 24.03.2025.
+//
+
+import SwiftUI
+
+struct GeometryReader_GettingCoordinates: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GeometryReader_GettingCoordinates()
+}

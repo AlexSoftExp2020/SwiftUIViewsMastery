922684344430f3eeecb5404b76b89a092cd5920d
Alex <oleksii.pidhornyi@gmail.com>
2025-03-24 19:36:13 +0100

    extend spacer introduction

--- SwiftUIViewsMastery/SwiftUI/LayoutViews/Spacer/Spacer_Introduction.swift
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/Spacer/Spacer_Introduction.swift
@@ -9,7 +9,36 @@ import SwiftUI
 
 struct Spacer_Introduction: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            
+            Text("Spacer")
+                .font(.largeTitle)
+            
+//            Color.yellow
+//                .frame(height: 100)
+//                .overlay {
+//                Text("Spacers push things away either vertically or horizontally")
+//            }
+            Text("Spacers push things away either vertically or horizontally")
+                .background(Color.yellow.frame(width: .infinity))
+            
+            Image(systemName: "arrow.up.circle.fill")
+            
+            Spacer()
+            
+            Image(systemName: "arrow.down.circle.fill")
+            
+            HStack {
+                Text("Horizontal Spacer")
+                Image(systemName: "arrow.left.circle.fill")
+                Spacer()
+                Image(systemName: "arrow.right.circle.fill")
+            }
+            .padding(.horizontal)
+            
+            Color.yellow.frame(maxHeight: 50) // Height can decrease but not go higher than 50
+        }
+        .font(.title) // Apply this font to every view within the VStack
     }
 }
 

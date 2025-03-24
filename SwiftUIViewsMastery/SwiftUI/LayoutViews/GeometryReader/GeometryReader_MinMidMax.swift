afa47069cbfb7c22d19dee4f3a0efc0d6ed4cd29
Alex <oleksii.pidhornyi@gmail.com>
2025-03-24 23:14:19 +0100

    extend geometry reader min mid max

--- SwiftUIViewsMastery/SwiftUI/LayoutViews/GeometryReader/GeometryReader_MinMidMax.swift
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/GeometryReader/GeometryReader_MinMidMax.swift
@@ -9,7 +9,58 @@ import SwiftUI
 
 struct GeometryReader_MinMidMax: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            HeaderView("GeometryReader",
+                       subtitle: "Min Mid Max",
+                       desc: "You can also get the minimum (min), middle (mid), and maximum (max)  X and Y coordinate from the geometry readers's frame.",
+                       back: .clear)
+            
+            GeometryReader { geometry in
+                VStack(alignment: .leading, spacing: 20) {
+                    Text("Local Coordinate Space")
+                    HStack(spacing: 10) {
+                        // I'm converting to Int just so we don't have so many zeros
+                        Text("minX: \(Int(geometry.frame(in: .local).minX))")
+                        Spacer()
+                        Text("midX: \(Int(geometry.frame(in: .local).midX))")
+                        Spacer()
+                        Text("maxX: \(Int(geometry.frame(in: .local).maxX))")
+                    }
+                    Text("Global Coordinate Space")
+                    HStack(spacing: 10) {
+                        // I'm converting to Int just so we don't have so many zeros
+                        Text("minX: \(Int(geometry.frame(in: .global).minX))")
+                        Spacer()
+                        Text("midX: \(Int(geometry.frame(in: .global).midX))")
+                        Spacer()
+                        Text("maxX: \(Int(geometry.frame(in: .global).maxX))")
+                    }
+                }.padding(.horizontal)
+            }
+            .frame(height: 200)
+            .foregroundStyle(.white)
+            .background(Color.pink)
+            
+            HStack {
+                GeometryReader { geometry in
+                    VStack(spacing: 10) {
+                        Text("minY: \(Int(geometry.frame(in: .global).minY))")
+                        Spacer()
+                        Text("midY: \(Int(geometry.frame(in: .global).midY))")
+                        Spacer()
+                        Text("maxY: \(Int(geometry.frame(in: .global).maxY))")
+                    }.padding(.vertical)
+                }
+                .foregroundStyle(.white)
+                .background(Color.pink)
+                
+                Image("MinMidMax")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+            }
+        }
+        .font(.title)
+        .padding()
     }
 }
 

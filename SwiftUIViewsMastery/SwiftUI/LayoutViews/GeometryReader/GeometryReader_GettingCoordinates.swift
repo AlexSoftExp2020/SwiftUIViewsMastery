c208a87ebf00c0f84c97706961f38b68c7fd4558
Alex <oleksii.pidhornyi@gmail.com>
2025-03-24 22:09:09 +0100

    extend geometry reader getting coordinates

--- SwiftUIViewsMastery/SwiftUI/LayoutViews/GeometryReader/GeometryReader_GettingCoordinates.swift
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/GeometryReader/GeometryReader_GettingCoordinates.swift
@@ -9,7 +9,35 @@ import SwiftUI
 
 struct GeometryReader_GettingCoordinates: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 10) {
+            HeaderView("GeometryReader",
+                       subtitle: "Getting Coordinates",
+                       desc: "Getting the coordinates (x, y) of a geometry view is little different. Take a look at this example:",
+                       back: .clear)
+            
+            GeometryReader { geometryProxy in
+                VStack(spacing: 10) {
+                    Text("X: \(geometryProxy.frame(in: CoordinateSpace.local).origin.x)")
+                    Text("Y: \(geometryProxy.frame(in: CoordinateSpace.local).origin.y)")
+                }
+                .foregroundStyle(.white)
+            }
+            .background(Color.pink)
+            
+            Text("The local coordinate space will always give you zeros.")
+            Text("You need to look globally to get the coordinates inside the current views:")
+            GeometryReader { geometryProxy in
+                VStack(spacing: 10) {
+                    Text("X: \(geometryProxy.frame(in: .global).origin.x)")
+                    Text("Y: \(geometryProxy.frame(in: .global).origin.y)")
+                }
+                .foregroundStyle(.white)
+            }
+            .background(Color.pink)
+            .frame(height: 200)
+        }
+        .font(.title)
+        .padding(.horizontal)
     }
 }
 

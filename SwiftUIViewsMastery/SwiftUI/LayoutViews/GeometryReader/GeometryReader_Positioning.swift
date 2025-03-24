f87dff3028c2f0ed8bd8bfd16de03fae360f84e7
Alex <oleksii.pidhornyi@gmail.com>
2025-03-24 21:35:13 +0100

    extend geometry reader positioning

--- SwiftUIViewsMastery/SwiftUI/LayoutViews/GeometryReader/GeometryReader_Positioning.swift
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/GeometryReader/GeometryReader_Positioning.swift
@@ -9,7 +9,30 @@ import SwiftUI
 
 struct GeometryReader_Positioning: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            Text("GeometryReader").font(.largeTitle)
+            Text("Positioning").font(.title).foregroundStyle(.gray)
+            Text("Use the GeometryProxy input parameter to help position child views at different lactions withing the geometry's view.")
+                .font(.title)
+                .padding()
+            
+            GeometryReader { geometryProxy in
+                Text("Upper Left")
+                    .font(.title)
+                    .position(x: geometryProxy.size.width / 5,
+                              y: geometryProxy.size.height / 10)
+                
+                Text("Lower Right")
+                    .font(.title)
+                    .position(x: geometryProxy.size.width - 90,
+                              y: geometryProxy.size.height - 40)
+            }
+            .background(Color.pink)
+            .foregroundStyle(.white)
+            
+            Text("Note: The position modifier uses the view's center point when setting the X and Y parameters.")
+                .font(.title)
+        }
     }
 }
 

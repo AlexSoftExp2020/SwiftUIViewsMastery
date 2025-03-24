27c3c740e1f96f2ece01b67312fc3bb9367c34ca
Alex <oleksii.pidhornyi@gmail.com>
2025-03-24 20:04:18 +0100

    extend spacer minimum length

--- SwiftUIViewsMastery/SwiftUI/LayoutViews/Spacer/Spacer_MinimumLength.swift
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/Spacer/Spacer_MinimumLength.swift
@@ -9,7 +9,37 @@ import SwiftUI
 
 struct Spacer_MinimumLength: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 10) {
+            Text("Spacer")
+                .font(.largeTitle)
+            Text("Minimum Length")
+                .font(.title)
+                .foregroundStyle(.gray)
+            Text("You can set a minimum space to exist between views using the minLength modifier on the Spacer.")
+            Text("No minLength set (system default is used)")
+                .bold()
+            HStack {
+                Image("yosemite")
+                Spacer()
+                Text("This is Yosemite National Park").lineLimit(1)
+            }.padding()
+            
+            Text("minLength = 0")
+                .bold()
+            HStack {
+                Image("yosemite")
+                Spacer(minLength: 0)
+                Text("This is Yosemite National Park").lineLimit(1)
+            }.padding()
+            
+            Text("minLength = 20")
+                .bold()
+            HStack {
+                Image("yosemite")
+                Spacer(minLength: 20)
+                Text("This is Yosemite National Park").lineLimit(1)
+            }.padding()
+        }
     }
 }
 

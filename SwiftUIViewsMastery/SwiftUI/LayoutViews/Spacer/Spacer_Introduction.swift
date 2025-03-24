03e8517c4aba00b31b2ecbfc45c7bf9376541637
Alex <oleksii.pidhornyi@gmail.com>
2025-03-24 18:55:19 +0100

    add spacer introduction

---
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/Spacer/Spacer_Introduction.swift
@@ -0,0 +1,18 @@
+//
+//  Spacer_Introduction.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 24.03.2025.
+//
+
+import SwiftUI
+
+struct Spacer_Introduction: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Spacer_Introduction()
+}

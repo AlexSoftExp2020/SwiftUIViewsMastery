a33be408484582743379674e787810641b87a7ab
Alex <oleksii.pidhornyi@gmail.com>
2025-03-24 19:49:31 +0100

    add spacer minimum length

---
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/Spacer/Spacer_MinimumLength.swift
@@ -0,0 +1,18 @@
+//
+//  Spacer_MinimumLength.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 24.03.2025.
+//
+
+import SwiftUI
+
+struct Spacer_MinimumLength: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Spacer_MinimumLength()
+}

ab12de08497f6a1aa20544fe03aa63e73f1410af
Alex <oleksii.pidhornyi@gmail.com>
2025-03-24 19:47:52 +0100

    extend spacer evenly spaced

--- SwiftUIViewsMastery/SwiftUI/LayoutViews/Spacer/Spacer_EvenlySpaced.swift
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/Spacer/Spacer_EvenlySpaced.swift
@@ -9,7 +9,39 @@ import SwiftUI
 
 struct Spacer_EvenlySpaced: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        Text("Use Spacer to evenly space views horizontally so they look good on any device.")
+            .background(Color.yellow)
+        
+        Text("After")
+        
+        HStack {
+            Spacer()
+            
+            VStack(alignment: .leading) {
+                Text("Names")
+                    .font(.largeTitle)
+                    .underline()
+                Text("Chase")
+                Text("Rodrigo")
+                Text("Mark")
+                Text("Evans")
+            }
+            
+            Spacer()
+            
+            VStack(alignment: .leading) {
+                Text("Color")
+                    .font(.largeTitle)
+                    .underline()
+                Text("Red")
+                Text("Orange")
+                Text("Green")
+                Text("Blue")
+            }
+            
+            Spacer()
+        }
     }
 }
 

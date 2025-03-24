5303152bba80c72e2998037f42bf8c969b9ea24d
Alex <oleksii.pidhornyi@gmail.com>
2025-03-24 20:46:31 +0100

    add geometry reader intro

---
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/GeometryReader/GeometryReader_Intro.swift
@@ -0,0 +1,18 @@
+//
+//  GeometryReader_Intro.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 24.03.2025.
+//
+
+import SwiftUI
+
+struct GeometryReader_Intro: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GeometryReader_Intro()
+}

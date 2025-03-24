c0efcf0c7c9508246cb0cb7e15bb5aef9f7c1aba
Alex <oleksii.pidhornyi@gmail.com>
2025-03-24 23:17:37 +0100

    add geometry reader safe area insets

---
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/GeometryReader/GeometryReader_SafeAreaInsets.swift
@@ -0,0 +1,18 @@
+//
+//  GeometryReader_SafeAreaInsets.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 24.03.2025.
+//
+
+import SwiftUI
+
+struct GeometryReader_SafeAreaInsets: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GeometryReader_SafeAreaInsets()
+}

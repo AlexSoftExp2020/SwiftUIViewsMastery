929189efa079c6533138f9d39373e00b69b72bb7
Alex <oleksii.pidhornyi@gmail.com>
2025-03-24 20:51:30 +0100

    extend geometry reader intro

--- SwiftUIViewsMastery/SwiftUI/LayoutViews/GeometryReader/GeometryReader_Intro.swift
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/GeometryReader/GeometryReader_Intro.swift
@@ -9,7 +9,18 @@ import SwiftUI
 
 struct GeometryReader_Intro: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            HeaderView("GeometryReader",
+                       subtitle: "Introduction",
+                       desc: "GeometryReader is a container view that pushes out to fill up all available space. You use it to help with positioning items within it.",
+                       back: .clear)
+            
+            GeometryReader { _ in
+                // No child views inside
+            }
+            .background(Color.pink)
+        }
+        .font(.title)
     }
 }
 

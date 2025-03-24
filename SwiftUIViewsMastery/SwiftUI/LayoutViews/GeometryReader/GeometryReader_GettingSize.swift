c7ddb640ce3d036b5eeec4a6003f3285c8a613c8
Alex <oleksii.pidhornyi@gmail.com>
2025-03-24 21:19:15 +0100

    extend geometry reader getting size

--- SwiftUIViewsMastery/SwiftUI/LayoutViews/GeometryReader/GeometryReader_GettingSize.swift
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/GeometryReader/GeometryReader_GettingSize.swift
@@ -9,7 +9,23 @@ import SwiftUI
 
 struct GeometryReader_GettingSize: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 10) {
+            HeaderView("GeometryReader",
+                       subtitle: "Getting Size",
+                       desc: "Use the geometry reader when you need to get the height and/or width of a space.",
+                       back: .clear)
+            
+            GeometryReader { geometryProxy in
+                VStack(spacing: 10) {
+                    Text("Width: \(geometryProxy.size.width)")
+                    Text("Height: \(geometryProxy.size.height)")
+                }
+                .padding()
+                .foregroundStyle(.white)
+            }
+            .background(Color.pink)
+        }
+        .font(.title)
     }
 }
 

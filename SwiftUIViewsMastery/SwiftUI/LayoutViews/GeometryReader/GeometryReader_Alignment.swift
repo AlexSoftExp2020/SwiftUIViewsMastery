ed409d52d58497de4517cd91b575b7fc737ed42c
Alex <oleksii.pidhornyi@gmail.com>
2025-03-24 20:54:02 +0100

    add geometry reader alignment

---
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/GeometryReader/GeometryReader_Alignment.swift
@@ -0,0 +1,18 @@
+//
+//  GeometryReader_Alignment.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 24.03.2025.
+//
+
+import SwiftUI
+
+struct GeometryReader_Alignment: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GeometryReader_Alignment()
+}

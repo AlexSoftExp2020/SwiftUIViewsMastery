4f42af9914dd93a3999b02f6074cd355414319b6
Alex <oleksii.pidhornyi@gmail.com>
2025-03-24 22:57:18 +0100

    add geometry reader min mid max

---
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/GeometryReader/GeometryReader_MinMidMax.swift
@@ -0,0 +1,18 @@
+//
+//  GeometryReader_MinMidMax.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 24.03.2025.
+//
+
+import SwiftUI
+
+struct GeometryReader_MinMidMax: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GeometryReader_MinMidMax()
+}

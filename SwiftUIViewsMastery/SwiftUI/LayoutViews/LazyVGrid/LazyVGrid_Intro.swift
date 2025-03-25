91e56fc1d1e8307a68fa2df17e788201f3689c59
Alex <oleksii.pidhornyi@gmail.com>
2025-03-25 17:04:56 +0100

    add lazy v grid intro

---
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/LazyVGrid/LazyVGrid_Intro.swift
@@ -0,0 +1,18 @@
+//
+//  LazyVGrid_Intro.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 25.03.2025.
+//
+
+import SwiftUI
+
+struct LazyVGrid_Intro: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LazyVGrid_Intro()
+}

16ea8783e37f07e4e68946e3bd47805718120cc6
Alex <oleksii.pidhornyi@gmail.com>
2025-03-25 13:44:29 +0100

    add lazy h grid column spacing

---
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/LazyHGrid/LazyHGrid_ColumnSpacing.swift
@@ -0,0 +1,18 @@
+//
+//  LazyHGrid_ColumnSpacing.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 25.03.2025.
+//
+
+import SwiftUI
+
+struct LazyHGrid_ColumnSpacing: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LazyHGrid_ColumnSpacing()
+}

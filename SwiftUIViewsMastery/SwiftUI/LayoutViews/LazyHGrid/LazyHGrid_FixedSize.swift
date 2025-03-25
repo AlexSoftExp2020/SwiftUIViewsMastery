509db0f805cdda25bbdb4dbe3a021c29de74b292
Alex <oleksii.pidhornyi@gmail.com>
2025-03-25 14:49:31 +0100

    extend lazy h grid fixed size

--- SwiftUIViewsMastery/SwiftUI/LayoutViews/LazyHGrid/LazyHGrid_FixedSize.swift
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/LazyHGrid/LazyHGrid_FixedSize.swift
@@ -9,7 +9,27 @@ import SwiftUI
 
 struct LazyHGrid_FixedSize: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20.0) {
+            HeaderView("LazyHGrid",
+                       subtitle: "Fixed Height",
+                       desc: "Give items in the LazyHGrid a fixed height using the GridItem. This will effectively change the height for the entire row.")
+            
+            Text("Fixed Heights: 50, 50, 20, 200")
+            
+            let rows = [GridItem(GridItem.Size.fixed(50)),
+                        GridItem(.fixed(50)),
+                        GridItem(.fixed(20)),
+                        GridItem(.fixed(200))]
+            
+            LazyHGrid(rows: rows) {
+                ForEach(1..<11) { item in
+                    Image(systemName: "\(item).circle")
+                }
+                Image(systemName: "arrow.right.circle")
+            }
+            .font(.largeTitle)
+        }
+        .font(.title)
     }
 }
 

254049c8449f9463d940d7bc4702b2e4a62bf5ad
Alex <oleksii.pidhornyi@gmail.com>
2025-03-25 16:50:00 +0100

    extend lazy h grid pinned views

--- SwiftUIViewsMastery/SwiftUI/LayoutViews/LazyHGrid/LazyHGridPinnedViews.swift
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/LazyHGrid/LazyHGridPinnedViews.swift
@@ -8,8 +8,32 @@
 import SwiftUI
 
 struct LazyHGridPinnedViews: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    @State private var teams = Data.getTeams()
+    var body: some View {        VStack(spacing: 15) {
+        HeaderView("LazyHGrid",
+                   subtitle: "Pinned Views",
+                   desc: "LazyHGrids can also have section headers and section footers that can be pinned so they only scroll when the next header/footer comes.",
+                   back: .yellow)
+        let rows = [GridItem(.adaptive(minimum: 150, maximum: 300))]
+        ScrollView(.horizontal) {
+            LazyHGrid(rows: rows, pinnedViews: [.sectionHeaders, .sectionFooters]) {
+                ForEach(teams) { team in
+                    Section {
+                        ForEach(team.people) { person in
+                            Image("\(person.imageName)")
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                        }
+                    } header: {
+                        TeamHeaderHGridView(team: team)
+                    } footer: {
+                        TeamFooterHGridView(team: team)
+                    }
+                }
+            }
+        }
+    }
+    .font(.title)
     }
 }
 

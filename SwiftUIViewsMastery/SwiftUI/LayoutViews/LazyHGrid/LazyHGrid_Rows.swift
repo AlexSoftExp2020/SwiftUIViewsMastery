6eb3f42da498f9a656416686e4ba236c3263ddcc
Alex <oleksii.pidhornyi@gmail.com>
2025-03-25 14:20:29 +0100

    extend lazy h grid rows

--- SwiftUIViewsMastery/SwiftUI/LayoutViews/LazyHGrid/LazyHGrid_Rows.swift
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/LazyHGrid/LazyHGrid_Rows.swift
@@ -9,7 +9,36 @@ import SwiftUI
 
 struct LazyHGrid_Rows: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20.0) {
+            HeaderView("LazyHGrid",
+                       subtitle: "Rows",
+                       desc: "You can specify the number of rows with an array of GridItems.")
+            
+            Text("Two Rows (2 GridItems):")
+            
+            let twoRows = [GridItem(), GridItem()]
+            
+            LazyHGrid(rows: twoRows) {
+                ForEach(1..<11) { item in
+                    Image(systemName: "\(item).circle")
+                }
+                Image(systemName: "arrow.right.circle")
+            }
+            .font(.largeTitle)
+            
+            Text("Four Rows (4 GridItems):")
+            
+            let fourRows = [GridItem(), GridItem(), GridItem(), GridItem()]
+            
+            LazyHGrid(rows: fourRows) {
+                ForEach(1..<11) { item in
+                    Image(systemName: "\(item).circle")
+                }
+                Image(systemName: "arrow.right.circle")
+            }
+            .font(.largeTitle)
+        }
+        .font(.title)
     }
 }
 

b8290a6132efaf291d8cfbc37d16d030412b2f25
Alex <oleksii.pidhornyi@gmail.com>
2025-03-25 14:06:27 +0100

    extend lazy h grid column spacing

--- SwiftUIViewsMastery/SwiftUI/LayoutViews/LazyHGrid/LazyHGrid_ColumnSpacing.swift
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/LazyHGrid/LazyHGrid_ColumnSpacing.swift
@@ -9,7 +9,34 @@ import SwiftUI
 
 struct LazyHGrid_ColumnSpacing: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20.0) {
+            HeaderView("LazyHGrid",
+                       subtitle: "Column Spacing",
+                       desc: "Control the spacing between items in the LazyHGrid with the spacing modifier.")
+            
+            Text("Spacing: 0")
+            
+            let gridItems = [GridItem()]
+            
+            LazyHGrid(rows: gridItems, spacing: 0) {
+                ForEach(1..<6) { item in
+                    Image(systemName: "\(item).circle")
+                }
+                Image(systemName: "arrow.right.circle")
+            }
+            .font(.largeTitle)
+            
+            Text("Spacing: 20")
+            
+            LazyHGrid(rows: gridItems, spacing: 20) {
+                ForEach(1..<6) { item in
+                    Image(systemName: "\(item).circle")
+                }
+                Image(systemName: "arrow.right.circle")
+            }
+            .font(.largeTitle)
+        }
+        .font(.title)
     }
 }
 

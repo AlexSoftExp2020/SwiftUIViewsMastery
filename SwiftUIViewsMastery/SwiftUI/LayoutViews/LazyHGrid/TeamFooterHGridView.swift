c8d6c49098f0f95a38c8a936fabca2ee85d9750f
Alex <oleksii.pidhornyi@gmail.com>
2025-03-25 16:55:12 +0100

    add team footer h grid view

---
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/LazyHGrid/TeamFooterHGridView.swift
@@ -0,0 +1,18 @@
+//
+//  TeamFooterHGridView.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 25.03.2025.
+//
+
+import SwiftUI
+
+struct TeamFooterHGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TeamFooterHGridView()
+}

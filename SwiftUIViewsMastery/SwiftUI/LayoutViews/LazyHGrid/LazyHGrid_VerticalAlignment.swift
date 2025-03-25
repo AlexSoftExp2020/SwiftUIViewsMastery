a4cfc75d03507503f4b862257af879d9be228650
Alex <oleksii.pidhornyi@gmail.com>
2025-03-25 16:44:17 +0100

    extend lazy h grid vertical alignment

--- SwiftUIViewsMastery/SwiftUI/LayoutViews/LazyHGrid/LazyHGrid_VerticalAlignment.swift
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/LazyHGrid/LazyHGrid_VerticalAlignment.swift
@@ -9,7 +9,32 @@ import SwiftUI
 
 struct LazyHGrid_VerticalAlignment: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20.0) {
+            HeaderView("LazyHGrid",
+                       subtitle: "Vertical Alignment",
+                       desc: "You can control the vertical alignment of each row through the GridItems.")
+            
+            HStack(spacing: 20) {
+                VStack(alignment: .trailing, spacing: 20.0) {
+                    Text("Bottom")
+                    Text("Top")
+                    Text("Center")
+                }
+                
+                let rows = [GridItem(GridItem.Size.fixed(50), alignment: .bottom),
+                            GridItem(.fixed(50), alignment: .top),
+                            GridItem(.fixed(50), alignment: .center)]
+                
+                LazyHGrid(rows: rows) {
+                    ForEach(1..<11) { item in
+                        Image(systemName: "\(item).circle")
+                    }
+                    Image(systemName: "arrow.right.circle")
+                }
+                .font(.largeTitle)
+            }
+        }
+        .font(.title)
     }
 }
 

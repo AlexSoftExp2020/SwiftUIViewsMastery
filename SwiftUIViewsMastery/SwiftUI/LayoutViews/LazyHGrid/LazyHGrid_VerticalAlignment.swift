adaefccfeefdd1470c85c4815225981a2b2459e3
Alex <oleksii.pidhornyi@gmail.com>
2025-03-25 16:33:29 +0100

    add lazy h grid vertical alignment

---
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/LazyHGrid/LazyHGrid_VerticalAlignment.swift
@@ -0,0 +1,18 @@
+//
+//  LazyHGrid_VerticalAlignment.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 25.03.2025.
+//
+
+import SwiftUI
+
+struct LazyHGrid_VerticalAlignment: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LazyHGrid_VerticalAlignment()
+}

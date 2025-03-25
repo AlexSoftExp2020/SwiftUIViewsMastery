086af4045f1ae8c7cafba184f2579745a298f062
Alex <oleksii.pidhornyi@gmail.com>
2025-03-25 14:24:04 +0100

    add lazy h grid fixed size

---
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/LazyHGrid/LazyHGrid_FixedSize.swift
@@ -0,0 +1,18 @@
+//
+//  LazyHGrid_FixedSize.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 25.03.2025.
+//
+
+import SwiftUI
+
+struct LazyHGrid_FixedSize: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LazyHGrid_FixedSize()
+}

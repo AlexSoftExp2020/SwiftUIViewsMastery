2b909438991da0469ef263b660947d2a268941f1
Alex <oleksii.pidhornyi@gmail.com>
2025-03-25 16:51:26 +0100

    add team header h grid view

---
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/LazyHGrid/TeamHeaderHGridView.swift
@@ -0,0 +1,18 @@
+//
+//  TeamHeaderHGridView.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 25.03.2025.
+//
+
+import SwiftUI
+
+struct TeamHeaderHGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TeamHeaderHGridView()
+}

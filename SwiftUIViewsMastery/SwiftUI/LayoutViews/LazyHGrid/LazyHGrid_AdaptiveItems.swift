6fab7f744be4119d0a7e19737db845d7759762a9
Alex <oleksii.pidhornyi@gmail.com>
2025-03-25 15:23:08 +0100

    extend lazy h grid adaptive items

--- SwiftUIViewsMastery/SwiftUI/LayoutViews/LazyHGrid/LazyHGrid_AdaptiveItems.swift
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/LazyHGrid/LazyHGrid_AdaptiveItems.swift
@@ -9,7 +9,24 @@ import SwiftUI
 
 struct LazyHGrid_AdaptiveItems: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 15) {
+            HeaderView("LazyHGrid",
+                       subtitle: "Adaptive Height",
+                       desc: "Let items in the LazyHGrid decide how many rows they need through the GridItem. This example uses only one GridItem.")
+            
+            Text("Height Range: 20 to 60")
+            
+            let rows = [GridItem(.adaptive(minimum: 20, maximum: 60))]
+            
+            LazyHGrid(rows: rows) {
+                ForEach(1..<21) { item in
+                    Color.green.frame(width: 50)
+                }
+                Image(systemName: "arrow.right.circle")
+            }
+            .padding(.bottom)
+        }
+        .font(.title)
     }
 }
 

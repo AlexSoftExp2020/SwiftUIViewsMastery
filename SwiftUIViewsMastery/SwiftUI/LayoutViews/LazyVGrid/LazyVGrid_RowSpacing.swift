38e93d63d4a46ae341c32516e7ae50b3775c0ae8
Alex <oleksii.pidhornyi@gmail.com>
2025-03-26 19:59:48 +0100

    add lazy v grid row spacing

---
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/LazyVGrid/LazyVGrid_RowSpacing.swift
@@ -0,0 +1,18 @@
+//
+//  LazyVGrid_RowSpacing.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 26.03.2025.
+//
+
+import SwiftUI
+
+struct LazyVGrid_RowSpacing: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LazyVGrid_RowSpacing()
+}

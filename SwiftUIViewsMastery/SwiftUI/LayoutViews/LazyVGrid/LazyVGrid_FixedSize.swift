eda3fe92915870cfbccf243d45460c7f1111157a
Alex <oleksii.pidhornyi@gmail.com>
2025-03-26 20:40:32 +0100

    extend lazy v grid fixed size

--- SwiftUIViewsMastery/SwiftUI/LayoutViews/LazyVGrid/LazyVGrid_FixedSize.swift
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/LazyVGrid/LazyVGrid_FixedSize.swift
@@ -9,7 +9,25 @@ import SwiftUI
 
 struct LazyVGrid_FixedSize: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20.0) {
+            HeaderView("LazyVGrid",
+                       subtitle: "Fixed Width",
+                       desc: "Give items in the LazyVGrid a fixed width using the GridItem. This will effectively change the width for the entire column.")
+            Text("Fixed Cols: 50, 50, 50, 0, 150")
+            let columns = [GridItem(GridItem.Size.fixed(50)),
+                           GridItem(.fixed(50)),
+                           GridItem(.fixed(50)),
+                           GridItem(.fixed(0)),
+                           GridItem(.fixed(150))]
+            LazyVGrid(columns: columns) {
+                ForEach(1..<11) { item in
+                    Image(systemName: "\(item).circle")
+                }
+                Image(systemName: "arrow.down.circle")
+            }
+            .font(.largeTitle)
+        }
+        .font(.title)
     }
 }
 

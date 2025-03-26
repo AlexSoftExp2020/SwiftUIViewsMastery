c82e92244f9cf3cc66515bdb07b16b0d9a6550f1
Alex <oleksii.pidhornyi@gmail.com>
2025-03-26 23:27:36 +0100

    extend lazy v grid columns spacing

--- SwiftUIViewsMastery/SwiftUI/LayoutViews/LazyVGrid/LazyVGrid_ColumnSpacing.swift
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/LazyVGrid/LazyVGrid_ColumnSpacing.swift
@@ -9,7 +9,34 @@ import SwiftUI
 
 struct LazyVGrid_ColumnSpacing: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            HeaderView("LazyVGrid",
+                       subtitle: "Column Spacing",
+                       desc: "Specify the amount of spacing to the right of each column by using the GridItem's spacing parameter. This can be used with any GridItem.Size option: fixed, flexible, or adaptive.")
+            
+            Text("Column Spacing")
+            HStack {
+                Text("40")
+                Spacer()
+                Text("2").padding(.leading, 40)
+                Spacer()
+                Text("20")
+                Spacer()
+                Text("None")
+            }.padding(.horizontal)
+            
+            let columns = [GridItem(spacing: 40),
+                           GridItem(spacing: 2),
+                           GridItem(spacing: 20),
+                           GridItem()]
+            
+            LazyVGrid(columns: columns) {
+                ForEach(1..<21) { item in
+                    Color.green.frame(height: 50)
+                }
+            }
+        }
+        .font(.title)
     }
 }
 

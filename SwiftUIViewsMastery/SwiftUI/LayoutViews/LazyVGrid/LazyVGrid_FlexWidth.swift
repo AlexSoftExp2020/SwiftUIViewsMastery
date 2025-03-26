7c878af8b16fe16d345cb2d043fdf973f90389f3
Alex <oleksii.pidhornyi@gmail.com>
2025-03-26 22:53:17 +0100

    add lazy v grid flex width

---
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/LazyVGrid/LazyVGrid_FlexWidth.swift
@@ -0,0 +1,18 @@
+//
+//  LazyVGrid_FlexWidth.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 26.03.2025.
+//
+
+import SwiftUI
+
+struct LazyVGrid_FlexWidth: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LazyVGrid_FlexWidth()
+}

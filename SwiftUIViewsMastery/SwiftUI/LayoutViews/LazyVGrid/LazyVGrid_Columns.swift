55059cb4d08fa091c99df6f85433b1d88c0ba950
Alex <oleksii.pidhornyi@gmail.com>
2025-03-26 20:20:11 +0100

    extend lazy v grid columns

--- SwiftUIViewsMastery/SwiftUI/LayoutViews/LazyVGrid/LazyVGrid_Columns.swift
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/LazyVGrid/LazyVGrid_Columns.swift
@@ -9,7 +9,36 @@ import SwiftUI
 
 struct LazyVGrid_Columns: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20.0) {
+            HeaderView("LazyVGrid",
+                       subtitle: "Columns",
+                       desc: "You can specify the number of columns with an array of GridItems.")
+            
+            Text("Three Columns (3 GridItems)")
+            
+            let threeColumns = [GridItem(), GridItem(), GridItem()]
+            
+            LazyVGrid(columns: threeColumns) {
+                ForEach(1..<10) { item in
+                    Image(systemName: "\(item).circle")
+                }
+                Image(systemName: "arrow.down.circle")
+            }
+            .font(.largeTitle)
+            
+            Text("Five Columns (5 GridItems):")
+            
+            let fiveColumns = [GridItem(), GridItem(), GridItem(), GridItem(), GridItem()]
+            
+            LazyVGrid(columns: fiveColumns) {
+                ForEach(1..<11) { item in
+                    Image(systemName: "\(item).circle")
+                }
+                Image(systemName: "arrow.down.circle")
+            }
+            .font(.largeTitle)
+        }
+        .font(.title)
     }
 }
 

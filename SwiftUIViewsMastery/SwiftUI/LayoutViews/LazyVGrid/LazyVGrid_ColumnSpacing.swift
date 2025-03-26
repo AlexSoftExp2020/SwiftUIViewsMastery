c6b289c7d7440cc063ea89c5e3c6b0d5fedff213
Alex <oleksii.pidhornyi@gmail.com>
2025-03-26 23:17:18 +0100

    add lazy v grid column spacing

---
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/LazyVGrid/LazyVGrid_ColumnSpacing.swift
@@ -0,0 +1,18 @@
+//
+//  LazyVGrid_ColumnSpacing.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 26.03.2025.
+//
+
+import SwiftUI
+
+struct LazyVGrid_ColumnSpacing: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LazyVGrid_ColumnSpacing()
+}

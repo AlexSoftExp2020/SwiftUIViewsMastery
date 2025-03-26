43ff7e4695632f074ce999f29ee1edfb7636da67
Alex <oleksii.pidhornyi@gmail.com>
2025-03-26 23:15:49 +0100

    extend lazy v grid  adaptive width

--- SwiftUIViewsMastery/SwiftUI/LayoutViews/LazyVGrid/LazyVGrid_AdaptiveWidth.swift
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/LazyVGrid/LazyVGrid_AdaptiveWidth.swift
@@ -9,7 +9,25 @@ import SwiftUI
 
 struct LazyVGrid_AdaptiveWidth: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 15) {
+            HeaderView("LazyVGrid",
+                       subtitle: "Adaptive Width",
+                       desc: "Let items in the LazyVGrid decide how many columns they need through the GridItem. This example uses only one GridItem.")
+            
+            Text("Width Range: 20 to 60")
+            
+            let columns  = [GridItem(.adaptive(minimum: 20, maximum: 60))]
+            
+            LazyVGrid(columns: columns) {
+                ForEach(1..<41) { item in
+                    Color.green
+                        .frame(height: 50)
+                }
+                Image(systemName: "arrow.down.circle")
+            }
+            .padding(.bottom)
+        }
+        .font(.title)
     }
 }
 

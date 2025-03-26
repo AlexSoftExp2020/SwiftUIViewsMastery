d81530c54c2345ea339a67c11f0f628cd1c4e163
Alex <oleksii.pidhornyi@gmail.com>
2025-03-26 23:08:36 +0100

    add lazy v grid adaptive width

---
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/LazyVGrid/LazyVGrid_AdaptiveWidth.swift
@@ -0,0 +1,18 @@
+//
+//  LazyVGrid_AdaptiveWidth.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 26.03.2025.
+//
+
+import SwiftUI
+
+struct LazyVGrid_AdaptiveWidth: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LazyVGrid_AdaptiveWidth()
+}

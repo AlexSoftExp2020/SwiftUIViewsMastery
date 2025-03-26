a4eddcfcbb4d738744eed8ebc4fbe3b81120541f
Alex <oleksii.pidhornyi@gmail.com>
2025-03-26 20:26:45 +0100

    add lazy v grid fixed size

---
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/LazyVGrid/LazyVGrid_FixedSize.swift
@@ -0,0 +1,18 @@
+//
+//  LazyVGrid_FixedSize.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 26.03.2025.
+//
+
+import SwiftUI
+
+struct LazyVGrid_FixedSize: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LazyVGrid_FixedSize()
+}

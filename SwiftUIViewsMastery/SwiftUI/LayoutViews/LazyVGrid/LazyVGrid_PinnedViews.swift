80834011e338a891a72a0b4f47054418b082b8aa
Alex <oleksii.pidhornyi@gmail.com>
2025-03-27 21:45:01 +0100

    add lazy v grid pinned views

---
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/LazyVGrid/LazyVGrid_PinnedViews.swift
@@ -0,0 +1,18 @@
+//
+//  LazyVGrid_PinnedViews.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 27.03.2025.
+//
+
+import SwiftUI
+
+struct LazyVGrid_PinnedViews: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LazyVGrid_PinnedViews()
+}

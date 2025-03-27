9cb94c23ef91252f0afd0cb798ccd4a2f3569abb
Alex <oleksii.pidhornyi@gmail.com>
2025-03-27 21:52:53 +0100

    extend team header v grid view

--- SwiftUIViewsMastery/SwiftUI/LayoutViews/LazyVGrid/TeamHeaderVGridView.swift
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/LazyVGrid/TeamHeaderVGridView.swift
@@ -8,8 +8,20 @@
 import SwiftUI
 
 struct TeamHeaderVGridView: View {
+    var team: Data.Team
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(spacing: 8) {
+            Spacer()
+            Text("Team")
+                .font(.largeTitle)
+            Image(systemName: team.imageName)
+                .font(.largeTitle)
+            Spacer()
+        }
+        .frame(height: 75)
+        .background(Rectangle()
+            .fill(Color.yellow)
+            .opacity(0.9))
     }
 }
 

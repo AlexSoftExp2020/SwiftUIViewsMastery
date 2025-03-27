34cb111199a11f7ec6f154ceccd6fc8a9fec425b
Alex <oleksii.pidhornyi@gmail.com>
2025-03-27 21:58:01 +0100

    extend team footer v grid view

--- SwiftUIViewsMastery/SwiftUI/LayoutViews/LazyVGrid/TeamFooterVGridView.swift
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/LazyVGrid/TeamFooterVGridView.swift
@@ -8,8 +8,19 @@
 import SwiftUI
 
 struct TeamFooterVGridView: View {
+    var team: Data.Team
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(spacing: 0) {
+            Spacer()
+            Text("Team Total: ")
+                .font(.title)
+            Text("\(team.people.count)")
+                .font(.title)
+                .fontWeight(.bold)
+            Spacer()
+        }
+        .frame(height: 60)
+        .background(Rectangle().fill(Color.yellow).opacity(0.9))
     }
 }
 

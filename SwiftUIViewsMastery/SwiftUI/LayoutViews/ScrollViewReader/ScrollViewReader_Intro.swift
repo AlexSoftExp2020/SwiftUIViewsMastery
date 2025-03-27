c313b7104e078f22192474c6798105590c9eda0b
Alex <oleksii.pidhornyi@gmail.com>
2025-03-27 22:01:15 +0100

    add scroll view reader intro

---
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/ScrollViewReader/ScrollViewReader_Intro.swift
@@ -0,0 +1,18 @@
+//
+//  ScrollViewReader_Intro.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 27.03.2025.
+//
+
+import SwiftUI
+
+struct ScrollViewReader_Intro: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScrollViewReader_Intro()
+}

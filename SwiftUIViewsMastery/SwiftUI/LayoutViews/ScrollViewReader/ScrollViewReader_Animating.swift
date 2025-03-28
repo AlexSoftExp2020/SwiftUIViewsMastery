d17d73d6b3fb5cafc1c39ed4d4f67f554dfe39dc
Alex <oleksii.pidhornyi@gmail.com>
2025-03-28 23:36:32 +0100

    add scroll view reader animating

---
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/ScrollViewReader/ScrollViewReader_Animating.swift
@@ -0,0 +1,18 @@
+//
+//  ScrollViewReader_Animating.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 28.03.2025.
+//
+
+import SwiftUI
+
+struct ScrollViewReader_Animating: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScrollViewReader_Animating()
+}

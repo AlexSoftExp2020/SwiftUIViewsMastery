c6d886577bda4eb2585a2548e5e94c21f6bdc36c
Alex <oleksii.pidhornyi@gmail.com>
2025-03-28 02:34:39 +0100

    extend scroll view reader anchor

--- SwiftUIViewsMastery/SwiftUI/LayoutViews/ScrollViewReader/ScrollViewReader_Anchor.swift
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/ScrollViewReader/ScrollViewReader_Anchor.swift
@@ -9,7 +9,37 @@ import SwiftUI
 
 struct ScrollViewReader_Anchor: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20.0) {
+            HeaderView("ScrollViewReader",
+                       subtitle: "Anchor",
+                       desc: "You can control the position of where you want the view to go when calling scrollTo.")
+            
+            ScrollViewReader { scrollViewProxy in
+                Button("Scroll 25 To Top") {
+                    scrollViewProxy.scrollTo(25, anchor: .top)
+                }
+                
+                HStack(spacing: 20) {
+                    Button("Scroll 25 To Center") {
+                        scrollViewProxy.scrollTo(25, anchor: .center)
+                    }
+                    
+                    ScrollView {
+                        ForEach(1..<51) { index in
+                            Image(systemName: "\(index).square.fill")
+                                .font(.largeTitle)
+                                .frame(height: 70)
+                                .foregroundStyle(index == 25 ? .red : .gray)
+                                .id(index)
+                        }
+                    }
+                }
+                Button("Scroll 25 To Bottom") {
+                    scrollViewProxy.scrollTo(25, anchor: .bottom)
+                }
+            }
+        }
+        .font(.title)
     }
 }
 

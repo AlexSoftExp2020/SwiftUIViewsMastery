da3b1139128e4b1f390c74fc98086eda03b841ee
Alex <oleksii.pidhornyi@gmail.com>
2025-03-28 02:04:22 +0100

    add scroll view reader anchor

---
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/ScrollViewReader/ScrollViewReader_Anchor.swift
@@ -0,0 +1,18 @@
+//
+//  ScrollViewReader_Anchor.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 28.03.2025.
+//
+
+import SwiftUI
+
+struct ScrollViewReader_Anchor: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScrollViewReader_Anchor()
+}

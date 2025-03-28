0cb2f4c516c098e3cc45ecdeb19145344b6e818b
Alex <oleksii.pidhornyi@gmail.com>
2025-03-28 23:54:48 +0100

    extend scroll view reader animating

--- SwiftUIViewsMastery/SwiftUI/LayoutViews/ScrollViewReader/ScrollViewReader_Animating.swift
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/ScrollViewReader/ScrollViewReader_Animating.swift
@@ -9,7 +9,22 @@ import SwiftUI
 
 struct ScrollViewReader_Animating: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20.0) {
+            HeaderView("ScrollViewReader",
+                       subtitle: "Animation",
+                       desc: "You can animate the scrolling by putting withAnimation around the scrollTo function.")
+            
+            ScrollViewReader { scrollViewProxy in
+                Button("Animate Scrolling To Bottom") {
+                    withAnimation { scrollViewProxy.scrollTo(50) }
+                }
+                ScrollView(showsIndicators: true) {
+                    ForEach(1..<51) { index in
+                        //TODO: getimage
+                    }
+                }
+            }
+        }
     }
 }
 

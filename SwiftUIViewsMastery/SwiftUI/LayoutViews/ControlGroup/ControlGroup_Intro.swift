35d4c25b015e60687af47c67c499b17407d0968a
Alex <oleksii.pidhornyi@gmail.com>
2025-03-29 01:34:09 +0100

    extend control group intro

--- SwiftUIViewsMastery/SwiftUI/LayoutViews/ControlGroup/ControlGroup_Intro.swift
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/ControlGroup/ControlGroup_Intro.swift
@@ -9,7 +9,28 @@ import SwiftUI
 
 struct ControlGroup_Intro: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20.0) {
+            HeaderView("ControlGroup",
+                       subtitle: "Introduction",
+                       desc: "Use a ControlGroup view to group up related controls.")
+            
+            ControlGroup {
+                Button("Hello") { }
+                Button(action: { }) {
+                    Image(systemName: "gearshape.fill")
+                }
+            }
+            
+            DescView("You can change the default style to 'navigation':")
+            ControlGroup {
+                Button("Hello!") { }
+                Button(action: { }) {
+                    Image(systemName: "gearshape.fill")
+                }
+            }
+            .controlGroupStyle(.navigation)
+        }
+        .font(.title)
     }
 }
 

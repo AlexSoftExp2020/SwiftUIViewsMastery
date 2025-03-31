2919f108976dfa44fc0bbb4acf020831939b9c43
Alex <oleksii.pidhornyi@gmail.com>
2025-03-31 23:40:52 +0200

    add color info

---
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/Table/ColorInfo.swift
@@ -0,0 +1,14 @@
+//
+//  ColorInfo.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 31.03.2025.
+//
+
+import SwiftUICore
+
+struct ColorInfo: Identifiable {
+    let id = UUID()
+    var name = ""
+    var desc = Color.clear
+}

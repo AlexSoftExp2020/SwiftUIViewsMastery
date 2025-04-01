b6290e65fe0ba8161f929386b42268cc92992f9c
Alex <oleksii.pidhornyi@gmail.com>
2025-04-01 18:42:41 +0200

    add button with borders

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Button/Button_WithBorders.swift
@@ -0,0 +1,18 @@
+//
+//  Button_WithBorders.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 01.04.2025.
+//
+
+import SwiftUI
+
+struct Button_WithBorders: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Button_WithBorders()
+}

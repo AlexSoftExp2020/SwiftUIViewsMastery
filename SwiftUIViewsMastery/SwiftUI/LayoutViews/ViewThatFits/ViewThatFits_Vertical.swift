104d214fb1b77451fb292fa01d38a68aed4cff32
Alex <oleksii.pidhornyi@gmail.com>
2025-04-01 15:43:46 +0200

    add view that fits vertical

---
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/ViewThatFits/ViewThatFits_Vertical.swift
@@ -0,0 +1,18 @@
+//
+//  ViewThatFits_Vertical.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 01.04.2025.
+//
+
+import SwiftUI
+
+struct ViewThatFits_Vertical: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ViewThatFits_Vertical()
+}

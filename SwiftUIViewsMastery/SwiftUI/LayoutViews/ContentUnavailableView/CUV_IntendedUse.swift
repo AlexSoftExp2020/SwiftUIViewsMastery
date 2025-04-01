25d1eef358ee20df02fb70a0b78e46ccfc8f20f0
Alex <oleksii.pidhornyi@gmail.com>
2025-04-01 16:44:55 +0200

    extend cuv intended use

--- SwiftUIViewsMastery/SwiftUI/LayoutViews/ContentUnavailableView/CUV_IntendedUse.swift
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/ContentUnavailableView/CUV_IntendedUse.swift
@@ -8,8 +8,29 @@
 import SwiftUI
 
 struct CUV_IntendedUse: View {
+    @State private var tags: [String] = []
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        if tags.isEmpty {
+            ContentUnavailableView {
+                Label("No Tags", systemImage: "tag.fill")
+            } description: {
+                Text("You don't have any tags yet.\nAdd a new tag today to get started!")
+            } actions: {
+                Button {
+                    tags.append("Switzerland")
+                } label: {
+                    Text("Add Tag")
+                }
+                .buttonStyle(.borderedProminent)
+                .controlSize(.regular)
+            }
+
+        } else {
+            List(tags, id: \.self) { tag in
+                Text(tag)
+            }
+        }
     }
 }
 

07da3934535d488509f062581184e0c7495a7541
Alex <oleksii.pidhornyi@gmail.com>
2025-04-01 22:52:53 +0200

    add button disabled

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Button/Button_Disabled.swift
@@ -0,0 +1,18 @@
+//
+//  Button_Disabled.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 01.04.2025.
+//
+
+import SwiftUI
+
+struct Button_Disabled: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Button_Disabled()
+}

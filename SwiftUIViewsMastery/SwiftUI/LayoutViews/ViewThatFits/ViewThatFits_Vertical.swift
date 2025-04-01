e8da4c06d09412dcf3158706d3ffbaeaac25e3ff
Alex <oleksii.pidhornyi@gmail.com>
2025-04-01 15:48:13 +0200

    extend view that fits vertical

--- SwiftUIViewsMastery/SwiftUI/LayoutViews/ViewThatFits/ViewThatFits_Vertical.swift
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/ViewThatFits/ViewThatFits_Vertical.swift
@@ -9,7 +9,19 @@ import SwiftUI
 
 struct ViewThatFits_Vertical: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ViewThatFits(in: .vertical) {
+            RoundedRectangle(cornerRadius: 16)
+                .fill(Color.purple.opacity(0.7))
+                .overlay(Text("For Portarait"))
+                .frame(width: 200, height: 500)
+            
+            RoundedRectangle(cornerRadius: 16)
+                .fill(Color.blue.opacity(0.7))
+                .overlay(Text("For Landscape"))
+                .frame(width: 400, height: 100)
+            
+            Text("No views fit")
+        }
     }
 }
 

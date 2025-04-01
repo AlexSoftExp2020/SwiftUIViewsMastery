d0eed2e07bbfde463ceeb17d10e1065908fc2a85
Alex <oleksii.pidhornyi@gmail.com>
2025-04-01 16:31:17 +0200

    add cuv with actions

---
+++ SwiftUIViewsMastery/SwiftUI/LayoutViews/ContentUnavailableView/CUV_WithActions.swift
@@ -0,0 +1,18 @@
+//
+//  CUV_WithActions.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 01.04.2025.
+//
+
+import SwiftUI
+
+struct CUV_WithActions: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CUV_WithActions()
+}

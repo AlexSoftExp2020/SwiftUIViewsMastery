5dab920116aebc8cd230f649c5d409d0d8bc456e
Alex <oleksii.pidhornyi@gmail.com>
2025-04-01 16:56:40 +0200

    add regular button

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Button/RegularButton.swift
@@ -0,0 +1,18 @@
+//
+//  RegularButton.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 01.04.2025.
+//
+
+import SwiftUI
+
+struct RegularButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RegularButton()
+}

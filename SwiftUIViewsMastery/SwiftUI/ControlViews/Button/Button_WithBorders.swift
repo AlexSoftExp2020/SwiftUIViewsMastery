a326f1593d1333421c8a11e079135197ef874642
Alex <oleksii.pidhornyi@gmail.com>
2025-04-01 19:24:18 +0200

    extend button with borders

--- SwiftUIViewsMastery/SwiftUI/ControlViews/Button/Button_WithBorders.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Button/Button_WithBorders.swift
@@ -9,7 +9,22 @@ import SwiftUI
 
 struct Button_WithBorders: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 60) {
+            Button(action: {}) {
+                Text("Square Border Button")
+                    .padding()
+                    .border(Color.purple, width: 2)
+            }
+            
+            Button(action: {}) {
+                Text("Border Button").padding()
+                    .background {
+                    RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color.purple, lineWidth: 2)
+                }
+            }
+        }
+        .font(.title)
     }
 }
 

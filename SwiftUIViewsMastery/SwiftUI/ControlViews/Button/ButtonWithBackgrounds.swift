309482861ad1389b88ebb708d1fba45170343968
Alex <oleksii.pidhornyi@gmail.com>
2025-04-01 18:01:02 +0200

    extend button with backgrounds

--- SwiftUIViewsMastery/SwiftUI/ControlViews/Button/ButtonWithBackgrounds.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Button/ButtonWithBackgrounds.swift
@@ -9,7 +9,34 @@ import SwiftUI
 
 struct ButtonWithBackgrounds: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 60) {
+            Button(action: {}) {
+                Text("Solid Button")
+                    .padding()
+                    .foregroundStyle(.white)
+                    .background(Color.purple)
+                    .clipShape(RoundedRectangle(cornerRadius: 16))
+            }
+            
+            Button(action: {}) {
+                Text("Button With Shadow")
+                    .padding(12)
+                    .foregroundStyle(.white)
+                    .background(Color.purple)
+                    .shadow(color: Color.purple, radius: 20, y: 5)
+            }
+            
+            Button(action: {}) {
+                Text("Button With Rounded Ends")
+                    .padding(EdgeInsets(top: 12,
+                                        leading: 20,
+                                        bottom: 12,
+                                        trailing: 20))
+                    .foregroundStyle(.white)
+                    .background(Color.purple, in: Capsule())
+            }
+        }
+        .font(.title)
     }
 }
 

77245533047084dd92ca7c5e1d1231cf491d3414
Alex <oleksii.pidhornyi@gmail.com>
2025-04-01 22:48:46 +0200

    extend button button border shape

--- SwiftUIViewsMastery/SwiftUI/ControlViews/Button/Button_ButtonBorderShape.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Button/Button_ButtonBorderShape.swift
@@ -9,7 +9,27 @@ import SwiftUI
 
 struct Button_ButtonBorderShape: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 80.0) {
+            Button("Automatic") { }
+                .buttonBorderShape(.automatic)
+            
+            Button("Capsule") { }
+                .buttonBorderShape(.capsule)
+            
+            Button("Capsule") { }
+                .buttonBorderShape(.capsule)
+                .buttonStyle(.borderedProminent)
+            
+            Button("RoundedRectangle") { }
+                .buttonBorderShape(.roundedRectangle)
+            
+            Button("Set Radius") { }
+                .buttonBorderShape(.roundedRectangle(radius: 24))
+        }
+        .buttonStyle(.bordered)
+        .controlSize(.large)
+        .font(.title)
+        .tint(.purple)
     }
 }
 

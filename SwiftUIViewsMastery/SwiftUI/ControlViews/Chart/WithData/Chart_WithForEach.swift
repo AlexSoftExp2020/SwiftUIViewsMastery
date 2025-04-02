2d0f1d7840b7570f4aa116f62c423a876daf6efc
Alex <oleksii.pidhornyi@gmail.com>
2025-04-02 19:53:15 +0200

    add chart with for each

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Chart/WithData/Chart_WithForEach.swift
@@ -0,0 +1,18 @@
+//
+//  Chart_WithForEach.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 02.04.2025.
+//
+
+import SwiftUI
+
+struct Chart_WithForEach: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Chart_WithForEach()
+}

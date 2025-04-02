51c325e35d3b2dc136ec1cc8f65513fe1defe885
Alex <oleksii.pidhornyi@gmail.com>
2025-04-02 23:43:27 +0200

    add chart multiple marks

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Chart/Chart_MultipleMarks.swift
@@ -0,0 +1,18 @@
+//
+//  Chart_MultipleMarks.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 02.04.2025.
+//
+
+import SwiftUI
+
+struct Chart_MultipleMarks: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Chart_MultipleMarks()
+}

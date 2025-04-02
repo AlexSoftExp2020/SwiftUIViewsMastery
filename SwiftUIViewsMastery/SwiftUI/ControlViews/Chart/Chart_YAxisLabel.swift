0445f478cf42cc2255573060197b7ed744243834
Alex <oleksii.pidhornyi@gmail.com>
2025-04-02 23:26:15 +0200

    add chart y axis labes

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Chart/Chart_YAxisLabel.swift
@@ -0,0 +1,18 @@
+//
+//  Chart_YAxisLabel.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 02.04.2025.
+//
+
+import SwiftUI
+
+struct Chart_YAxisLabel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Chart_YAxisLabel()
+}

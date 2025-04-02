193c3b4bc960addb93adeb9dd958cff81fb42193
Alex <oleksii.pidhornyi@gmail.com>
2025-04-02 20:56:07 +0200

    add chart rule mark annotation

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Chart/Chart_RuleMark_Annotation.swift
@@ -0,0 +1,18 @@
+//
+//  Chart_RuleMark_Annotation.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 02.04.2025.
+//
+
+import SwiftUI
+
+struct Chart_RuleMark_Annotation: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Chart_RuleMark_Annotation()
+}

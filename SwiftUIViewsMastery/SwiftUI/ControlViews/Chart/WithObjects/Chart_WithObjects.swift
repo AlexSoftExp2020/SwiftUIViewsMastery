9567f32a427c421577d84f211c4b298688cd264e
Alex <oleksii.pidhornyi@gmail.com>
2025-04-02 20:19:23 +0200

    extend chart with objects

--- SwiftUIViewsMastery/SwiftUI/ControlViews/Chart/WithObjects/Chart_WithObjects.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Chart/WithObjects/Chart_WithObjects.swift
@@ -5,4 +5,20 @@
 //  Created by Oleksii on 02.04.2025.
 //
 
-import Foundation
+import SwiftUI
+import Charts
+
+struct Chart_WithObjects: View {
+    @State private var plots = PlotInfo.fetchData()
+    
+    var body: some View {
+        Chart(plots) { plot in
+            BarMark(x: .value("Labels", plot.x),
+                    y: .value("Values", plot.y))
+        }
+    }
+}
+
+#Preview {
+    Chart_WithObjects()
+}

7d2ea4aad97f904a2c0be65fc2ee17b40d20810a
Alex <oleksii.pidhornyi@gmail.com>
2025-04-02 23:52:28 +0200

    extend chart multiple marks

--- SwiftUIViewsMastery/SwiftUI/ControlViews/Chart/Chart_MultipleMarks.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Chart/Chart_MultipleMarks.swift
@@ -6,10 +6,25 @@
 //
 
 import SwiftUI
+import Charts
 
 struct Chart_MultipleMarks: View {
+    @State private var plots = PlotInfo.fetchData()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Chart(plots) { plot in
+            LineMark(x: .value("Labels", plot.x),
+                     y: .value("Values", plot.y))
+            .foregroundStyle(Color.red)
+            
+            AreaMark(x: .value("Labels", plot.x),
+                     y: .value("Values", plot.y))
+            .foregroundStyle(Color.orange.gradient.opacity(0.6))
+            
+            PointMark(x: .value("Labels", plot.x),
+                      y: .value("Values", plot.y))
+            
+        }
     }
 }
 

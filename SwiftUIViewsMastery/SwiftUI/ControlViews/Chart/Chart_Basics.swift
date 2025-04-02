14778f52a9c6812c7809866140c2e15b46a1146a
Alex <oleksii.pidhornyi@gmail.com>
2025-04-02 19:00:28 +0200

    add chart basics

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Chart/Chart_Basics.swift
@@ -0,0 +1,19 @@
+//
+//  Chart_Basics.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 01.04.2025.
+//
+
+import Charts
+import SwiftUI
+
+struct Chart_Basics: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Chart_Basics()
+}

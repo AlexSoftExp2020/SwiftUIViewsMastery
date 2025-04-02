92ca0837053e8e629e96ac9cb06acf4774a63d21
Alex <oleksii.pidhornyi@gmail.com>
2025-04-02 19:35:13 +0200

    add chart with data

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Chart/WithData/Chart_WithData.swift
@@ -0,0 +1,19 @@
+//
+//  Chart_WithData.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 02.04.2025.
+//
+
+import SwiftUI
+import Charts
+
+struct Chart_WithData: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Chart_WithData()
+}

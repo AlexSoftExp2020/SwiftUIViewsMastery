4b091ff4ccb93553cd9a2b58a787f0809a172cbb
Alex <oleksii.pidhornyi@gmail.com>
2025-04-02 23:21:05 +0200

    extend chart x axis label

--- SwiftUIViewsMastery/SwiftUI/ControlViews/Chart/Chart_XAxisLabel.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Chart/Chart_XAxisLabel.swift
@@ -6,10 +6,33 @@
 //
 
 import SwiftUI
+import Charts
 
 struct Chart_XAxisLabel: View {
+    @State private var plots = PlotInfo.fetchData()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Chart(plots) { plot in
+            BarMark(x: .value("Labels", plot.x),
+                    y: .value("Values", plot.y))
+        }
+        .chartXAxisLabel("Quarters (a)")
+        .chartXAxisLabel {
+            Text("Quarters (b)")
+                .font(.title)
+                .foregroundStyle(.purple)
+                .padding(.top)
+        }
+        .chartXAxisLabel("Quarters (c)", position: .leading, alignment: .center, spacing: 8)
+        .chartXAxisLabel(position: .top, alignment: .center, spacing: 8) {
+            Text("Quarters (d)")
+                .font(.title3.weight(.bold))
+                .frame(width: 300)
+                .padding()
+                .background(Color.blue.opacity(0.2),
+                            in: RoundedRectangle(cornerRadius: 8))
+        }
+        .padding()
     }
 }
 

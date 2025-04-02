b17a7b11435eeb704eb935e871e792f2dc90541f
Alex <oleksii.pidhornyi@gmail.com>
2025-04-02 21:10:20 +0200

    extend chart rule mark annotation

--- SwiftUIViewsMastery/SwiftUI/ControlViews/Chart/Chart_RuleMark_Annotation.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Chart/Chart_RuleMark_Annotation.swift
@@ -6,10 +6,32 @@
 //
 
 import SwiftUI
+import Charts
 
 struct Chart_RuleMark_Annotation: View {
+    @State private var plots = PlotInfo.fetchData()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Chart(plots) { plot in
+            LineMark(x: .value("Labels", plot.x),
+                     y: .value("Values", plot.y))
+            
+            RuleMark(y: .value("Goal", 65))
+                .foregroundStyle(Color.orange)
+                .lineStyle(StrokeStyle(lineWidth: 2, dash: [8]))
+                .annotation {
+                    Text("Goal (b)")
+                }
+                .annotation(alignment: .leading) {
+                    Text("Goal (a)")
+                }
+                .annotation(position: .bottom,
+                            alignment: .trailing,
+                            spacing: 8) {
+                    Text("Goal (c)")
+                }
+        }
+        .padding()
     }
 }
 

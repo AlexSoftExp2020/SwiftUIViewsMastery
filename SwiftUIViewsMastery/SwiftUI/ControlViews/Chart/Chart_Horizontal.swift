bc8199c3c55f7992950aa227faebc7fb4dcbb1cb
Alex <oleksii.pidhornyi@gmail.com>
2025-04-02 19:23:20 +0200

    add chart horizontal

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Chart/Chart_Horizontal.swift
@@ -0,0 +1,18 @@
+//
+//  Chart_Horizontal.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 02.04.2025.
+//
+
+import SwiftUI
+
+struct Chart_Horizontal: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Chart_Horizontal()
+}

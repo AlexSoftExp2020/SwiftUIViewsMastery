558a1b5ffbea1ff624cb47a314898a1d9a434e7b
Alex <oleksii.pidhornyi@gmail.com>
2025-04-03 23:35:54 +0200

    extend chart multiple series of data

--- SwiftUIViewsMastery/SwiftUI/ControlViews/Chart/Chart_MultipleSeriesOfData.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Chart/Chart_MultipleSeriesOfData.swift
@@ -6,10 +6,33 @@
 //
 
 import SwiftUI
+import Charts
 
 struct Chart_MultipleSeriesOfData: View {
+    @State private var performanceReport = [
+        Performance(employee: "Rod", taskData: [
+            PlotInfo(x: "Quarter 1", y: 75),
+            PlotInfo(x: "Quarter 2", y: 25),
+            PlotInfo(x: "Quarter 3", y: 100),
+            PlotInfo(x: "Quarter 4", y: 50)
+        ]),
+        Performance(employee: "Lem", taskData: [
+            PlotInfo(x: "Quarter 1", y: 50),
+            PlotInfo(x: "Quarter 2", y: 75),
+            PlotInfo(x: "Quarter 3", y: 65),
+            PlotInfo(x: "Quarter 4", y: 70)
+        ])
+    ]
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Chart(performanceReport) { performances in
+            ForEach(performances.taskData) { datum in
+                LineMark(x: .value("", datum.x),
+                         y: .value("", datum.y))
+                .foregroundStyle(by: .value("", performances.employee))
+            }
+        }
+        .padding()
+        .dynamicTypeSize(.xLarge)
     }
 }
 

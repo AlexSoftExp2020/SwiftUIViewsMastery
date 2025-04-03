212b3be3579168169572123333d2e23632750ac3
Alex <oleksii.pidhornyi@gmail.com>
2025-04-03 19:56:19 +0200

    add chart comparing data

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Chart/Chart_ComparingData.swift
@@ -0,0 +1,18 @@
+//
+//  Chart_ComparingData.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 03.04.2025.
+//
+
+import SwiftUI
+
+struct Chart_ComparingData: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Chart_ComparingData()
+}

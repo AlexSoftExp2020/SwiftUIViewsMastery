378b789e403e462ea3c5337f080768c8aa8a8c69
Alex <oleksii.pidhornyi@gmail.com>
2025-04-04 20:09:45 +0200

    add chart groups

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Chart/Chart_Groups.swift
@@ -0,0 +1,18 @@
+//
+//  Chart_Groups.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 04.04.2025.
+//
+
+import SwiftUI
+
+struct Chart_Groups: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Chart_Groups()
+}

2b434beef3002ec453dfa5ebbd7659363a9658b6
Alex <oleksii.pidhornyi@gmail.com>
2025-04-04 21:42:28 +0200

    add chart linear mark customization

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Chart/Chart_LinearMarkCustomization.swift
@@ -0,0 +1,18 @@
+//
+//  Chart_LinearMarkCustomization.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 04.04.2025.
+//
+
+import SwiftUI
+
+struct Chart_LinearMarkCustomization: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Chart_LinearMarkCustomization()
+}

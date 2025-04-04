109c9db8555eddbcce2ed318ec8dc1fa80df5e1a
Alex <oleksii.pidhornyi@gmail.com>
2025-04-04 22:35:36 +0200

    extend chart linear mark customization

--- SwiftUIViewsMastery/SwiftUI/ControlViews/Chart/Chart_LinearMarkCustomization.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Chart/Chart_LinearMarkCustomization.swift
@@ -6,10 +6,36 @@
 //
 
 import SwiftUI
+import Charts
 
 struct Chart_LinearMarkCustomization: View {
+    @State private var data = GroupInfo.fetchData()
+    @State private var iMethod = InterpolationMethod.linear
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Chart(data) { datum in
+                LineMark(x: .value("Category", datum.x),
+                         y: .value("Quantity", datum.y)
+                )
+                .foregroundStyle(by: .value("Group", datum.group))
+                .lineStyle(StrokeStyle(lineWidth: 4, lineJoin: CGLineJoin.round))
+                .symbol(by: .value("", datum.group))
+                .interpolationMethod(iMethod)
+            }
+            .padding()
+            .dynamicTypeSize(.xLarge)
+            
+            GroupBox("some Line Options") {
+                HStack(spacing: 16) {
+                    Button("Rounded") { iMethod = .catmullRom }
+                    Button("Linear") { iMethod = .linear }
+                    Button("Cardinal") { iMethod = .cardinal }
+                    Button("Mono") { iMethod = .monotone }
+                    Button("Step") { iMethod = .stepCenter }
+                }
+            }
+        }
     }
 }
 

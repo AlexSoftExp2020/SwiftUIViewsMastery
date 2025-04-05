65ea49ee47aad9dcc78371d75bf97416d040c929
Alex <oleksii.pidhornyi@gmail.com>
2025-04-05 22:35:03 +0200

    add date picker 14 displayed components

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/DatePicker/DatePicker14_DisplayedComponents.swift
@@ -0,0 +1,18 @@
+//
+//  DatePicker14_DisplayedComponents.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 05.04.2025.
+//
+
+import SwiftUI
+
+struct DatePicker14_DisplayedComponents: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DatePicker14_DisplayedComponents()
+}

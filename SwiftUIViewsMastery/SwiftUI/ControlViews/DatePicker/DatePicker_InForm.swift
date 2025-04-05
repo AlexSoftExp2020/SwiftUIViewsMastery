c0d51d81634884279fe5d06c4315bc621f04c71d
Alex <oleksii.pidhornyi@gmail.com>
2025-04-05 23:10:12 +0200

    add date picker in form

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/DatePicker/DatePicker_InForm.swift
@@ -0,0 +1,18 @@
+//
+//  DatePicker_InForm.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 05.04.2025.
+//
+
+import SwiftUI
+
+struct DatePicker_InForm: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DatePicker_InForm()
+}

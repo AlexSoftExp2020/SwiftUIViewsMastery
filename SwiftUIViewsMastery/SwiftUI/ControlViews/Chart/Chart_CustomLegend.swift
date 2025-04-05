55f0e29920f9c4086ab2d9825f5158da3c29d009
Alex <oleksii.pidhornyi@gmail.com>
2025-04-05 19:12:09 +0200

    extend chart custom legend

--- SwiftUIViewsMastery/SwiftUI/ControlViews/Chart/Chart_CustomLegend.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Chart/Chart_CustomLegend.swift
@@ -6,10 +6,25 @@
 //
 
 import SwiftUI
+import Charts
 
 struct Chart_CustomLegend: View {
+    @State private var data = GroupInfo.fetchData()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Chart(data) {
+            LineMark(x: .value("Category", $0.x),
+                     y: .value("Quantity", $0.y)
+            )
+            .foregroundStyle(by: .value("Group", $0.group))
+        }
+        .chartForegroundStyleScale([
+            "Rod": .purple,
+            "Lem": .orange,
+            "Mark": .red /* No data but in Legend */
+        ])
+        .padding()
+        .dynamicTypeSize(.xLarge)
     }
 }
 

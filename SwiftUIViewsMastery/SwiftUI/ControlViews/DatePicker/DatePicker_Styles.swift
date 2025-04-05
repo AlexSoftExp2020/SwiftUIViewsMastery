4c2cebc207e237b4e087086fce7101991b24cd6f
Alex <oleksii.pidhornyi@gmail.com>
2025-04-05 22:22:46 +0200

    add date picker styles

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/DatePicker/DatePicker_Styles.swift
@@ -0,0 +1,18 @@
+//
+//  DatePicker_Styles.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 05.04.2025.
+//
+
+import SwiftUI
+
+struct DatePicker_Styles: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DatePicker_Styles()
+}

8286a2b45eb13fd09e4a6d0d35e2570487f993c6
Alex <oleksii.pidhornyi@gmail.com>
2025-04-05 20:25:37 +0200

    extend color picker intro

--- SwiftUIViewsMastery/SwiftUI/ControlViews/ColorPicker/ColorPicker_Intro.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/ColorPicker/ColorPicker_Intro.swift
@@ -8,8 +8,23 @@
 import SwiftUI
 
 struct ColorPicker_Intro: View {
+    @State private var color = Color.pink
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            HeaderView("ColorPicker",
+                       subtitle: "Introduction",
+                       desc: "Use the ColorPicker to provide your users with color options. You will need to bind it to a color variable to store the color selected.",
+                       back: color)
+            
+            Spacer()
+            
+            ColorPicker("Pick a color", selection: $color)
+                .padding(.horizontal)
+            
+            Spacer()
+        }
+        .font(.title)
     }
 }
 

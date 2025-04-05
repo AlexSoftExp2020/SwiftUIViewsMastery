19d742e63124150a6979008840a67bd88584fc84
Alex <oleksii.pidhornyi@gmail.com>
2025-04-05 21:16:30 +0200

    extend color picker opacity option

--- SwiftUIViewsMastery/SwiftUI/ControlViews/ColorPicker/ColorPicker_OpacityOption.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/ColorPicker/ColorPicker_OpacityOption.swift
@@ -8,8 +8,25 @@
 import SwiftUI
 
 struct ColorPicker_OpacityOption: View {
+    @State private var color = Color.pink
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 40) {
+            HeaderView("ColorPicker",
+                       subtitle: "Opacity Option",
+                       desc: "By default, the color picker shows an opacity option. You can disable this option.",
+                       back: color)
+            
+            ColorPicker("Pick a Color",
+                        selection: $color)
+            .padding(.horizontal)
+            
+            ColorPicker("Pick a Color (No Opacity)",
+                        selection: $color,
+                        supportsOpacity: false)
+                .padding(.horizontal)
+        }
+        .font(.title)
     }
 }
 

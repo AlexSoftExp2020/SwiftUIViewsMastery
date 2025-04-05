0de3535a490cce52585c358b4aa560828187db82
Alex <oleksii.pidhornyi@gmail.com>
2025-04-05 22:10:53 +0200

    add date picker intro

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/DatePicker/DatePicker_Intro.swift
@@ -0,0 +1,18 @@
+//
+//  DatePicker_Intro.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 05.04.2025.
+//
+
+import SwiftUI
+
+struct DatePicker_Intro: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DatePicker_Intro()
+}

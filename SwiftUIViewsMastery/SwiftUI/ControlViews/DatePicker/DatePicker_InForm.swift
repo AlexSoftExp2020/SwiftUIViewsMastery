478c8569b88346c0db16ebef7c3d64b1dbcec93e
Alex <oleksii.pidhornyi@gmail.com>
2025-04-05 23:21:31 +0200

    extend date picker in form

--- SwiftUIViewsMastery/SwiftUI/ControlViews/DatePicker/DatePicker_InForm.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/DatePicker/DatePicker_InForm.swift
@@ -8,8 +8,30 @@
 import SwiftUI
 
 struct DatePicker_InForm: View {
+    @State private var date = Date()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            HeaderView("DatePicker",
+                       subtitle: "Used in a Form",
+                       desc: "When used in a form, the date picker used the compact styling by default.",
+                       back: .green)
+            
+            Form {
+                DatePicker("Today",
+                           selection: $date,
+                           displayedComponents: .date)
+                
+                Section {
+                    Text("Graphical Picker Style:")
+                    DatePicker("Birthday",
+                               selection: $date,
+                               displayedComponents: .date)
+                    .datePickerStyle(.graphical)
+                }
+            }
+        }
+        .font(.title)
     }
 }
 

9f93b022d90d252dac2291e39ee36df2315aa5dc
Alex <oleksii.pidhornyi@gmail.com>
2025-04-06 01:09:01 +0200

    extend date picker custom selector

--- SwiftUIViewsMastery/SwiftUI/ControlViews/DatePicker/DatePicker_CustomSelector.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/DatePicker/DatePicker_CustomSelector.swift
@@ -8,8 +8,36 @@
 import SwiftUI
 
 struct DatePicker_CustomSelector: View {
+    @State private var date = Date()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 30) {
+            HeaderView("DatePicker",
+                       subtitle: "Custom Selector",
+                       desc: "At this time we can customize the color of the text or background. Here is one option though.",
+                       back: .green)
+            
+            DatePicker("Today", selection: $date, displayedComponents: .date)
+                .labelsHidden()
+                .background(RoundedRectangle(cornerRadius: 8,
+                                             style: .continuous)
+                    .fill(Color.green)
+                    .opacity(0.2))
+            
+            DescView("What does NOT work", back: .green)
+            Form {
+                DatePicker("foregroundStyle", selection: $date, displayedComponents: .date)
+                    .foregroundStyle(.green)
+                
+                DatePicker("foregroundStyle", selection: $date, displayedComponents: .date)
+                    .foregroundStyle(.green, .green, .green)
+                
+                DatePicker("foregroundStyle", selection: $date, displayedComponents: .date)
+                    .tint(.green)
+            }
+            .font(.title2)
+        }
+        .font(.title)
     }
 }
 

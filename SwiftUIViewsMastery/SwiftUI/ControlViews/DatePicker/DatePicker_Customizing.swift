edf7cae2fddb2dbee7234e604ad187b63a6f982b
Alex <oleksii.pidhornyi@gmail.com>
2025-04-06 00:18:30 +0200

    extend date picker customizing

--- SwiftUIViewsMastery/SwiftUI/ControlViews/DatePicker/DatePicker_Customizing.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/DatePicker/DatePicker_Customizing.swift
@@ -8,8 +8,33 @@
 import SwiftUI
 
 struct DatePicker_Customizing: View {
+    @State private var date = Date()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 30) {
+            HeaderView("DatePicker",
+                       subtitle: "Customizing",
+                       desc: "Customize the background and accent color:",
+                       back: .green)
+            
+            DatePicker("Birthday", selection: $date, displayedComponents: .date)
+            .datePickerStyle(.graphical)
+            .accentColor(.green)
+            .padding()
+            .background(RoundedRectangle(cornerRadius: 20)
+                .fill(Color.green)
+                .opacity(0.1)
+                .shadow(radius: 1, x: 4, y: 4))
+            .padding(.horizontal)
+            
+            DatePicker("Today", selection: $date, displayedComponents: .date)
+            .frame(height: 50)
+            .padding()
+            .background(Rectangle()
+                .fill(Color.green)
+                .shadow(radius: 4)
+                .opacity(0.2))
+        }.font(.title)
     }
 }
 

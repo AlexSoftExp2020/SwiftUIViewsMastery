5c2871aab1b302b19a498be7eff47eccfa67b6f8
Alex <oleksii.pidhornyi@gmail.com>
2025-04-06 21:42:22 +0200

    add disclosure group nested

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/DisclosureGroup/DisclosureGroup_Nested.swift
@@ -0,0 +1,18 @@
+//
+//  DisclosureGroup_Nested.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 06.04.2025.
+//
+
+import SwiftUI
+
+struct DisclosureGroup_Nested: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DisclosureGroup_Nested()
+}

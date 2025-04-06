752038fe66492d104e8a74620a46f8e7c4f2bba7
Alex <oleksii.pidhornyi@gmail.com>
2025-04-06 22:05:22 +0200

    extend disclosure group nested

--- SwiftUIViewsMastery/SwiftUI/ControlViews/DisclosureGroup/DisclosureGroup_Nested.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/DisclosureGroup/DisclosureGroup_Nested.swift
@@ -8,8 +8,51 @@
 import SwiftUI
 
 struct DisclosureGroup_Nested: View {
+    @State private var disclosureExpanded = false
+    @State private var sunExpanded = false
+    @State private var cloudsExpanded = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            HeaderView("DisclosureGroup",
+                       subtitle: "Nested",
+                       desc: "You can nest disclosure groups to allow users to drill down into views.",
+                       back: .orange)
+            
+            DisclosureGroup(isExpanded: $disclosureExpanded) {
+                VStack {
+                    DisclosureGroup("Sun", isExpanded: $sunExpanded) {
+                        HStack(spacing: 40) {
+                            Image(systemName: "sunrise.fill")
+                            Image(systemName: "sun.min.fill")
+                            Image(systemName: "sun.max.fill")
+                            Image(systemName: "sunset.fill")
+                        }.padding(.vertical)
+                    }
+                    DisclosureGroup("Clouds", isExpanded: $cloudsExpanded) {
+                        HStack(spacing: 40) {
+                            Image(systemName: "cloud.fill")
+                            Image(systemName: "cloud.drizzle.fill")
+                            Image(systemName: "cloud.bolt.fill")
+                            Image(systemName: "smoke.fill")
+                        }.padding(.vertical)
+                    }
+                }
+                .padding()
+                .background(RoundedRectangle(cornerRadius: 20)
+                    .fill(Color.orange)
+                    .opacity(0.1))
+            } label: {
+                HStack(spacing: 20) {
+                    Image(systemName: "sun.max")
+                    Text("Weather")
+                }
+            }
+            .padding(.horizontal)
+            
+            Spacer()
+        }
+        .font(.title)
     }
 }
 

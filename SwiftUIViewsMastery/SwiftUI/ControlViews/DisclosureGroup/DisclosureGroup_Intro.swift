83c10c7441c49f9cdb6775c4160c7b3fa571c873
Alex <oleksii.pidhornyi@gmail.com>
2025-04-06 08:37:05 +0200

    add disclosure group

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/DisclosureGroup/DisclosureGroup_Intro.swift
@@ -0,0 +1,18 @@
+//
+//  DisclosureGroup_Intro.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 06.04.2025.
+//
+
+import SwiftUI
+
+struct DisclosureGroup_Intro: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DisclosureGroup_Intro()
+}

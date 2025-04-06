dbe931212abea8ed4682ceb59f0cd020f56d1174
Alex <oleksii.pidhornyi@gmail.com>
2025-04-06 12:44:01 +0200

    extend disclosure group color

--- SwiftUIViewsMastery/SwiftUI/ControlViews/DisclosureGroup/DisclosureGroup_Color.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/DisclosureGroup/DisclosureGroup_Color.swift
@@ -8,8 +8,31 @@
 import SwiftUI
 
 struct DisclosureGroup_Color: View {
+    @State private var disclosureExpanded = true
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            HeaderView("DisclosureGroup",
+                       subtitle: "Color",
+                       desc: "You can change the color of the chevron and label with the use of the tint modifier.")
+            VStack(spacing: 20) {
+                DisclosureGroup("Color",
+                                isExpanded: $disclosureExpanded) {
+                    Text("If you want to expand a DisclosureGroup programmatically you can bind a property to the isExpanded parameter.")
+                        .font(.title2)
+                        .padding()
+                }
+                                .tint(.orange)
+                                .padding()
+                                .background(RoundedRectangle(cornerRadius: 20)
+                                    .fill(Color.orange)
+                                    .opacity(0.1))
+            }
+            .padding(.horizontal)
+            
+            Spacer()
+        }
+        .font(.title)
     }
 }
 

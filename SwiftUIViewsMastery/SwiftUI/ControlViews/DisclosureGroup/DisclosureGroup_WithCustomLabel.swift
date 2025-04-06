f79830b175f61d8116bdb5a9f8e2bbf7ea8d06a4
Alex <oleksii.pidhornyi@gmail.com>
2025-04-06 21:39:59 +0200

    extend disclosure group with custom label

--- SwiftUIViewsMastery/SwiftUI/ControlViews/DisclosureGroup/DisclosureGroup_WithCustomLabel.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/DisclosureGroup/DisclosureGroup_WithCustomLabel.swift
@@ -8,8 +8,36 @@
 import SwiftUI
 
 struct DisclosureGroup_WithCustomLabel: View {
+    @State private var sunExpanded = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            HeaderView("DisclosureGroup",
+                       subtitle: "Custom Label Views",
+                       desc: "You can use a custom view for the disclosure group's label by using a different initializer.",
+                       back: .orange)
+            DisclosureGroup(
+                isExpanded: $sunExpanded) {
+                    HStack(spacing: 40) {
+                        Image(systemName: "sunrise.fill")
+                        Image(systemName: "sun.min.fill")
+                        Image(systemName: "sun.max.fill")
+                        Image(systemName: "sunset.fill")
+                    }
+                    .padding(24)
+                    .background(RoundedRectangle(cornerRadius: 20)
+                        .fill(Color.orange)
+                        .opacity(0.1))
+                } label: {
+                    HStack(spacing: 20) {
+                        Image(systemName: "sun.max")
+                        Text("Sun")
+                    }
+                }
+                .padding(.horizontal)
+            Spacer()
+        }
+        .font(.title)
     }
 }
 

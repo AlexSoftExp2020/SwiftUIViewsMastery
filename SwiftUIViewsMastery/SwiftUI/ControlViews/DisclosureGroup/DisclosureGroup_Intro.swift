ab666c3cb9dc8b88cb858f93040d38fb1f3cbb8e
Alex <oleksii.pidhornyi@gmail.com>
2025-04-06 08:46:43 +0200

    extend disclosure group intro

--- SwiftUIViewsMastery/SwiftUI/ControlViews/DisclosureGroup/DisclosureGroup_Intro.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/DisclosureGroup/DisclosureGroup_Intro.swift
@@ -8,8 +8,32 @@
 import SwiftUI
 
 struct DisclosureGroup_Intro: View {
+    @State private var disclosureExpanded = true
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            HeaderView("DisclosureGroup",
+                       subtitle: "Introduction",
+                       desc: "Use the DisclosureGroup when you want to expand or collapse other views.")
+            VStack(spacing: 20) {
+                DisclosureGroup("More Info") {
+                    Text("Tap the row to expand/collapse the content of the DisclosureGroup. DisclosureGroups are collapsed by default.")
+                }
+                DisclosureGroup {
+                    Image(systemName: "info.circle.fill")
+                        .foregroundStyle(.orange)
+                    Text("You can use another initializer to customize your label too.")
+                } label: {
+                    Text("More Info")
+                        .bold()
+                }
+
+            }
+            .padding(.horizontal)
+            
+            Spacer()
+        }
+        .font(.title)
     }
 }
 

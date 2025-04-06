4240d101eb0f6b09c00b9f22ccc5a74f04e44c28
Alex <oleksii.pidhornyi@gmail.com>
2025-04-06 23:38:26 +0200

    add section text and image

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Form/SectionTextAndImage.swift
@@ -0,0 +1,18 @@
+//
+//  SectionTextAndImage.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 06.04.2025.
+//
+
+import SwiftUI
+
+struct SectionTextAndImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SectionTextAndImage()
+}

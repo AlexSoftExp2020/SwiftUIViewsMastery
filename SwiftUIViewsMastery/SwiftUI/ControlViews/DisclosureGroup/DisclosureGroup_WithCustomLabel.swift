b334e525ec1b416d11162fe81796e43475b6775a
Alex <oleksii.pidhornyi@gmail.com>
2025-04-06 12:47:08 +0200

    add disclosure group with custom label

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/DisclosureGroup/DisclosureGroup_WithCustomLabel.swift
@@ -0,0 +1,18 @@
+//
+//  DisclosureGroup_WithCustomLabel.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 06.04.2025.
+//
+
+import SwiftUI
+
+struct DisclosureGroup_WithCustomLabel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DisclosureGroup_WithCustomLabel()
+}

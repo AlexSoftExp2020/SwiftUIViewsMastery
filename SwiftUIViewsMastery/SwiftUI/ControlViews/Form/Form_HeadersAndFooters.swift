2c961462f077cce974d5bd2d0d4e59d7fbbbbc8c
Alex <oleksii.pidhornyi@gmail.com>
2025-04-07 23:42:59 +0200

    add form headers and footers

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Form/Form_HeadersAndFooters.swift
@@ -0,0 +1,18 @@
+//
+//  Form_HeadersAndFooters.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 07.04.2025.
+//
+
+import SwiftUI
+
+struct Form_HeadersAndFooters: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Form_HeadersAndFooters()
+}

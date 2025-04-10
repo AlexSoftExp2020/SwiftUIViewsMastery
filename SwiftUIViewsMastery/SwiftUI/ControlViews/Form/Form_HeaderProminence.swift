311a257cdd771d33abee7d50b93f8948d5500a99
Alex <oleksii.pidhornyi@gmail.com>
2025-04-10 22:28:17 +0200

    add form header prominence

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Form/Form_HeaderProminence.swift
@@ -0,0 +1,18 @@
+//
+//  Form_HeaderProminence.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 10.04.2025.
+//
+
+import SwiftUI
+
+struct Form_HeaderProminence: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Form_HeaderProminence()
+}

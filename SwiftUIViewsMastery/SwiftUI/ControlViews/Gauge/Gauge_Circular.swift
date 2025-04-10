122368503f8f548f74936bd514927934e5d625f2
Alex <oleksii.pidhornyi@gmail.com>
2025-04-11 01:19:50 +0200

    add gauge circular

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Gauge/Gauge_Circular.swift
@@ -0,0 +1,18 @@
+//
+//  Gauge_Circular.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 11.04.2025.
+//
+
+import SwiftUI
+
+struct Gauge_Circular: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Gauge_Circular()
+}

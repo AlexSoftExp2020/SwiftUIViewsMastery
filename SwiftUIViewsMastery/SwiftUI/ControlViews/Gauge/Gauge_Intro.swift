cc24a22f585f5df4aae15c38a94d2d6b5c5e5968
Alex <oleksii.pidhornyi@gmail.com>
2025-04-11 00:53:09 +0200

    add gauge intro

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Gauge/Gauge_Intro.swift
@@ -0,0 +1,18 @@
+//
+//  Gauge_Intro.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 11.04.2025.
+//
+
+import SwiftUI
+
+struct Gauge_Intro: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Gauge_Intro()
+}

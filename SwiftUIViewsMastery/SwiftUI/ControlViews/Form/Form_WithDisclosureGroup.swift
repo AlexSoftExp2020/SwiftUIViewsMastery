34fc50300cb5fed88e8d435dac8d9bb62214ebe1
Alex <oleksii.pidhornyi@gmail.com>
2025-04-11 00:42:02 +0200

    extend with disclosure group

--- SwiftUIViewsMastery/SwiftUI/ControlViews/Form/Form_WithDisclosureGroup.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Form/Form_WithDisclosureGroup.swift
@@ -8,8 +8,33 @@
 import SwiftUI
 
 struct Form_WithDisclosureGroup: View {
+    @State private var settingsExpanded = true
+    @State private var trebleOn = true
+    @State private var bassOn = false
+    @State private var levels = 0.5
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            HeaderView("Form",
+                       subtitle: "With Disclosure Groups",
+                       desc: "You can add disclosure group to a form to allow users to expand into more settings or views.",
+                       back: .purple,
+                       textColor: .white)
+            
+            Form {
+                DisclosureGroup("Audio Settings", isExpanded: $settingsExpanded) {
+                    VStack {
+                        Toggle("Treble", isOn: $trebleOn)
+                        Toggle("Bass", isOn: $bassOn)
+                        Slider(value: $levels)
+                    }
+                    .font(.title2)
+                    .padding()
+                }
+            }
+        }
+        .font(.title)
+        .tint(.purple)
     }
 }
 

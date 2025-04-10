f89b1f7b53deb9190dbb3f80eaec3e635a6cc607
Alex <oleksii.pidhornyi@gmail.com>
2025-04-10 21:33:37 +0200

    extend form headers and footers

--- SwiftUIViewsMastery/SwiftUI/ControlViews/Form/Form_HeadersAndFooters.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Form/Form_HeadersAndFooters.swift
@@ -9,7 +9,27 @@ import SwiftUI
 
 struct Form_HeadersAndFooters: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            
+            Section {
+                Text("You can add any view in a section header")
+                Text("Notice the default foreground color is gray")
+            } header: {
+                Text("Section Header Text")
+            }
+            
+            Section {
+                Text("Here's and example of a section header with image and text")
+            } header: {
+                SectionTextAndImage(name: "People", image: "person.2.square.stack.fill")
+            }
+            
+            Section {
+                Text("Here is an example of a section footer")
+            } header: {
+                Text("Total: $5,600.00").bold()
+            }
+        }
     }
 }
 

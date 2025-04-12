266de27c53a4faa9217963a63e8cdadccd9660f1
Alex <oleksii.pidhornyi@gmail.com>
2025-04-12 23:36:30 +0200

    extend gauge labels

--- SwiftUIViewsMastery/SwiftUI/ControlViews/Gauge/Gauge_Labels.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Gauge/Gauge_Labels.swift
@@ -8,8 +8,49 @@
 import SwiftUI
 
 struct Gauge_Labels: View {
+    @State private var value = 0.75
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 50) {
+            Gauge(value: value) {
+                Text("Range")
+            } currentValueLabel: {
+                Text(value, format: .number) // number under gauge
+            } markedValueLabels: {
+                Text("Not currently used")
+            }
+            
+            Gauge(value: value) {
+                Text("Range")
+            } currentValueLabel: {
+                Text(value, format: .number) // number inside gauge
+            }
+            .gaugeStyle(.accessoryCircularCapacity)
+            .tint(.blue)
+            
+            Gauge(value: value) {
+                Text("Range")
+            } currentValueLabel: {
+                Text(value, format: .number) // number under gauge
+            } minimumValueLabel: {
+                Text(0, format: .number) // leading
+            } maximumValueLabel: {
+                Text(100, format: .number) // trailing
+            }
+            
+            Gauge(value: value) {
+                Text("Range") // Not shown
+            } currentValueLabel: {
+                Text(value, format: .number)
+            } minimumValueLabel: {
+                Image(systemName: "circle")
+            } maximumValueLabel: {
+                Image(systemName: "circle.fill")
+            }
+            .gaugeStyle(.accessoryLinear)
+            .tint(.blue)
+
+
+        }
     }
 }
 

8912fc29c43efd9efe30bba2fa2df45fa0740a60
Alex <oleksii.pidhornyi@gmail.com>
2025-04-12 22:52:18 +0200

    add gauge range

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Gauge/Gauge_Range.swift
@@ -0,0 +1,18 @@
+//
+//  Gauge_Range.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 12.04.2025.
+//
+
+import SwiftUI
+
+struct Gauge_Range: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Gauge_Range()
+}

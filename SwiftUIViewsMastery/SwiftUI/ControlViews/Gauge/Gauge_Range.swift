38c4452e105350825fe41e2ff134688ee29bc928
Alex <oleksii.pidhornyi@gmail.com>
2025-04-12 23:07:46 +0200

    extend gauge range

--- SwiftUIViewsMastery/SwiftUI/ControlViews/Gauge/Gauge_Range.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Gauge/Gauge_Range.swift
@@ -8,8 +8,28 @@
 import SwiftUI
 
 struct Gauge_Range: View {
+    @State private var value = 75.0
+    private let minValue = 0.0
+    private let maxValue = 100.0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Gauge(value: value) {
+                Text("No Range")
+            }
+            
+            Gauge(value: value, in: minValue...maxValue) {
+                Text("Range")
+            }
+            
+            Gauge(value: value, in: minValue...maxValue) {
+                Text("Range")
+            }
+            .gaugeStyle(.accessoryCircularCapacity)
+            .tint(.blue)
+        }
+        .font(.title)
+        .padding()
     }
 }
 

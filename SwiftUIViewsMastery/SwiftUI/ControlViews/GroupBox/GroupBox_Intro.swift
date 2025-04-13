7e8e655c326f400aff55518f62dd79d866134817
Alex <oleksii.pidhornyi@gmail.com>
2025-04-13 23:43:39 +0200

    add group box intro

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/GroupBox/GroupBox_Intro.swift
@@ -0,0 +1,18 @@
+//
+//  GroupBox_Intro.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 13.04.2025.
+//
+
+import SwiftUI
+
+struct GroupBox_Intro: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GroupBox_Intro()
+}

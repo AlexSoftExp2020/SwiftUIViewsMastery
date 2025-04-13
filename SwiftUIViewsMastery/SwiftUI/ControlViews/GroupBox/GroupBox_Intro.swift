449aa7e59db5922aa73fc536d9d717d00c1fca20
Alex <oleksii.pidhornyi@gmail.com>
2025-04-13 23:53:59 +0200

    add group box intro

--- SwiftUIViewsMastery/SwiftUI/ControlViews/GroupBox/GroupBox_Intro.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/GroupBox/GroupBox_Intro.swift
@@ -8,8 +8,25 @@
 import SwiftUI
 
 struct GroupBox_Intro: View {
+    @State private var username = ""
+    @State private var password = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HeaderView("GroupBox",
+                       subtitle: "Introduction",
+                       desc: "You can use the GroupBox to combine views that are related.")
+            
+            GroupBox {
+                Text("Login")
+                TextField("Username", text: $username)
+                    .textFieldStyle(.roundedBorder)
+                SecureField("Password", text: $username)
+                    .textFieldStyle(.roundedBorder)
+            }
+            .padding(.horizontal)
+        }
+        .font(.title)
     }
 }
 

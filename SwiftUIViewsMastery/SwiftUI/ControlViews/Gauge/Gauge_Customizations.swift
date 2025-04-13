a2956c2e030965927a53e9ec7a935c21fb630002
Alex <oleksii.pidhornyi@gmail.com>
2025-04-13 23:36:02 +0200

    extend gauge customizations

--- SwiftUIViewsMastery/SwiftUI/ControlViews/Gauge/Gauge_Customizations.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Gauge/Gauge_Customizations.swift
@@ -8,8 +8,35 @@
 import SwiftUI
 
 struct Gauge_Customizations: View {
+    @State private var value = 75.0
+    private let minValue = 0.0
+    private let maxValue = 100.0
+    
+    let gradient = Gradient(colors: [.blue, .yellow, .orange, .red])
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 50.0) {
+            Gauge(value: value, in: minValue...maxValue) {
+                Label("Range", systemImage: "arrow.left.and.right")
+            } currentValueLabel: {
+                Text(value, format: .number)
+            } minimumValueLabel: {
+                Text(minValue, format: .number)
+            } maximumValueLabel: {
+                Text(maxValue, format: .number)
+            }
+            .tint(gradient) // Doesn't affect labels
+            
+            Gauge(value: value, in: minValue...maxValue) {
+                Text("Color")
+            }
+            .gaugeStyle(.accessoryCircular)
+            .tint(gradient)
+            .scaleEffect(3)
+            .frame(height: 220)
+        }
+        .font(.title)
+        .padding()
     }
 }
 

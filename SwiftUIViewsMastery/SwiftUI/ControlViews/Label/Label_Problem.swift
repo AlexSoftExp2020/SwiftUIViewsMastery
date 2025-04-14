fa75431d40eff97f3b71d1079300db150caec22e
Alex <oleksii.pidhornyi@gmail.com>
2025-04-14 10:58:50 +0200

    add label problem

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Label/Label_Problem.swift
@@ -0,0 +1,18 @@
+//
+//  Label_Problem.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 14.04.2025.
+//
+
+import SwiftUI
+
+struct Label_Problem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Label_Problem()
+}

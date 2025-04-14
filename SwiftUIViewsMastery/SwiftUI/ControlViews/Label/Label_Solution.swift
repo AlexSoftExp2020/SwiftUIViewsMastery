c62342662a17219de218c992a59a8a9100fb966d
Alex <oleksii.pidhornyi@gmail.com>
2025-04-14 11:14:11 +0200

    add label solution

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Label/Label_Solution.swift
@@ -0,0 +1,18 @@
+//
+//  Label_Solution.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 14.04.2025.
+//
+
+import SwiftUI
+
+struct Label_Solution: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Label_Solution()
+}

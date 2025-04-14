f744f41a43f526e50d9784c15842c2dde1c801a0
Alex <oleksii.pidhornyi@gmail.com>
2025-04-14 10:44:36 +0200

    extend group box with label

--- SwiftUIViewsMastery/SwiftUI/ControlViews/GroupBox/GroupBox_WithLabel.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/GroupBox/GroupBox_WithLabel.swift
@@ -8,8 +8,40 @@
 import SwiftUI
 
 struct GroupBox_WithLabel: View {
+    @State private var username = ""
+    @State private var password = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 10) {
+            HeaderView("GroupBox",
+                       subtitle: "With Label",
+                       desc: "Instead of using your own text label, the GroupBox has one built in.",
+                       back: .blue,
+                       textColor: .white)
+            
+            GroupBox("Login (Default text format)") {
+                TextField("Username", text: $username)
+                    .textFieldStyle(.roundedBorder)
+                SecureField("Password", text: $username)
+                    .textFieldStyle(.roundedBorder)
+            }
+            
+            DescView("You can override the default label formatting.",
+                     back: .blue,
+                     textColor: .white)
+            
+            GroupBox {
+                TextField("Username", text: $username)
+                    .textFieldStyle(.roundedBorder)
+                TextField("Password", text: $username)
+                    .textFieldStyle(.roundedBorder)
+            } label: {
+                Text("Reset Password")
+                    .font(.title2)
+                    .fontWeight(.light)
+            }
+        }
+        .font(.title)
     }
 }
 

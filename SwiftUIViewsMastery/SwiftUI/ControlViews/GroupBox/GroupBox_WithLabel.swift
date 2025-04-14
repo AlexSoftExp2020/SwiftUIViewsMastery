fb6419c5e5c2bb18ae43ca852df8d280fb917a15
Alex <oleksii.pidhornyi@gmail.com>
2025-04-14 08:09:10 +0200

    add group box with label

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/GroupBox/GroupBox_WithLabel.swift
@@ -0,0 +1,18 @@
+//
+//  GroupBox_WithLabel.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 14.04.2025.
+//
+
+import SwiftUI
+
+struct GroupBox_WithLabel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GroupBox_WithLabel()
+}

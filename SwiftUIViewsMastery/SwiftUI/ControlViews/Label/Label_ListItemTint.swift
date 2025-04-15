e9e59434d097c668f78a20cdfa2b6fd7d8462d66
Alex <oleksii.pidhornyi@gmail.com>
2025-04-15 23:42:12 +0200

    extend label list item tint

--- SwiftUIViewsMastery/SwiftUI/ControlViews/Label/Label_ListItemTint.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Label/Label_ListItemTint.swift
@@ -9,7 +9,28 @@ import SwiftUI
 
 struct Label_ListItemTint: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            HeaderView("Label",
+                       subtitle: "List Item Tint",
+                       desc: "In iOS, you can apply an item tint color to a label view to a change just the image.",
+                       back: .pink)
+            .layoutPriority(1)
+            
+            List {
+                Label("Edit", systemImage: "rectangle.and.pencil.and.ellipsis")
+                    .listItemTint(.pink)
+                Label("Archives", systemImage: "archivebox.circle")
+                    .listItemTint(.orange)
+                Label("Document", systemImage: "doc.richtext")
+                    .listItemTint(.red)
+                Label("Camera Settings", systemImage: "camera.badge.ellipsis")
+                    .listItemTint(.monochrome)
+            }
+            
+            Label("Bullet List", systemImage: "list.bullet.rectangle.fill")
+                .listItemTint(.red) // listItemTint does not work when outside a list
+        }
+        .font(.title)
     }
 }
 

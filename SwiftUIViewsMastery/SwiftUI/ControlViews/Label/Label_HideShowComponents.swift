15b597f4d6b06413d8419b7be02632c819b50ae6
Alex <oleksii.pidhornyi@gmail.com>
2025-04-15 23:52:21 +0200

    add label hide show components

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Label/Label_HideShowComponents.swift
@@ -0,0 +1,18 @@
+//
+//  Label_HideShowComponents.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 15.04.2025.
+//
+
+import SwiftUI
+
+struct Label_HideShowComponents: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Label_HideShowComponents()
+}

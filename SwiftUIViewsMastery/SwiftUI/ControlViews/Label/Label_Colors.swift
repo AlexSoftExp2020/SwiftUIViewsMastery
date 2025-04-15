969515efb3c8ee70d9cfed49aa82876792cf67a2
Alex <oleksii.pidhornyi@gmail.com>
2025-04-15 23:43:17 +0200

    add label colors

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Label/Label_Colors.swift
@@ -0,0 +1,18 @@
+//
+//  Label_Colors.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 15.04.2025.
+//
+
+import SwiftUI
+
+struct Label_Colors: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Label_Colors()
+}

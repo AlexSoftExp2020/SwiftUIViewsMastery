fe095a9f58863fd90e5c00dbf2ee08bab1b64efa
Alex <oleksii.pidhornyi@gmail.com>
2025-04-15 23:30:22 +0200

    add label list item tint

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Label/Label_ListItemTint.swift
@@ -0,0 +1,18 @@
+//
+//  Label_ListItemTint.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 15.04.2025.
+//
+
+import SwiftUI
+
+struct Label_ListItemTint: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Label_ListItemTint()
+}

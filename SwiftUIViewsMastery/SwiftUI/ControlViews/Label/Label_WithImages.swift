e231610b632c6322f7eaa46003d51b73c781defd
Alex <oleksii.pidhornyi@gmail.com>
2025-04-15 23:28:49 +0200

    extend label with images

--- SwiftUIViewsMastery/SwiftUI/ControlViews/Label/Label_WithImages.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Label/Label_WithImages.swift
@@ -9,7 +9,23 @@ import SwiftUI
 
 struct Label_WithImages: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            HeaderView("Label",
+                       subtitle: "With Images",
+                       desc: "Instead of SF Symbols, you can also use images.",
+                       back: .pink)
+            
+            Spacer()
+            
+            Label("Helena", image: "profile")
+                .border(Color.pink, width: 3)
+            
+            Spacer()
+            
+            DescView("It's important to know that the Label will not resize your image. You will have to resize your images before they are used in the Label.",
+                     back: .pink)
+        }
+        .font(.title)
     }
 }
 

6b998ccee46e09a58d753e41ef6bfb366ef1f57f
Alex <oleksii.pidhornyi@gmail.com>
2025-04-16 21:46:59 +0200

    add link button styles

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Link/Link_ButtonStyles.swift
@@ -0,0 +1,18 @@
+//
+//  Link_ButtonStyles.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 16.04.2025.
+//
+
+import SwiftUI
+
+struct Link_ButtonStyles: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Link_ButtonStyles()
+}

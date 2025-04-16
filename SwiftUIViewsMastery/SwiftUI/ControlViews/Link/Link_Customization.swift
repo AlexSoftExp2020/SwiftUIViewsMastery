f5ac077595092a934ec7f88e444c0795179bd3a1
Alex <oleksii.pidhornyi@gmail.com>
2025-04-16 21:44:03 +0200

    extend link customization

--- SwiftUIViewsMastery/SwiftUI/ControlViews/Link/Link_Customization.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Link/Link_Customization.swift
@@ -9,7 +9,26 @@ import SwiftUI
 
 struct Link_Customization: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 40) {
+            HeaderView("Link",
+                       subtitle: "Customizations",
+                       desc: "Like the Button, there's another initializer you can use create a custom button for the Link.")
+            
+            Link(destination: URL(string: "https://www.apple.com")!) {
+                HStack(spacing: 20.0) {
+                    Image(systemName: "applelogo")
+                    Text("Go to Apple")
+                }
+                .foregroundStyle(.white)
+                .padding()
+                .padding(.horizontal)
+                .background(Capsule()
+                    .fill(Color.blue)
+                    .shadow(radius: 8, y: 18))
+            }
+            Spacer()
+        }
+        .font(.title)
     }
 }
 

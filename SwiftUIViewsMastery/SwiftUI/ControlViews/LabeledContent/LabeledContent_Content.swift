ef7d93cdffb93c373323e1e5b9a89dba6a973b8b
Alex <oleksii.pidhornyi@gmail.com>
2025-04-16 11:06:02 +0200

    add labeled content content

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/LabeledContent/LabeledContent_Content.swift
@@ -0,0 +1,18 @@
+//
+//  LabeledContent_Content.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 16.04.2025.
+//
+
+import SwiftUI
+
+struct LabeledContent_Content: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LabeledContent_Content()
+}

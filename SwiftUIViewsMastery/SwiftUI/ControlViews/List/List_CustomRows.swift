f7175305f14751ce453a639f8b848db2f62db6f7
Alex <oleksii.pidhornyi@gmail.com>
2025-04-16 22:48:12 +0200

    add list custom rows

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/List/List_CustomRows.swift
@@ -0,0 +1,18 @@
+//
+//  List_CustomRows.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 16.04.2025.
+//
+
+import SwiftUI
+
+struct List_CustomRows: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    List_CustomRows()
+}

319e5311e2cd25c92c39659b5bb0b3849c12c276
Alex <oleksii.pidhornyi@gmail.com>
2025-04-16 23:04:30 +0200

    add list move row

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/List/List_MoveRow.swift
@@ -0,0 +1,18 @@
+//
+//  List_MoveRow.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 16.04.2025.
+//
+
+import SwiftUI
+
+struct List_MoveRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    List_MoveRow()
+}

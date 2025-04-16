049178fef844a14563764f75dcdd8bb2244f13f1
Alex <oleksii.pidhornyi@gmail.com>
2025-04-16 10:46:29 +0200

    extend label hide show components

--- SwiftUIViewsMastery/SwiftUI/ControlViews/Label/Label_HideShowComponents.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Label/Label_HideShowComponents.swift
@@ -9,7 +9,24 @@ import SwiftUI
 
 struct Label_HideShowComponents: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            HeaderView("Colors",
+                       subtitle: "Hide/Show Components",
+                       desc: "You can hide or show the icon or text in Labels using labelStyle.",
+                       back: .pink)
+            
+            VStack(alignment: .leading, spacing: 20) {
+                Label("Automatic", systemImage: "camera.badge.ellipsis")
+                    .labelStyle(.automatic)
+                Label("Icon Only", systemImage: "camera.badge.ellipsis")
+                    .labelStyle(.iconOnly)
+                Label("Title Only", systemImage: "camera.badge.ellipsis")
+                    .labelStyle(.titleOnly)
+                Label("Title & Icon", systemImage: "camera.badge.ellipsis")
+                    .labelStyle(.titleAndIcon)
+            }
+        }
+        .font(.title)
     }
 }
 

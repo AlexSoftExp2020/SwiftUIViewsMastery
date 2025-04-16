0c4af30fd337d0c3ca5b243ed516ee77bac1dd76
Alex <oleksii.pidhornyi@gmail.com>
2025-04-16 11:02:27 +0200

    extend labeled content intro

--- SwiftUIViewsMastery/SwiftUI/ControlViews/LabeledContent/LabeledContent_Intro.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/LabeledContent/LabeledContent_Intro.swift
@@ -9,7 +9,36 @@ import SwiftUI
 
 struct LabeledContent_Intro: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 50.0) {
+            VStack(spacing: 24.0) {
+                LabeledContent("Label", value: "Value")
+                
+                LabeledContent("Label") {
+                    Label("Value", systemImage: "camera.macro")
+                }
+                
+                LabeledContent("Label", value: 55, format: .percent)
+                
+                LabeledContent {
+                    Text("Value")
+                        .font(.title.weight(.thin))
+                } label: {
+                    Text("Label")
+                        .fontWeight(.bold)
+                }
+            }
+            .padding()
+            
+            Form {
+                LabeledContent("Number") {
+                    Text(55, format: .number)
+                }
+                
+                Text("Text with Badge")
+                    .badge(55)
+            }
+        }
+        .font(.title)
     }
 }
 

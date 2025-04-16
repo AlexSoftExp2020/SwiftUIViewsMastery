378021745a2356c12a79039c8282c88fe5f73b9c
Alex <oleksii.pidhornyi@gmail.com>
2025-04-16 22:12:36 +0200

    extend link button styles

--- SwiftUIViewsMastery/SwiftUI/ControlViews/Link/Link_ButtonStyles.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Link/Link_ButtonStyles.swift
@@ -9,7 +9,30 @@ import SwiftUI
 
 struct Link_ButtonStyles: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 40) {
+            HeaderView("Link",
+                       subtitle: "Button Styles",
+                       desc: "You can use any of the same styling options that you also use on buttons.")
+            Link("Go to Apple", destination: URL(string: "https://www.apple.com")!)
+                .buttonStyle(.bordered)
+            
+            Link("Call Now", destination: URL(string: "tel: 8005551234")!)
+                .buttonStyle(.bordered)
+                .buttonBorderShape(.capsule)
+                .controlSize(.large)
+            
+            Link("Email Me", destination: URL(string: "mailto:myemail@swiftui.com")!)
+                .buttonStyle(.borderedProminent)
+                .buttonBorderShape(.roundedRectangle)
+                .controlSize(.large)
+                .tint(.mint)
+            
+            Link("Message Me", destination: URL(string: "sms:+18885551212&body=Hello")!)
+                .buttonStyle(.borderedProminent)
+            
+            Spacer()
+        }
+        .font(.title)
     }
 }
 

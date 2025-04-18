bfcdce8e025adc737d724f0424b495c30b4e6ea3
Alex <oleksii.pidhornyi@gmail.com>
2025-04-18 21:52:43 +0200

    extend list selection disabled

--- SwiftUIViewsMastery/SwiftUI/ControlViews/List/List_SelectionDisabled.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/List/List_SelectionDisabled.swift
@@ -8,8 +8,23 @@
 import SwiftUI
 
 struct List_SelectionDisabled: View {
+    @State private var data = ["Things to do", "Practice Coding",
+                               "Grocery shopping", "Get tickets",
+                               "Clean house", "Do laundry",
+                               "Cook dinner", "Paint room"]
+    @State private var selection: String?
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack(spacing: 0) {
+                List(data, id: \.self, selection: $selection) { item in
+                    Text(item)
+                        .selectionDisabled(item == "Things to do")
+                }
+            }
+            .font(.title)
+            .navigationTitle("List")
+        }
     }
 }
 

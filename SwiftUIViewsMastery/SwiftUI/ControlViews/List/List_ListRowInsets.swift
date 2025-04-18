65c351f50561ec7af2a5fe9ff2e65830a69da881
Alex <oleksii.pidhornyi@gmail.com>
2025-04-18 22:47:09 +0200

    extend list list row insets

--- SwiftUIViewsMastery/SwiftUI/ControlViews/List/List_ListRowInsets.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/List/List_ListRowInsets.swift
@@ -8,8 +8,52 @@
 import SwiftUI
 
 struct List_ListRowInsets: View {
+    @State private var newToDo = ""
+    
+    @State var data = [
+        Todo(action: "Practice using List Row Insets", due: "Today"),
+        Todo(action: "Grocery shopping", due: "Today"),
+        Todo(action: "Vegetables", due: "Today", isIndented: true),
+        Todo(action: "Spices", due: "Today", isIndented: true),
+        Todo(action: "Cook dinner", due: "Next Week"),
+        Todo(action: "Paint room", due: "Next Week")
+    ]
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            VStack {
+                Text("To Do")
+                    .font(.title)
+                    .foregroundStyle(.black)
+                HStack {
+                    TextField("new todo", text: $newToDo)
+                        .textFieldStyle(.roundedBorder)
+                    Button {
+                        data.append(Todo(action: newToDo))
+                        newToDo = ""
+                    } label: {
+                        Image(systemName: "plus.circle.fill")
+                            .font(.title)
+                            .foregroundStyle(.white)
+                    }
+                }
+            }
+            .padding()
+            .background(Color.green)
+            
+            List {
+                ForEach(self.data) { datum in
+                    Text(datum.action)
+                        .font(.title)
+                        .padding()
+                    // Inset row based on data
+                        .listRowInsets(EdgeInsets(top: 0,
+                                                  leading: datum.isIndented ? 60 : 20,
+                                                  bottom: 0,
+                                                  trailing: 0))
+                }
+            }
+            .listStyle(.plain)
+        }
     }
 }
 

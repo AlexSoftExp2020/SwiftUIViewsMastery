4af0b8b1159680ea858372609bd8f2be6a95b7e9
Alex <oleksii.pidhornyi@gmail.com>
2025-04-18 20:59:14 +0200

    extend list delete

--- SwiftUIViewsMastery/SwiftUI/ControlViews/List/List_Delete.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/List/List_Delete.swift
@@ -8,8 +8,22 @@
 import SwiftUI
 
 struct List_Delete: View {
+    @State var data = ["Swipe to Delete", "Practice Coding", "Grocery shopping", "Get tickets", "Clean house", "Do laundry", "Cook dinner", "Paint room"]
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            Section {
+                ForEach(data, id: \.self) { datum in
+                    Text(datum).font(Font.system(size: 24)).padding()
+                }
+                .onDelete { offset in
+                    data.remove(atOffsets: offset)
+                }
+            } header: {
+                Text("To Do")
+                    .padding()
+            }
+        }
+        .listStyle(.plain)
     }
 }
 

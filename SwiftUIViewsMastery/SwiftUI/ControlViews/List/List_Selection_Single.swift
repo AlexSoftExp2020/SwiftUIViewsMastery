3b3d2b42ce39e595e996dd380a045c39d99dc50a
Alex <oleksii.pidhornyi@gmail.com>
2025-04-18 21:25:46 +0200

    extend list selection single

--- SwiftUIViewsMastery/SwiftUI/ControlViews/List/List_Selection_Single.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/List/List_Selection_Single.swift
@@ -8,8 +8,23 @@
 import SwiftUI
 
 struct List_Selection_Single: View {
+    @State private var data = ["Practice Coding", "Grocery shopping", "Get tickets", "Clean house", "Do laundry", "Do laundry", "Cook dinner", "Paint room"]
+    
+    @State private var selection: String?
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack(spacing: 0) {
+                List(data, id: \.self, selection: $selection) { item in
+                    Text(item)
+                }
+                Text("To do first") +
+                Text(selection ?? "")
+                    .bold()
+            }
+            .font(.title)
+            .navigationTitle("List")
+        }
     }
 }
 

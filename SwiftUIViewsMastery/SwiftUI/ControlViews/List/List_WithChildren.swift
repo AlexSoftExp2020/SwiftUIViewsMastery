c9a6e333199f7f5ead302f7d2d727618de3d4c2d
Alex <oleksii.pidhornyi@gmail.com>
2025-04-18 22:53:27 +0200

    add parent struct

--- SwiftUIViewsMastery/SwiftUI/ControlViews/List/List_WithChildren.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/List/List_WithChildren.swift
@@ -7,6 +7,13 @@
 
 import SwiftUI
 
+// Need to conform to Identifiable
+struct Parent: Identifiable {
+    var id = UUID()
+    var name = ""
+    var children: [Parent]? // Had to make this optional
+}
+
 struct List_WithChildren: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)

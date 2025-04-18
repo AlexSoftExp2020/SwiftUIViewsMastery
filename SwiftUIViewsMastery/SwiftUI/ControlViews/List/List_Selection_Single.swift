511925a598a3df86823d1ff181db9a475189cffc
Alex <oleksii.pidhornyi@gmail.com>
2025-04-18 21:06:12 +0200

    add list selection single

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/List/List_Selection_Single.swift
@@ -0,0 +1,18 @@
+//
+//  List_Selection_Single.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 18.04.2025.
+//
+
+import SwiftUI
+
+struct List_Selection_Single: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    List_Selection_Single()
+}

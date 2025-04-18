a92061b679b4d95743048d0264db04d2f92c24e9
Alex <oleksii.pidhornyi@gmail.com>
2025-04-18 23:39:08 +0200

    extend list list style automatic

--- SwiftUIViewsMastery/SwiftUI/ControlViews/List/List_ListStyle_Automatic.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/List/List_ListStyle_Automatic.swift
@@ -9,7 +9,29 @@ import SwiftUI
 
 struct List_ListStyle_Automatic: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            HeaderView("List",
+                       subtitle: "List Style Automatic",
+                       desc: "You can apply different styles to lists. Here is what a list looks like using the default style 'automatic'.",
+                       back: .green)
+            
+            List {
+                Text("What yould you like to learn?")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                Label("Learn Geography", systemImage: "signpost.right.fill")
+                Label("Learn Music", systemImage: "doc.richtext")
+                Label("Learn Photography", systemImage: "camera.aperture")
+                Label("Learn Art", systemImage: "paintpalette.fill")
+                    .font(Font.system(.title3).weight(.bold))
+                Label("Learn Physics", systemImage: "atom")
+                Label("Learn 3D", systemImage: "cube.transparent")
+                Label("Learn Hair Styling", systemImage: "comb.fill")
+            }
+            .accentColor(.green)
+            .listStyle(.automatic)
+        }
+        .font(.title)
     }
 }
 

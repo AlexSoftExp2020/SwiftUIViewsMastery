0d9cd65151fca6c6bade89061ee58137511191c7
Alex <oleksii.pidhornyi@gmail.com>
2025-04-18 23:25:33 +0200

    add  list list style automatic

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/List/List_ListStyle_Automatic.swift
@@ -0,0 +1,18 @@
+//
+//  List_ListStyle_Automatic.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 18.04.2025.
+//
+
+import SwiftUI
+
+struct List_ListStyle_Automatic: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    List_ListStyle_Automatic()
+}

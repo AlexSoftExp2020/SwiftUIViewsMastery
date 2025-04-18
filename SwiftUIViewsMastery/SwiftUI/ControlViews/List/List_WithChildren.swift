56c06edae7732f136af26c86689a6f1da6ad245d
Alex <oleksii.pidhornyi@gmail.com>
2025-04-18 23:12:52 +0200

    extend list with children

--- SwiftUIViewsMastery/SwiftUI/ControlViews/List/List_WithChildren.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/List/List_WithChildren.swift
@@ -15,8 +15,25 @@ struct Parent: Identifiable {
 }
 
 struct List_WithChildren: View {
+    var parents = [Parent(name: "Mark", children: [Parent(name: "Paola")]),
+                   Parent(name: "Rodrigo", children: [Parent(name: "Kai"),
+                                                      Parent(name: "Brennan"),
+                                                      Parent(name: "Easton")]),
+                   Parent(name: "Marcella", children: [Parent(name: "Sam"),
+                                                       Parent(name: "Melissa"),
+                                                       Parent(name: "Melanie")])]
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20.0) {
+            HeaderView("List",
+                       subtitle: "Children",
+                       desc: "You can arrange your data to allow the List view to show it in an outline style.",
+                       back: .green)
+            
+            List(parents, children: \.children) { parent in
+                Text("\(parent.name)")
+            }
+        }
+        .font(.title)
     }
 }
 

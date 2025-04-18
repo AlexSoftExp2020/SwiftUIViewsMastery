6db1db579ede519943af3633c8481e580a15cee6
Alex <oleksii.pidhornyi@gmail.com>
2025-04-18 22:23:26 +0200

    extend list list row background

--- SwiftUIViewsMastery/SwiftUI/ControlViews/List/List_ListRowBackground.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/List/List_ListRowBackground.swift
@@ -15,8 +15,52 @@ struct Todo: Identifiable {
 }
 
 struct List_ListRowBackground: View {
+    @State private var newToDo = ""
+    
+    @State var data = [
+        Todo(action: "Practice Coding", due: "Today"),
+        Todo(action: "Grocery shopping", due: "Today"),
+        Todo(action: "Get tickets", due: "Tomorrow"),
+        Todo(action: "Clean house", due: "Next Week"),
+        Todo(action: "Do laundry", due: "Next Week"),
+        Todo(action: "Cook dinner", due: "Next Week"),
+        Todo(action: "Paint room", due: "Next Week")
+    ]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            Section {
+                ForEach(data) { datum in
+                    Text(datum.action)
+                        .font(Font.system(size: 24))
+                        .foregroundStyle(self.getTextColor(due: datum.due))
+                    // Turn row green if due today
+                        .listRowBackground(datum.due == "Today" ? Color.green : Color.clear)
+                        .padding()
+                }
+            } header: {
+                VStack {
+                    Text("To Do").font(.title)
+                    HStack {
+                        TextField("new todo", text: $newToDo)
+                            .textFieldStyle(.roundedBorder)
+                        Button {
+                            data.append(Todo(action: newToDo))
+                            newToDo = ""
+                        } label: {
+                            Image(systemName: "plus.circle.fill").font(.title)
+                        }
+                    }
+                }
+                .padding()
+            }
+        }
+        .listStyle(.plain)
+    }
+    
+    // This logic was inline but the compiler said it was "too complex" :)
+    private func getTextColor(due: String) -> Color {
+        due == "Today" ? Color.black : Color.primary
     }
 }
 

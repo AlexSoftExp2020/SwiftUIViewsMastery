95aba44d45b28c2dc234504036b49c53e5d34375
Alex <oleksii.pidhornyi@gmail.com>
2025-04-19 16:47:02 +0200

    extend list list style sidebar

--- SwiftUIViewsMastery/SwiftUI/ControlViews/List/List_ListStyle_Sidebar.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/List/List_ListStyle_Sidebar.swift
@@ -9,7 +9,30 @@ import SwiftUI
 
 struct List_ListStyle_Sidebar: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            HeaderView("List",
+                       subtitle: "List Style: Sidebar",
+                       desc: "Using the sidebar list style will remove the separator lines from the list.",
+                       back: .green,
+                       textColor: .black)
+            
+            List {
+                Text("What would you like to learn?")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                Label("Learn Geography", systemImage: "signpost.right.fill")
+                Label("Learn Music", systemImage: "doc.richtext")
+                Label("Learn Photography", systemImage: "camera.aperture")
+                Label("Learn Art", systemImage: "paintpalette.fill")
+                    .font(Font.system(.title3).weight(.bold))
+                Label("Learn Physics", systemImage: "atom")
+                Label("Learn 3D", systemImage: "cube.transparent")
+                Label("Learn Hair Styling", systemImage: "comb.fill")
+            }
+            .listStyle(.sidebar)
+            .tint(.green)
+        }
+        .font(.title)
     }
 }
 

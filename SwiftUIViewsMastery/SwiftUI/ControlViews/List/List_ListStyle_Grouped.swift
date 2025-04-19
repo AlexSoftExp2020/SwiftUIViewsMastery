c03698110cf00eb2eccda7ee126b97e7ce4ed0a0
Alex <oleksii.pidhornyi@gmail.com>
2025-04-19 16:24:23 +0200

    add list list style grouped

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/List/List_ListStyle_Grouped.swift
@@ -0,0 +1,18 @@
+//
+//  List_ListStyle_Grouped.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 19.04.2025.
+//
+
+import SwiftUI
+
+struct List_ListStyle_Grouped: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    List_ListStyle_Grouped()
+}

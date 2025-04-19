af1bafb1469b9dcf866ac803b1df8135eb044747
Alex <oleksii.pidhornyi@gmail.com>
2025-04-19 19:20:11 +0200

    add list safe area inset scroll view

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/List/List_SafeAreaInset_ScrollView.swift
@@ -0,0 +1,18 @@
+//
+//  List_SafeAreaInset_ScrollView.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 19.04.2025.
+//
+
+import SwiftUI
+
+struct List_SafeAreaInset_ScrollView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    List_SafeAreaInset_ScrollView()
+}

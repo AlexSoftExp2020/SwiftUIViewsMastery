06b4a270a642678e2bd6065ebeee807b32576d94
Alex <oleksii.pidhornyi@gmail.com>
2025-04-19 17:57:16 +0200

    extend list header footer sidebar

--- SwiftUIViewsMastery/SwiftUI/ControlViews/List/List_HeaderFooter_Sidebar.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/List/List_HeaderFooter_Sidebar.swift
@@ -9,7 +9,26 @@ import SwiftUI
 
 struct List_HeaderFooter_Sidebar: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            HeaderView("List",
+                       subtitle: "Sidebar - Headers & Footers",
+                       desc: "This list style also changes the way headers and footers are redered.",
+                       back: .green,
+                       textColor: .black)
+            List {
+                Section(header: Text("What would you like to learn?"),
+                        footer: Text("Count: 5")) {
+                    Label("Learn Geography", systemImage: "signpost.right.fill")
+                    Label("Learn Music", systemImage: "doc.richtext")
+                    Label("Learn Photography", systemImage: "camera.aperture")
+                    Label("Learn Art", systemImage: "paintpalette.fill")
+                    Label("Learn Physics", systemImage: "atom")
+                }
+            }
+            .listStyle(.sidebar)
+            .tint(.green)
+        }
+        .font(.title)
     }
 }
 

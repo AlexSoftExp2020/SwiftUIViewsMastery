db1c4abcabad570dd5c1c5bdff9293d0cf7af0c6
Alex <oleksii.pidhornyi@gmail.com>
2025-04-19 19:38:39 +0200

    add list row spacing

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/List/List_RowSpacing.swift
@@ -0,0 +1,18 @@
+//
+//  List_RowSpacing.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 19.04.2025.
+//
+
+import SwiftUI
+
+struct List_RowSpacing: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    List_RowSpacing()
+}

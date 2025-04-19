8c253ef39f152efb2c6fd3d9d85625f26634002f
Alex <oleksii.pidhornyi@gmail.com>
2025-04-19 19:30:23 +0200

    add to do item struct

--- SwiftUIViewsMastery/SwiftUI/ControlViews/List/List_BindingWithin.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/List/List_BindingWithin.swift
@@ -7,6 +7,13 @@
 
 import SwiftUI
 
+struct ToDoItem: Identifiable {
+    let id = UUID()
+    var task = ""
+    var priority = false
+    var done = false
+}
+
 struct List_BindingWithin: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)

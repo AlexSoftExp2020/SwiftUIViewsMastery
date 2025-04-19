e84876a641a623498cb720a9c31e1c8fc37c1f78
Alex <oleksii.pidhornyi@gmail.com>
2025-04-19 17:36:15 +0200

    add list header prominence

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/List/List_HeaderProminence.swift
@@ -0,0 +1,18 @@
+//
+//  List_HeaderProminence.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 19.04.2025.
+//
+
+import SwiftUI
+
+struct List_HeaderProminence: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    List_HeaderProminence()
+}

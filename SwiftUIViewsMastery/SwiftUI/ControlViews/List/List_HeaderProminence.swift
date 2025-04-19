f26eadbdcd07fc78804d3c4ed56983d282f3c3e8
Alex <oleksii.pidhornyi@gmail.com>
2025-04-19 17:44:14 +0200

    extend list header prominence

--- SwiftUIViewsMastery/SwiftUI/ControlViews/List/List_HeaderProminence.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/List/List_HeaderProminence.swift
@@ -9,7 +9,24 @@ import SwiftUI
 
 struct List_HeaderProminence: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            Section {
+                Text("You can also use header prominence to style the header.")
+                    .listRowBackground(Color.green)
+            } header: {
+                Text("Standard Header Prominence")
+            }
+            .headerProminence(.standard)
+            
+            Section {
+                Text("Use increased header prominence to make it stand out more.")
+                    .listRowBackground(Color.green)
+            } header: {
+                Text("Increased Header Prominence")
+            }
+            .headerProminence(.increased)
+        }
+        .font(.title)
     }
 }
 

39e0aa4b32c475469f026283c80feae87b3f61a4
Alex <oleksii.pidhornyi@gmail.com>
2025-04-19 17:07:53 +0200

    add list header footer intro

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/List/List_HeaderFooter_Intro.swift
@@ -0,0 +1,18 @@
+//
+//  List_HeaderFooter_Intro.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 19.04.2025.
+//
+
+import SwiftUI
+
+struct List_HeaderFooter_Intro: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    List_HeaderFooter_Intro()
+}

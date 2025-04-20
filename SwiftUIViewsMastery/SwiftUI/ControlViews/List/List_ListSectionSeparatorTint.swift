5539c17f6caddfe422b6fa3c6c1262174502055b
Alex <oleksii.pidhornyi@gmail.com>
2025-04-20 23:38:16 +0200

    extend list list section separator tint

--- SwiftUIViewsMastery/SwiftUI/ControlViews/List/List_ListSectionSeparatorTint.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/List/List_ListSectionSeparatorTint.swift
@@ -9,7 +9,36 @@ import SwiftUI
 
 struct List_ListSectionSeparatorTint: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 0) {
+            HeaderView("List",
+                       subtitle: "ListSectionSeparatorTint",
+                       desc: "Use the listSectionSeparatorTint to modify the color of the separator.")
+            
+            List {
+                Section {
+                    Text("Row 1")
+                    Text("Row 2")
+                    Text("Row 3")
+                    Text("Row 4")
+                } header: {
+                    Text("Section 1")
+                }
+                .listSectionSeparatorTint(.red)
+                
+                Section {
+                    Text("Row 1")
+                    Text("Row 2")
+                    Text("Row 3")
+                    Text("Row 4")
+                } header: {
+                    Text("Section 2")
+                }
+                .listSectionSeparatorTint(.green, edges: .bottom)
+                #warning("Top not working yet.")
+            }
+            .listStyle(.plain) // Bottom only
+        }
+        .font(.title)
     }
 }
 

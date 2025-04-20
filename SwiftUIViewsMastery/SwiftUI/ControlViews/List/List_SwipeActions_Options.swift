b0c6ef93898624170a170ab85ba8b10cdf5a2853
Alex <oleksii.pidhornyi@gmail.com>
2025-04-21 00:09:28 +0200

    add list swipe actions options

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/List/List_SwipeActions_Options.swift
@@ -0,0 +1,18 @@
+//
+//  List_SwipeActions_Options.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 21.04.2025.
+//
+
+import SwiftUI
+
+struct List_SwipeActions_Options: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    List_SwipeActions_Options()
+}

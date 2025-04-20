39b8d21c951533d886a65a00c6cdffd3de62096b
Alex <oleksii.pidhornyi@gmail.com>
2025-04-20 23:40:22 +0200

    add list refreshable

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/List/List_Refreshable.swift
@@ -0,0 +1,18 @@
+//
+//  List_Refreshable.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 20.04.2025.
+//
+
+import SwiftUI
+
+struct List_Refreshable: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    List_Refreshable()
+}

716d4e004065ee3a59b2ec2f5410bc5f9fb534e2
Alex <oleksii.pidhornyi@gmail.com>
2025-04-20 23:09:49 +0200

    add list list row separator tint

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/List/List_ListRowSeparatorTint.swift
@@ -0,0 +1,18 @@
+//
+//  List_ListRowSeparatorTint.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 20.04.2025.
+//
+
+import SwiftUI
+
+struct List_ListRowSeparatorTint: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    List_ListRowSeparatorTint()
+}

cb1be1e2e0bc56c15b8ccc2447ad13564e34c1a1
Alex <oleksii.pidhornyi@gmail.com>
2025-04-20 23:50:29 +0200

    add list swipe actions

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/List/List_SwipeActions.swift
@@ -0,0 +1,18 @@
+//
+//  List_SwipeActions.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 20.04.2025.
+//
+
+import SwiftUI
+
+struct List_SwipeActions: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    List_SwipeActions()
+}

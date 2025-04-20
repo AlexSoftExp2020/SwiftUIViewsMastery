604ceb350ae908f088fce25c4a2740b84664e0c2
Alex <oleksii.pidhornyi@gmail.com>
2025-04-20 23:48:26 +0200

    extend list refreshable

--- SwiftUIViewsMastery/SwiftUI/ControlViews/List/List_Refreshable.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/List/List_Refreshable.swift
@@ -8,8 +8,33 @@
 import SwiftUI
 
 struct List_Refreshable: View {
+    @State private var stringArray = ["Evans",
+                                      "Lemuel",
+                                      "Mark",
+                                      "Durtschi",
+                                      "Chase",
+                                      "Adam",
+                                      "Rodrigo"]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            Group {
+                Text(Image(systemName: "arrow.down")) +
+                Text(" Pull to Refresh ") +
+                Text(Image(systemName: "arrow.down"))
+            }
+            .frame(maxWidth: .infinity)
+            .foregroundStyle(.green)
+            
+            ForEach(stringArray, id: \.self) { string in
+                Text(string)
+            }
+        }
+        .font(.title)
+        .refreshable {
+            // Perform some action
+            stringArray.append("👉 New Name")
+        }
     }
 }
 

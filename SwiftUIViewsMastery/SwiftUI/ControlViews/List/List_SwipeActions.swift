fd4dc6eb6da9d9208b41240b95dc116db7078cfa
Alex <oleksii.pidhornyi@gmail.com>
2025-04-21 00:07:52 +0200

    extend list swipe actions

--- SwiftUIViewsMastery/SwiftUI/ControlViews/List/List_SwipeActions.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/List/List_SwipeActions.swift
@@ -8,8 +8,30 @@
 import SwiftUI
 
 struct List_SwipeActions: View {
+    @State private var items = [ToDoItem(task: "Get milk", done: false),
+                                ToDoItem(task: "Wash car", done: true),
+                                ToDoItem(task: "Get milk", done: false)]
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20.0) {
+            HeaderView("List",
+                       subtitle: "SwipeActions",
+                       desc: "Use swipeActions to expose additional functionality on repeated items in a List.")
+            List {
+                ForEach($items) { $item in
+                    Text(item.task)
+                        .strikethrough(item.done)
+                        .swipeActions {
+                            Button {
+                                item.done.toggle()
+                            } label: {
+                                Image(systemName: item.done ? "arrow.uturn.backward.square" : "checkmark.square")
+                            }
+                            .tint(item.done ? .blue : .green)
+                        }
+                }
+            }
+        }
+        .font(.title)
     }
 }
 

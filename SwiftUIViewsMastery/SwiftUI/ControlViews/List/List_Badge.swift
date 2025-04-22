c6426247acf63104558494c79ee2f92945649a06
Alex <oleksii.pidhornyi@gmail.com>
2025-04-22 14:07:06 +0200

    extend list badge

--- SwiftUIViewsMastery/SwiftUI/ControlViews/List/List_Badge.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/List/List_Badge.swift
@@ -9,7 +9,32 @@ import SwiftUI
 
 struct List_Badge: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20.0) {
+            List {
+                Text("Row 0")
+                    .badge(0)
+                Text("Row 0")
+                    .badge(Text("0"))
+                Text("Row 1")
+                    .badge(1)
+                    .foregroundStyle(.red)
+                Text("Row 2")
+                    .badge(2)
+                    .font(.title3.weight(.bold))
+                Text("Row 2")
+                    .badge(Text("2").font(.title3.weight(.bold)))
+                    .font(.title3.weight(.bold))
+                Text("Row 3")
+                    .badge(3)
+                    .badgeProminence(.decreased)
+                Text("Row 4")
+                    .badge(4)
+                    .badgeProminence(.standard)
+                Text("Row 5")
+                    .badge(5)
+                    .badgeProminence(.increased)
+            }
+        }
     }
 }
 

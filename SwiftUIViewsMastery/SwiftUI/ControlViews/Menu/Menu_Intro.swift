15df19080935153454ed7f3638bb3cedb853f91e
Alex <oleksii.pidhornyi@gmail.com>
2025-04-22 14:53:40 +0200

    extend menu intro

--- SwiftUIViewsMastery/SwiftUI/ControlViews/Menu/Menu_Intro.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Menu/Menu_Intro.swift
@@ -9,7 +9,28 @@ import SwiftUI
 
 struct Menu_Intro: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            HeaderView("Menu",
+                       subtitle: "Introduction",
+                       desc: "Use the Menu view to group up similiar actions to present to the user from a tappable button.",
+                       back: .blue,
+                       textColor: .white)
+            Spacer()
+            
+            Menu("Edit") {
+                Button { } label: {
+                    Text("Add color")
+                    Image(systemName: "eyedropper.full")
+                }
+                Button { } label: {
+                    Image(systemName: "circle.lefthalf.fill")
+                        .foregroundStyle(.blue) // No effect
+                    Text("Change contrast")
+                }
+            }
+            Spacer()
+        }
+        .font(.title)
     }
 }
 

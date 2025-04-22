35cf92fdfc9b206295e35655f7eb95c423f54037
Alex <oleksii.pidhornyi@gmail.com>
2025-04-22 14:56:28 +0200

    add menu inside menu

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Menu/Menu_InsideMenu.swift
@@ -0,0 +1,18 @@
+//
+//  Menu_InsideMenu.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 22.04.2025.
+//
+
+import SwiftUI
+
+struct Menu_InsideMenu: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Menu_InsideMenu()
+}

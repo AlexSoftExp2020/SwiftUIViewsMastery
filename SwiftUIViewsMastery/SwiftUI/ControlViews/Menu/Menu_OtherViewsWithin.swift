f3437300ea28c8651ace73ce446ef4636857a447
Alex <oleksii.pidhornyi@gmail.com>
2025-04-22 16:34:16 +0200

    add menu other views within

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Menu/Menu_OtherViewsWithin.swift
@@ -0,0 +1,18 @@
+//
+//  Menu_OtherViewsWithin.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 22.04.2025.
+//
+
+import SwiftUI
+
+struct Menu_OtherViewsWithin: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Menu_OtherViewsWithin()
+}

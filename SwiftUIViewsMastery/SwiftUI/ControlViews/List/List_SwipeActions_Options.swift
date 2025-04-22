95af5405e17337f981757b2dd75c56a631c08db8
Alex <oleksii.pidhornyi@gmail.com>
2025-04-22 13:53:26 +0200

    extend list swipe actions options

--- SwiftUIViewsMastery/SwiftUI/ControlViews/List/List_SwipeActions_Options.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/List/List_SwipeActions_Options.swift
@@ -9,7 +9,38 @@ import SwiftUI
 
 struct List_SwipeActions_Options: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20.0) {
+            HeaderView("List",
+                       subtitle: "SwipeActions - Options",
+                       desc: "You can specify which side to swipe with the edge parameter and if you want a full swipe.").layoutPriority(1)
+            List {
+                ForEach(0..<3) { item in
+                    Text("Item \(item)")
+                        .swipeActions(allowsFullSwipe: false) {
+                            Button { } label: {
+                                Image(systemName: "xmark")
+                            }
+                            .tint(.red)
+                            Button { } label: {
+                                Image(systemName: "checkmark.square")
+                            }
+                            .tint(.green)
+                        }
+                }
+            }
+            List {
+                ForEach(3..<6) { item in
+                    Text("Item \(item)")
+                        .swipeActions(edge: .leading, allowsFullSwipe: true) {
+                            Button { } label: {
+                                Image(systemName: "flag")
+                            }
+                            .tint(.purple)
+                        }
+                }
+            }
+        }
+        .font(.title)
     }
 }
 

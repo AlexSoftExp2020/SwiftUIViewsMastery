93e441d156213986011abfe7c3eb3fda8c756692
Alex <oleksii.pidhornyi@gmail.com>
2025-04-22 14:09:57 +0200

    add list scroll content background

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/List/List_ScrollContentBackground.swift
@@ -0,0 +1,18 @@
+//
+//  List_ScrollContentBackground.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 22.04.2025.
+//
+
+import SwiftUI
+
+struct List_ScrollContentBackground: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    List_ScrollContentBackground()
+}

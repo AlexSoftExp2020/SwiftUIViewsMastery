363e98b33763f779a31139ad39c3ec9cc2c20e76
Alex <oleksii.pidhornyi@gmail.com>
2025-04-22 14:43:20 +0200

    add menu intro

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Menu/Menu_Intro.swift
@@ -0,0 +1,18 @@
+//
+//  Menu_Intro.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 22.04.2025.
+//
+
+import SwiftUI
+
+struct Menu_Intro: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Menu_Intro()
+}

81281a1768cfba46f8c562b98db4c7f187b9b977
Alex <oleksii.pidhornyi@gmail.com>
2025-04-22 15:42:31 +0200

    extend menu inside menu

--- SwiftUIViewsMastery/SwiftUI/ControlViews/Menu/Menu_InsideMenu.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Menu/Menu_InsideMenu.swift
@@ -9,7 +9,38 @@ import SwiftUI
 
 struct Menu_InsideMenu: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            HeaderView("Menu",
+                       subtitle: "Inner Menu",
+                       desc: "You can also have a Menu inside of a Menu.",
+                       back: .blue,
+                       textColor: .white)
+            
+            Spacer()
+            
+            Menu("Edit") {
+                Button { } label: {
+                    Text("Add color")
+                    Image(systemName: "eyedropper.full")
+                }
+                Button { } label: {
+                    Image(systemName: "circle.lefthalf.fill")
+                    Text("Change contrast")
+                }
+                Menu("Menu...") {
+                    Button { } label: {
+                        Text("Magic Wand")
+                        Image(systemName: "wand.and.stars")
+                    }
+                    Button { } label: {
+                        Image(systemName: "paintbrush.fill")
+                        Text("Magic Paint")
+                    }
+                }
+            }
+            Spacer()
+        }
+        .font(.title)
     }
 }
 

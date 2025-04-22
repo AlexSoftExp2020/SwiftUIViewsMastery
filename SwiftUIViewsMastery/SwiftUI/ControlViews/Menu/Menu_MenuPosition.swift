30ae08f9921bb250b5bdea20dff7c62283214675
Alex <oleksii.pidhornyi@gmail.com>
2025-04-22 16:10:57 +0200

    extend menu menu position

--- SwiftUIViewsMastery/SwiftUI/ControlViews/Menu/Menu_MenuPosition.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Menu/Menu_MenuPosition.swift
@@ -9,7 +9,34 @@ import SwiftUI
 
 struct Menu_MenuPosition: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            HStack {
+                Menu {
+                    Button { } label: {
+                        Text("Menu Item 1")
+                        Image(systemName: "network")
+                    }
+                    Button { } label: {
+                        Text("Menu Item 2")
+                        Image(systemName: "antenna.radiowaves.left.and.right")
+                            .padding(.leading, 20)
+                    }
+                } label: {
+                    Image(systemName: "slider.horizontal.3")
+                        .padding(.leading, 20)
+                }
+                Spacer()
+            }
+            Spacer()
+            
+            HeaderView("Menu",
+                       subtitle: "Menu Position",
+                       desc: "You may notice that the menu seems to always expand upward and you menu items are backwards. If the menu label (button) is toward the top of the screen, then the menu wil expand down from the button.",
+                       back: .blue,
+                       textColor: .white)
+            Spacer()
+        }
+        .font(.title)
     }
 }
 

d10f823ec8100878e23202eb517c12b0352a251f
Alex <oleksii.pidhornyi@gmail.com>
2025-04-23 14:52:34 +0200

    add menu order

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Menu/Menu_Order.swift
@@ -0,0 +1,18 @@
+//
+//  Menu_Order.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 23.04.2025.
+//
+
+import SwiftUI
+
+struct Menu_Order: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Menu_Order()
+}

e3283da3ec2387af880df0503844f380f49bd738
Alex <oleksii.pidhornyi@gmail.com>
2025-04-23 14:33:40 +0200

    extend menu other views within

--- SwiftUIViewsMastery/SwiftUI/ControlViews/Menu/Menu_OtherViewsWithin.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Menu/Menu_OtherViewsWithin.swift
@@ -9,7 +9,38 @@ import SwiftUI
 
 struct Menu_OtherViewsWithin: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            HStack {
+                Menu {
+                    Button("Menu Item 1", action: {})
+                    Divider()
+                    Button(action: {}) {
+                        Text("Dividers create a thicker separator")
+                        Image(systemName: "hand.point.up")
+                        }
+                    Divider()
+                    Text("This is a text view")
+                        .font(.caption) // No effect
+                        .textCase(.uppercase)
+                    Button(action: {}) {
+                        Text("Menu text will use up to two lines if too long")
+                    }
+                } label: {
+                    Image(systemName: "slider.horizontal.3")
+                        .padding(.leading, 20)
+                }
+                Spacer()
+            }
+            Spacer()
+            
+            HeaderView("Menu",
+                       subtitle: "Other Views Within",
+                       desc: "You can include other views not just buttons, inside the menu.",
+                       back: .blue,
+                       textColor: .white)
+            Spacer()
+        }
+        .font(.title)
     }
 }
 

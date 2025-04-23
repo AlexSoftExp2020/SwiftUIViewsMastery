0b16d666e0313a8e24f9fcaf2ad96cbade802ef4
Alex <oleksii.pidhornyi@gmail.com>
2025-04-23 14:42:24 +0200

    add menu with sections

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Menu/Menu_WithSections.swift
@@ -0,0 +1,18 @@
+//
+//  Menu_WithSections.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 23.04.2025.
+//
+
+import SwiftUI
+
+struct Menu_WithSections: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Menu_WithSections()
+}

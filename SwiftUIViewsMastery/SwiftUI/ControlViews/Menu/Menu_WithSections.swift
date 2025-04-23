afb6ba3394e784228f000fe9f0553f6638d7d4de
Alex <oleksii.pidhornyi@gmail.com>
2025-04-23 14:51:16 +0200

    extend menu with sections

--- SwiftUIViewsMastery/SwiftUI/ControlViews/Menu/Menu_WithSections.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Menu/Menu_WithSections.swift
@@ -9,7 +9,33 @@ import SwiftUI
 
 struct Menu_WithSections: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            HeaderView("Menu",
+                       subtitle: "With Sections",
+                       desc: "You can also use Sections to divide up your menu items.",
+                       back: .blue,
+                       textColor: .white)
+            Menu("Edit") {
+                Section {
+                    Button("Menu Item 1", action: {})
+                    Button("Menu Item 2", action: {})
+                    Button("Menu Item 3", action: {})
+                    Button("Menu Item 4", action: {})
+                    Button("Menu Item 5", action: {})
+                }
+                
+                Section {
+                    Button("Menu Item 6", action: {})
+                    Button("Menu Item 7", action: {})
+                    Button("Menu Item 8", action: {})
+                    Button("Menu Item 9", action: {})
+                    Button("Menu Item 10", action: {})
+                    Button("Menu Item 11", action: {})
+                }
+            }
+            Spacer()
+        }
+        .font(.title)
     }
 }
 

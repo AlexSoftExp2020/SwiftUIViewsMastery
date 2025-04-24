b707523d31103a43b02e34d654d58eaf08bf3d49
Alex <oleksii.pidhornyi@gmail.com>
2025-04-24 16:02:11 +0200

    add navigation custom background

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/NavigationStack/Navigation_CustomBackground.swift
@@ -0,0 +1,18 @@
+//
+//  Navigation_CustomBackground.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 24.04.2025.
+//
+
+import SwiftUI
+
+struct Navigation_CustomBackground: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Navigation_CustomBackground()
+}

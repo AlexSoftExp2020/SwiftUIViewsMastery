eedb40fe392e738120f35b77387cbadacb14e7d2
Alex <oleksii.pidhornyi@gmail.com>
2025-04-24 16:46:03 +0200

    add navigation bar hidden

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/NavigationStack/Navigation_BarHidden.swift
@@ -0,0 +1,18 @@
+//
+//  Navigation_BarHidden.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 24.04.2025.
+//
+
+import SwiftUI
+
+struct Navigation_BarHidden: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Navigation_BarHidden()
+}

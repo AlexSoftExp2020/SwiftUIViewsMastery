3d1fb9cb7ce175dd24889bd00937c93ac52fa493
Alex <oleksii.pidhornyi@gmail.com>
2025-04-24 14:58:53 +0200

    add multi date picker presenting in sheet

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/MultiDatePicker/MultiDatePicker_PresentingInSheet.swift
@@ -0,0 +1,18 @@
+//
+//  MultiDatePicker_PresentingInSheet.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 24.04.2025.
+//
+
+import SwiftUI
+
+struct MultiDatePicker_PresentingInSheet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MultiDatePicker_PresentingInSheet()
+}

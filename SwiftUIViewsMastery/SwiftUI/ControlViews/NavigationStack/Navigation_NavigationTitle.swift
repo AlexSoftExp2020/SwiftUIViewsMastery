12673e53b8edf2dac96fea462b8a15952f2b7b58
Alex <oleksii.pidhornyi@gmail.com>
2025-04-24 15:31:44 +0200

    add navigation navigation title

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/NavigationStack/Navigation_NavigationTitle.swift
@@ -0,0 +1,18 @@
+//
+//  Navigation_NavigationTitle.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 24.04.2025.
+//
+
+import SwiftUI
+
+struct Navigation_NavigationTitle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Navigation_NavigationTitle()
+}

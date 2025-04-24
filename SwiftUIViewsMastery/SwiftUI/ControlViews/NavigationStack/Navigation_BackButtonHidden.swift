2f46d70be0bf3baff94f82c372b940c93c714583
Alex <oleksii.pidhornyi@gmail.com>
2025-04-24 20:36:58 +0200

    add navigation back button hidden

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/NavigationStack/Navigation_BackButtonHidden.swift
@@ -0,0 +1,18 @@
+//
+//  Navigation_BackButtonHidden.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 24.04.2025.
+//
+
+import SwiftUI
+
+struct Navigation_BackButtonHidden: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Navigation_BackButtonHidden()
+}

48e34a423c2a829ec368a6600ca09c0c3dc9c02c
Alex <oleksii.pidhornyi@gmail.com>
2025-04-24 16:30:13 +0200

    add navigation display mode

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/NavigationStack/Navigation_DisplayMode.swift
@@ -0,0 +1,18 @@
+//
+//  Navigation_DisplayMode.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 24.04.2025.
+//
+
+import SwiftUI
+
+struct Navigation_DisplayMode: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Navigation_DisplayMode()
+}

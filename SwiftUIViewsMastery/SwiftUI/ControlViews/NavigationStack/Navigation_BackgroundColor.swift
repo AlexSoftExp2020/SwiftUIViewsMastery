f8815ce13bef0aba4fa0949e595ecc05fc9047f7
Alex <oleksii.pidhornyi@gmail.com>
2025-04-24 15:37:55 +0200

    add navigation background color

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/NavigationStack/Navigation_BackgroundColor.swift
@@ -0,0 +1,18 @@
+//
+//  Navigation_BackgroundColor.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 24.04.2025.
+//
+
+import SwiftUI
+
+struct Navigation_BackgroundColor: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Navigation_BackgroundColor()
+}

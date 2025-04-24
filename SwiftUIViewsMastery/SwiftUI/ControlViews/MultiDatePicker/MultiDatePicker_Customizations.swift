dcd3d48b3d282251fa2dc26ef13c028fea4f3535
Alex <oleksii.pidhornyi@gmail.com>
2025-04-24 14:55:25 +0200

    extend multi date picker customizations

--- SwiftUIViewsMastery/SwiftUI/ControlViews/MultiDatePicker/MultiDatePicker_Customizations.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/MultiDatePicker/MultiDatePicker_Customizations.swift
@@ -8,8 +8,17 @@
 import SwiftUI
 
 struct MultiDatePicker_Customizations: View {
+    @State private var dates: Set<DateComponents> = [
+        DateComponents(year: 2022, month: 9, day: 6),
+        DateComponents(year: 2022, month: 9, day: 7),
+        DateComponents(year: 2022, month: 9, day: 8)
+    ]
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        MultiDatePicker("Customizations", selection: $dates)
+            .tint(.red)
+            .foregroundStyle(.purple)
+            .background(.quaternary.opacity(0.5), in: RoundedRectangle(cornerRadius: 16))
+            .padding()
     }
 }
 

da04703a2ae3fe8d96aa8354ca02ce4576538aeb
Alex <oleksii.pidhornyi@gmail.com>
2025-04-24 15:16:02 +0200

    extend multi date picker presenting in sheet

--- SwiftUIViewsMastery/SwiftUI/ControlViews/MultiDatePicker/MultiDatePicker_PresentingInSheet.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/MultiDatePicker/MultiDatePicker_PresentingInSheet.swift
@@ -8,8 +8,43 @@
 import SwiftUI
 
 struct MultiDatePicker_PresentingInSheet: View {
+    @State private var showDates = false
+    @State private var dates: Set<DateComponents> = []
+    
+    private var datesArray: [String] {
+        dates.sorted{date1, date2 in
+            date1.date! < date2.date!
+        }.map { date in
+            "\(date.month!)/\(date.day!)"
+        }
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            LabeledContent("Dates:") {
+                Button {
+                    showDates = true
+                } label: {
+                    if dates.count == 0 {
+                        Text("Select")
+                    } else {
+                        Text(datesArray, format: .list(type: .and, width: .short))
+                    }
+                }
+            }
+            .padding()
+            
+            Spacer()
+        }
+        .sheet(isPresented: $showDates) {
+            VStack {
+                MultiDatePicker("Customizations", selection: $dates)
+                Button("Done") { showDates = false }
+                    .buttonStyle(.borderedProminent)
+            }
+            .presentationDetents([.medium])
+        }
+        .font(.title)
     }
 }
 

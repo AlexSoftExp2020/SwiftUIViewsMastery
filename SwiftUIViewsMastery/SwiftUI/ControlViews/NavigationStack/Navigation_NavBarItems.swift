3dca86acc32819df54a543e45600247b83b51683
Alex <oleksii.pidhornyi@gmail.com>
2025-04-24 16:52:36 +0200

    add navigation nav bar items

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/NavigationStack/Navigation_NavBarItems.swift
@@ -0,0 +1,18 @@
+//
+//  Navigation_NavBarItems.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 24.04.2025.
+//
+
+import SwiftUI
+
+struct Navigation_NavBarItems: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Navigation_NavBarItems()
+}

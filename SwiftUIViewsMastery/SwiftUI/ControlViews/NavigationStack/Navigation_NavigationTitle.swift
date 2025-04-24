8b3da5581c078fd30e3188928e8c4623c1ae22b1
Alex <oleksii.pidhornyi@gmail.com>
2025-04-24 15:35:36 +0200

    extend navigation navigation title

--- SwiftUIViewsMastery/SwiftUI/ControlViews/NavigationStack/Navigation_NavigationTitle.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/NavigationStack/Navigation_NavigationTitle.swift
@@ -9,7 +9,11 @@ import SwiftUI
 
 struct Navigation_NavigationTitle: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            Text("😄")
+                .navigationTitle("Navigation Stack Title")
+        }
+        .font(.title)
     }
 }
 

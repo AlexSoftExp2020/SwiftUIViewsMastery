1f9bbcd287ae4ff0fa4ea841c5c6e50c0de3dbd1
Alex <oleksii.pidhornyi@gmail.com>
2025-04-24 22:59:02 +0200

    extend navigation custom back button detail

--- SwiftUIViewsMastery/SwiftUI/ControlViews/NavigationStack/CustomBackButton/Navigation_CustomBackButton_Detail.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/NavigationStack/CustomBackButton/Navigation_CustomBackButton_Detail.swift
@@ -8,8 +8,21 @@
 import SwiftUI
 
 struct Navigation_CustomBackButton_Detail: View {
+    @Environment(\.dismiss) var dismiss
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack { }
+            .navigationTitle("Detail VIew")
+            .navigationBarBackButtonHidden(true)
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button {
+                        dismiss()
+                    } label: {
+                        Label("Back", systemImage: "arrow.left.circle")
+                    }
+                }
+            }
     }
 }
 

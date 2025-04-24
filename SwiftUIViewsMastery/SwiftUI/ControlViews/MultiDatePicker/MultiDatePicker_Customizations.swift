c462f22512b99e8458da162a0ce154b2b5fbc442
Alex <oleksii.pidhornyi@gmail.com>
2025-04-24 14:47:46 +0200

    add multi date picker customizations

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/MultiDatePicker/MultiDatePicker_Customizations.swift
@@ -0,0 +1,18 @@
+//
+//  MultiDatePicker_Customizations.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 24.04.2025.
+//
+
+import SwiftUI
+
+struct MultiDatePicker_Customizations: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MultiDatePicker_Customizations()
+}

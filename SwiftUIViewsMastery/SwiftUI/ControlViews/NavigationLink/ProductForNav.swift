5cb0fb6d6789976746b3778f8a7c81d76bdc7a7d
Alex <oleksii.pidhornyi@gmail.com>
2025-04-26 00:18:55 +0200

    add product for nav

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/NavigationLink/ProductForNav.swift
@@ -0,0 +1,13 @@
+//
+//  ProductForNav.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 26.04.2025.
+//
+
+import SwiftUI
+
+struct ProductForNav: Hashable {
+    var name = ""
+    var price = 0.0
+}

75f324bdd21493411af9b38a856ae811b9d81611
Alex <oleksii.pidhornyi@gmail.com>
2025-04-25 23:21:54 +0200

    add nav link with different types

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/NavigationLink/NavLink_WithDifferentTypes.swift
@@ -0,0 +1,18 @@
+//
+//  NavLink_WithDifferentTypes.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 25.04.2025.
+//
+
+import SwiftUI
+
+struct NavLink_WithDifferentTypes: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NavLink_WithDifferentTypes()
+}

c5a339175a251fe60d73c34d93282bddf51c9a76
Alex <oleksii.pidhornyi@gmail.com>
2025-04-26 00:39:06 +0200

    extend nav with navigation path

--- SwiftUIViewsMastery/SwiftUI/ControlViews/NavigationLink/Nav_WithNavigationPath.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/NavigationLink/Nav_WithNavigationPath.swift
@@ -8,8 +8,34 @@
 import SwiftUI
 
 struct Nav_WithNavigationPath: View {
+    @State private var product = ProductForNav(name: "Mouse", price: 24.99)
+    @State private var cc = CreditCardForNav(number: "5111 1111 1111 1111", expiration: "02/28")
+    @State private var navPath = NavigationPath()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack(path: $navPath) {
+            Form {
+                NavigationLink(value: product) { Text(product.name) }
+                NavigationLink(value: cc) { Text(cc.number) }
+            }
+            .navigationTitle("Order")
+            .navigationDestination(for: ProductForNav.self) { product in
+                Form {
+                    Text(product.name)
+                    Text(product.price, format: .currency(code: "USD"))
+                        .foregroundStyle(.secondary)
+                }
+                .navigationTitle("Product Details")
+            }
+            .navigationDestination(for: CreditCardForNav.self) { cc in
+                Form {
+                    Text(cc.number)
+                    Text(cc.expiration).foregroundStyle(.secondary)
+                }
+                .navigationTitle("Credit Card Details")
+            }
+        }
+        .font(.title)
     }
 }
 

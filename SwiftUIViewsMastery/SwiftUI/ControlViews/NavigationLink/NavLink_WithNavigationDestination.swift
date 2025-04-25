040c6f7e434b581aad74ce076eda9c67550c3fed
Alex <oleksii.pidhornyi@gmail.com>
2025-04-25 22:30:16 +0200

    add nav link with navigation destination

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/NavigationLink/NavLink_WithNavigationDestination.swift
@@ -0,0 +1,18 @@
+//
+//  NavLink_WithNavigationDestination.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 25.04.2025.
+//
+
+import SwiftUI
+
+struct NavLink_WithNavigationDestination: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NavLink_WithNavigationDestination()
+}

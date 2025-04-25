db7cecf9ead2a63ac4922b940eb7ef2e328da326
Alex <oleksii.pidhornyi@gmail.com>
2025-04-25 22:08:00 +0200

    extend nav link intro

--- SwiftUIViewsMastery/SwiftUI/ControlViews/NavigationLink/NavLink_Intro.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/NavigationLink/NavLink_Intro.swift
@@ -9,7 +9,19 @@ import SwiftUI
 
 struct NavLink_Intro: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack(spacing: 20) {
+                NavigationLink("Just Text", destination: SecondView())
+                
+                NavigationLink {
+                    SecondView()
+                } label: {
+                    Label("Label", systemImage: "doc.text.fill")
+                }
+            }
+            .navigationTitle("NavigationLink")
+        }
+        .font(.title)
     }
 }
 

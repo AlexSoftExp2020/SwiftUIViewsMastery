c9c82ab8ed7561ff215104bf4dd331948d5882e5
Alex <oleksii.pidhornyi@gmail.com>
2025-04-26 00:04:20 +0200

    add navigation controller

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/NavigationLink/NavigationController.swift
@@ -0,0 +1,18 @@
+//
+//  NavigationController.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 26.04.2025.
+//
+
+import SwiftUI
+
+struct NavigationController: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NavigationController()
+}

9b7317f6496a425c192d63ca48e2d3118c364506
Alex <oleksii.pidhornyi@gmail.com>
2025-04-25 22:09:44 +0200

    add second view

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/NavigationLink/SecondView.swift
@@ -0,0 +1,18 @@
+//
+//  SecondView.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 25.04.2025.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SecondView()
+}

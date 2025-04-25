df5b49dfbd3158a39f611234fbd22e857f79e92b
Alex <oleksii.pidhornyi@gmail.com>
2025-04-25 23:51:28 +0200

    add screens enum

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/NavigationLink/Nav_WithPathAndEnum.swift
@@ -0,0 +1,24 @@
+//
+//  Nav_WithPathAndEnum.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 25.04.2025.
+//
+
+import SwiftUI
+
+enum Screens {
+    case screen1
+    case screen2
+    case screen3
+}
+
+struct Nav_WithPathAndEnum: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Nav_WithPathAndEnum()
+}

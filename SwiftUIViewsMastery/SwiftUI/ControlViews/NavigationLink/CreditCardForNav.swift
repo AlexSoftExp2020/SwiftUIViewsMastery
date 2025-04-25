0bc6e8497c35cf47c68f54fe49aee8b4bf45111e
Alex <oleksii.pidhornyi@gmail.com>
2025-04-26 00:23:19 +0200

    add credit card for nav

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/NavigationLink/CreditCardForNav.swift
@@ -0,0 +1,13 @@
+//
+//  CreditCardForNav.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 26.04.2025.
+//
+
+import SwiftUI
+
+struct CreditCardForNav: Hashable {
+    var number = ""
+    var expiration = ""
+}

56a7d054ee84e0262c08452dbf388b9df9c3d92e
Alex <oleksii.pidhornyi@gmail.com>
2025-04-26 00:40:26 +0200

    add nav link pop to root

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/NavigationLink/NavLink_PopToRoot.swift
@@ -0,0 +1,18 @@
+//
+//  NavLink_PopToRoot.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 26.04.2025.
+//
+
+import SwiftUI
+
+struct NavLink_PopToRoot: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NavLink_PopToRoot()
+}

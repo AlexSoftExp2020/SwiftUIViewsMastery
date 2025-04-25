045d7540362e9250204033fd22c6020f7e7240ac
Alex <oleksii.pidhornyi@gmail.com>
2025-04-26 00:24:24 +0200

    add nav with navigation path

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/NavigationLink/Nav_WithNavigationPath.swift
@@ -0,0 +1,18 @@
+//
+//  Nav_WithNavigationPath.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 26.04.2025.
+//
+
+import SwiftUI
+
+struct Nav_WithNavigationPath: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Nav_WithNavigationPath()
+}

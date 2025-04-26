a9d5d5112b1280921ad59624958c45a7be53fe5b
Alex <oleksii.pidhornyi@gmail.com>
2025-04-26 23:49:14 +0200

    extend outline group intro

--- SwiftUIViewsMastery/SwiftUI/ControlViews/OutlineGroup/OutlineGroup_Intro.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/OutlineGroup/OutlineGroup_Intro.swift
@@ -8,8 +8,36 @@
 import SwiftUI
 
 struct OutlineGroup_Intro: View {
+    var parents = [Parent1(name: "Mark",
+                          children: [Parent1(name: "Paola")]),
+                   Parent1(name: "Rodrigo",
+                          children: [Parent1(name: "Kai"),
+                                     Parent1(name: "Brennan"),
+                                     Parent1(name: "Easton")]),
+                   Parent1(name: "Marcella",
+                           children: [Parent1(name: "Sam",
+                                              children: [Parent1(name: "Joe")]),
+                                     Parent1(name: "Melissa"),
+                                     Parent1(name: "Melanie")])]
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20.0) {
+            HeaderView("OutlineGroup",
+                       subtitle: "Introduction",
+                       desc: "This is very similar to using the List with the children parameter except this container does not scroll.",
+                       back: Color.red,
+                       textColor: .primary)
+            
+            OutlineGroup(parents, children: \.children) { parent in
+                HStack {
+                    Image(systemName: "person.circle")
+                    Text("\(parent.name)")
+                    Spacer()
+                }
+            }
+            .padding(.horizontal)
+            .tint(.red)
+        }
+        .font(.title)
     }
 }
 

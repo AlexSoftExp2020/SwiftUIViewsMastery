5119fa8b981e620d67a4d248dee1f98339306f80
Alex <oleksii.pidhornyi@gmail.com>
2025-04-26 21:41:18 +0200

    extend detail link

--- SwiftUIViewsMastery/SwiftUI/ControlViews/NavigationLink/DetailLink.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/NavigationLink/DetailLink.swift
@@ -9,7 +9,16 @@ import SwiftUI
 
 struct DetailLink: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationSplitView {
+            VStack(spacing: 20) {
+                NavigationLink("Navigate There ->") {
+                    // MARK: TODO
+                }
+            }
+        } detail: {
+            <#code#>
+        }
+
     }
 }
 

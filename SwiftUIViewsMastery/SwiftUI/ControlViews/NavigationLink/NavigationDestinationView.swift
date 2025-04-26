cb0a404641dfe2dd236d9617e589b8d6739143a1
Alex <oleksii.pidhornyi@gmail.com>
2025-04-26 21:42:37 +0200

    add navigation destination view

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/NavigationLink/NavigationDestinationView.swift
@@ -0,0 +1,18 @@
+//
+//  NavigationDestinationView.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 26.04.2025.
+//
+
+import SwiftUI
+
+struct NavigationDestinationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NavigationDestinationView()
+}

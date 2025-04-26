fa2e491315fc9b8c966c9e214cbd4a7b7c1c1915
Alex <oleksii.pidhornyi@gmail.com>
2025-04-26 21:37:27 +0200

    add detail link

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/NavigationLink/DetailLink.swift
@@ -0,0 +1,18 @@
+//
+//  DetailLink.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 26.04.2025.
+//
+
+import SwiftUI
+
+struct DetailLink: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailLink()
+}

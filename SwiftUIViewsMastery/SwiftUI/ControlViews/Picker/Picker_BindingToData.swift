766080e60145f10bbecc6ac9c50745b4d878c9a6
Alex <oleksii.pidhornyi@gmail.com>
2025-04-27 14:45:34 +0200

    extend picker binding to data

--- SwiftUIViewsMastery/SwiftUI/ControlViews/Picker/Picker_BindingToData.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Picker/Picker_BindingToData.swift
@@ -8,8 +8,26 @@
 import SwiftUI
 
 struct Picker_BindingToData: View {
+    @State private var youTuberName = "Mark"
+    var youTubers = ["Sean", "Chris", "Mark", "Scott", "Paul"]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            HeaderView("Picker",
+                       subtitle: "Binding to Data",
+                       desc: "Use a ForEach with your Picker view to populate it with data.")
+            
+            Text("Who do you want to watch today?")
+            
+            Picker(selection: $youTuberName, label: Text("")) {
+                ForEach(youTubers, id: \.self) { name in
+                    Row(name: name)
+                }
+            }
+            .pickerStyle(.wheel)
+            Text("Selected: \(youTuberName)")
+        }
+        .font(.title)
     }
 }
 

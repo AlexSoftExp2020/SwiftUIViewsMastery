5c044c77d9ce801e46a302893b7f88fdf96d0859
Alex <oleksii.pidhornyi@gmail.com>
2025-04-27 13:47:54 +0200

    add picker wheel

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Picker/Picker_Wheel.swift
@@ -0,0 +1,18 @@
+//
+//  Picker_Wheel.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 27.04.2025.
+//
+
+import SwiftUI
+
+struct Picker_Wheel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Picker_Wheel()
+}

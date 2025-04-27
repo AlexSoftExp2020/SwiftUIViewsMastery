4d8bd8672d8c2444755d0891260811e29e711f76
Alex <oleksii.pidhornyi@gmail.com>
2025-04-27 14:06:37 +0200

    add picker customized

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Picker/Picker_Customized.swift
@@ -0,0 +1,18 @@
+//
+//  Picker_Customized.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 27.04.2025.
+//
+
+import SwiftUI
+
+struct Picker_Customized: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Picker_Customized()
+}

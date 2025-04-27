e30d75cd151a2ca2144fc6d330a0a68dc6bb3fcc
Alex <oleksii.pidhornyi@gmail.com>
2025-04-27 08:41:06 +0200

    add picker intro

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Picker/Picker_Intro.swift
@@ -0,0 +1,18 @@
+//
+//  Picker_Intro.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 27.04.2025.
+//
+
+import SwiftUI
+
+struct Picker_Intro: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Picker_Intro()
+}

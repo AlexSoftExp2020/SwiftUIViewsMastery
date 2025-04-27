aef8260e47f965fbc0729361713cdccd01e05867
Alex <oleksii.pidhornyi@gmail.com>
2025-04-27 14:18:13 +0200

    extend picker customized

--- SwiftUIViewsMastery/SwiftUI/ControlViews/Picker/Picker_Customized.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Picker/Picker_Customized.swift
@@ -8,8 +8,35 @@
 import SwiftUI
 
 struct Picker_Customized: View {
+    @State private var favoriteState = 1
+    @State private var youTuberName = "Mark"
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 16) {
+            Text("Picker").font(.largeTitle)
+            Text("With Modifiers").foregroundStyle(.gray)
+            Text("Your Favorite State:")
+            Picker("Select State", selection: $favoriteState) {
+                Text("California").tag(0)
+                Text("Utah").tag(1)
+                Text("Vermont").tag(2)
+            }
+            .pickerStyle(.wheel)
+            .padding(.horizontal)
+            .background(RoundedRectangle(cornerRadius: 20)
+                .fill(Color.blue))
+            .padding()
+            
+            Text("Who do you want to watch today?")
+            Picker("Select person", selection: $youTuberName) {
+                Text("Paul").tag("Paul")
+                Text("Chris").tag("Chris")
+                Text("Mark").tag("Mark")
+                Text("Scott").tag("Scott")
+                Text("Meng").tag("Meng")
+            }
+            .pickerStyle(.wheel)
+        }
     }
 }
 

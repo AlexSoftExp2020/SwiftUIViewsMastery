731a61fae31e02f6850aec368aa7230ce2d85632
Alex <oleksii.pidhornyi@gmail.com>
2025-04-27 14:05:12 +0200

    extend picker programmatic selection

--- SwiftUIViewsMastery/SwiftUI/ControlViews/Picker/Picker_ProgrammaticSelection.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Picker/Picker_ProgrammaticSelection.swift
@@ -8,8 +8,31 @@
 import SwiftUI
 
 struct Picker_ProgrammaticSelection: View {
+    @State private var favoriteState = 1
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            HeaderView("Picker",
+                       subtitle: "Programmatic Selection",
+                       desc: "You can programmatically change the Picker selection just by changing the bound property.")
+            
+            Picker("States", selection: $favoriteState) {
+                Text("California").tag(0)
+                Text("Colorado").tag(1)
+                Text("Montana").tag(2)
+                Text("Utah").tag(3)
+                Text("Vermont").tag(4)
+            }
+            .pickerStyle(.wheel)
+            .padding(.horizontal)
+            
+            Button("Select Vermont") {
+                withAnimation {
+                    favoriteState = 4
+                }
+            }
+        }
+        .font(.title)
     }
 }
 

774fff2b3543297a751770910bf39124d6bd807b
Alex <oleksii.pidhornyi@gmail.com>
2025-04-27 14:36:17 +0200

    add picker binding to data

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Picker/Picker_BindingToData.swift
@@ -0,0 +1,18 @@
+//
+//  Picker_BindingToData.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 27.04.2025.
+//
+
+import SwiftUI
+
+struct Picker_BindingToData: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Picker_BindingToData()
+}

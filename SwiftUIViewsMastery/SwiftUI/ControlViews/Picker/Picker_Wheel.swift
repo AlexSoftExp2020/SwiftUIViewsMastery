0438ac18673d42349224a7e134c5db4e80ad84e1
Alex <oleksii.pidhornyi@gmail.com>
2025-04-27 13:55:25 +0200

    extend picker wheel

--- SwiftUIViewsMastery/SwiftUI/ControlViews/Picker/Picker_Wheel.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Picker/Picker_Wheel.swift
@@ -8,8 +8,24 @@
 import SwiftUI
 
 struct Picker_Wheel: View {
+    @State private var yourName = "Mark"
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing:  20) {
+            HeaderView("Picker",
+                       subtitle: "Wheel Style",
+                       desc: "You can change the way a Picker looks by using the pickerStyle modifier.")
+            
+            Picker("Name", selection: $yourName) {
+                Text("Paul").tag("Paul")
+                Text("Chris").tag("Chris")
+                Text("Mark").tag("Mark")
+                Text("Scott").tag("Scott")
+                Text("Meng").tag("Meng")
+            }
+            .pickerStyle(.wheel)
+        }
+        .font(.title)
     }
 }
 

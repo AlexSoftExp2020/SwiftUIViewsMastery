68eb34bb9947bc20ca15274922105d5b51e848d9
Alex <oleksii.pidhornyi@gmail.com>
2025-04-28 10:49:15 +0200

    add picker inform

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Picker/Picker_Inform.swift
@@ -0,0 +1,18 @@
+//
+//  Picker_Inform.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 28.04.2025.
+//
+
+import SwiftUI
+
+struct Picker_Inform: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Picker_Inform()
+}

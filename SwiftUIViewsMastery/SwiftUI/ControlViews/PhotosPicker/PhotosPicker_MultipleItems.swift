ad9c2afab5f7d3171d6367b1ad14742c72e18271
Alex <oleksii.pidhornyi@gmail.com>
2025-04-28 11:29:40 +0200

    extend photos picker multiple items

--- SwiftUIViewsMastery/SwiftUI/ControlViews/PhotosPicker/PhotosPicker_MultipleItems.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/PhotosPicker/PhotosPicker_MultipleItems.swift
@@ -6,10 +6,39 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct PhotosPicker_MultipleItems: View {
+    @State private var photos: [PhotosPickerItem] = []
+    @State private var selectedImages: [Image] = []
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            PhotosPicker(selection: $photos,
+                         matching: .images) {
+                Text("Photos")
+            }
+            
+            if (photos.count > 0) {
+                Text("Selectedd Photos")
+                List {
+                    ForEach(0..<selectedImages.count, id: \.self) { index in
+                        selectedImages[index]
+                            .resizable()
+                            .scaledToFit()
+                    }
+                }
+            }
+            Spacer()
+        }
+        .font(.title)
+        .onChange(of: photos) { _, newPhotos in
+            Task {
+                for newPhoto in newPhotos {
+                    await selectedImages.append(newPhoto.convert())
+                }
+            }
+        }
     }
 }
 

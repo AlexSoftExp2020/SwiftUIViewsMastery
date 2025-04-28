668bdb97e534b7d9146c6e3062d59bf48849a003
Alex <oleksii.pidhornyi@gmail.com>
2025-04-28 13:24:13 +0200

    add scroll view intro

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/ScrollView/Scrollview_Intro.swift
@@ -0,0 +1,18 @@
+//
+//  Scrollview_Intro.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 28.04.2025.
+//
+
+import SwiftUI
+
+struct Scrollview_Intro: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Scrollview_Intro()
+}

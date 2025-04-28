c3afb914cd805a1f44829315459d8c7686488969
Alex <oleksii.pidhornyi@gmail.com>
2025-04-28 13:20:38 +0200

    extend rename button edit text

--- SwiftUIViewsMastery/SwiftUI/ControlViews/RenameButton/RenameButton_EditText.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/RenameButton/RenameButton_EditText.swift
@@ -8,8 +8,35 @@
 import SwiftUI
 
 struct RenameButton_EditText: View {
+    @State private var editText = false
+    @State private var name = "Mark"
+    @FocusState private var nameFocused: Bool
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            if editText {
+                HStack {
+                    TextField("name", text: $name)
+                        .textFieldStyle(.roundedBorder)
+                        .focused($nameFocused)
+                    Button("Done") {
+                        editText.toggle()
+                    }
+                }
+            } else {
+                HStack {
+                    Text(name)
+                    Spacer()
+                    RenameButton()
+                        .buttonStyle(.borderedProminent)
+                }
+            }
+        }
+        .renameAction {
+            editText.toggle()
+            nameFocused.toggle()
+        }
+        .padding()
     }
 }
 

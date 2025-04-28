a70a397eab59146ebfca835635c18877325d5b3a
Alex <oleksii.pidhornyi@gmail.com>
2025-04-28 11:31:33 +0200

    add progress view intro

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/ProgressView/ProgressView_Intro.swift
@@ -0,0 +1,18 @@
+//
+//  ProgressView_Intro.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 28.04.2025.
+//
+
+import SwiftUI
+
+struct ProgressView_Intro: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProgressView_Intro()
+}

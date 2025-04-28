90bc2a7571d5a26fefc4d06d56f8ba74ac60bc2d
Alex <oleksii.pidhornyi@gmail.com>
2025-04-28 11:38:17 +0200

    extend progress view intro

--- SwiftUIViewsMastery/SwiftUI/ControlViews/ProgressView/ProgressView_Intro.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/ProgressView/ProgressView_Intro.swift
@@ -9,7 +9,25 @@ import SwiftUI
 
 struct ProgressView_Intro: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HeaderView("ProgressView",
+                       subtitle: "Introduction",
+                       desc: "Use ProgressView to show that some activity is happening. If you do not pass in any values, it will just show an indeterminate spinner.",
+                       back: .blue,
+                       textColor: .white)
+            
+            ProgressView()
+            
+            DescView("You can add a label to it as well:", back: .blue, textColor: .white)
+            
+            ProgressView("Loading...")
+                .font(Font.system(.title2, design: .monospaced).weight(.bold))
+            
+            DescView("Which, as you can see, can be customized.",
+                     back: .blue,
+                     textColor: .white)
+        }
+        .font(.title)
     }
 }
 

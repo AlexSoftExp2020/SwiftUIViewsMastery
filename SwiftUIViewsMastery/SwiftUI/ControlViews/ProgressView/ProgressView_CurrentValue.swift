ee7aebd1471c50df9c33dd8199c8f95de6a72d9d
Alex <oleksii.pidhornyi@gmail.com>
2025-04-28 12:39:37 +0200

    add progress view current value

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/ProgressView/ProgressView_CurrentValue.swift
@@ -0,0 +1,18 @@
+//
+//  ProgressView_CurrentValue.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 28.04.2025.
+//
+
+import SwiftUI
+
+struct ProgressView_CurrentValue: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProgressView_CurrentValue()
+}

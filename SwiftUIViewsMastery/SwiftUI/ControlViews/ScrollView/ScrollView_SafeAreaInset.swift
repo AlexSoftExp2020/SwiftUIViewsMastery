d0d59a82c1527c6ae9b366c97e0583bc17d29b90
Alex <oleksii.pidhornyi@gmail.com>
2025-04-28 13:40:54 +0200

    extend scroll view safe area inset

--- SwiftUIViewsMastery/SwiftUI/ControlViews/ScrollView/ScrollView_SafeAreaInset.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/ScrollView/ScrollView_SafeAreaInset.swift
@@ -8,8 +8,42 @@
 import SwiftUI
 
 struct ScrollView_SafeAreaInset: View {
+    @State private var names = ["Scott",
+                                "Mark",
+                                "Chris",
+                                "Sean",
+                                "Rod",
+                                "Meng",
+                                "Natasha",
+                                "Chase",
+                                "Evans",
+                                "Paul",
+                                "Durtschi",
+                                "Max"]
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            ForEach(names, id: \.self) { name in
+                HStack {
+                    Text(name)
+                        .foregroundStyle(.primary)
+                    Image(systemName: "checkmark.seal.fill")
+                        .foregroundStyle(.green)
+                    Spacer()
+                }
+                .padding()
+                .background(Color.white.shadow(.drop(radius: 1, y: 1)),
+                            in: RoundedRectangle(cornerRadius: 8))
+            }
+            .padding(.horizontal)
+        }
+        .safeAreaInset(edge: .bottom) {
+            VStack(spacing: 20) {
+                Divider()
+                Text("12 People")
+            }
+            .background(.regularMaterial)
+        }
+        .font(.title)
     }
 }
 

e65a6bd8d445c4c6ca6eb3b1932f7e4af97525fa
Alex <oleksii.pidhornyi@gmail.com>
2025-04-28 14:00:50 +0200

    extend scroll view horizontal safe area inset

--- SwiftUIViewsMastery/SwiftUI/ControlViews/ScrollView/ScrollView_Horizontal_SafeAreaInset.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/ScrollView/ScrollView_Horizontal_SafeAreaInset.swift
@@ -8,8 +8,42 @@
 import SwiftUI
 
 struct ScrollView_Horizontal_SafeAreaInset: View {
+    var items = [Color.green,
+                 Color.blue,
+                 Color.purple,
+                 Color.pink,
+                 Color.yellow,
+                 Color.orange]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { gr in
+            VStack {
+                Spacer()
+                ScrollView(Axis.Set.horizontal, showsIndicators: true) {
+                    HStack {
+                        ForEach(items, id: \.self) { item in
+                            RoundedRectangle(cornerRadius: 15)
+                                .fill(item)
+                                .frame(width: gr.size.width - 60)
+                        }
+                    }
+                }
+                .padding(.trailing)
+                .safeAreaInset(edge: .leading) {
+                    VStack(spacing: 10) {
+                        Text("Scroll")
+                            .font(.body)
+                        Image(systemName: "arrow.left.circle")
+                    }
+                    .frame(maxHeight: .infinity)
+                    .padding(.horizontal)
+                    .background(.regularMaterial)
+                }
+                .frame(height: 200)
+                Spacer()
+            }
+        }
+        .font(.title)
     }
 }
 

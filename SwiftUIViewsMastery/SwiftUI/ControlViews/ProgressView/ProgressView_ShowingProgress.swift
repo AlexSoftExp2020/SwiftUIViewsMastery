609d629f9d760f4a2649f2beda16e2a62a6eece9
Alex <oleksii.pidhornyi@gmail.com>
2025-04-28 11:46:23 +0200

    extend progress view showing progress

--- SwiftUIViewsMastery/SwiftUI/ControlViews/ProgressView/ProgressView_ShowingProgress.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/ProgressView/ProgressView_ShowingProgress.swift
@@ -8,8 +8,27 @@
 import SwiftUI
 
 struct ProgressView_ShowingProgress: View {
+    @State private var progress = 0.75
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            HeaderView("ProgressView",
+                       subtitle: "Showing Progress",
+                       desc: "You can one-way bind data to your ProgressView to show progression.",
+                       back: .blue,
+                       textColor: .white)
+            
+            ProgressView(value: progress)
+                .padding()
+            
+            DescView("And with a label:",
+                     back: .blue,
+                     textColor: .white)
+            
+            ProgressView("Progress (\(Int(progress * 100))%)", value: progress)
+                .padding()
+        }
+        .font(.title)
     }
 }
 

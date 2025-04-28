a3b0c987d786156b275359dbd6fa9d15e74abd7a
Alex <oleksii.pidhornyi@gmail.com>
2025-04-28 11:00:07 +0200

    add photos picker intro

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/PhotosPicker/PhotosPicker_Intro.swift
@@ -0,0 +1,18 @@
+//
+//  PhotosPicker_Intro.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 28.04.2025.
+//
+
+import SwiftUI
+
+struct PhotosPicker_Intro: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PhotosPicker_Intro()
+}

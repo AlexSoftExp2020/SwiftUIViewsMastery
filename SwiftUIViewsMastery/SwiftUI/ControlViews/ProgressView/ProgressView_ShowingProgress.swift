d546d48d051ca66f55ad19cd827d2cba5aec19f3
Alex <oleksii.pidhornyi@gmail.com>
2025-04-28 11:39:47 +0200

    add progress view showing progress

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/ProgressView/ProgressView_ShowingProgress.swift
@@ -0,0 +1,18 @@
+//
+//  ProgressView_ShowingProgress.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 28.04.2025.
+//
+
+import SwiftUI
+
+struct ProgressView_ShowingProgress: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProgressView_ShowingProgress()
+}

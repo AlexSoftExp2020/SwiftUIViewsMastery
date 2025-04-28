91e3ed28d9e65e95a227639cf7252217b0e601b8
Alex <oleksii.pidhornyi@gmail.com>
2025-04-28 13:51:08 +0200

    add scroll view horizontal safe area inset

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/ScrollView/ScrollView_Horizontal_SafeAreaInset.swift
@@ -0,0 +1,18 @@
+//
+//  ScrollView_Horizontal_SafeAreaInset.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 28.04.2025.
+//
+
+import SwiftUI
+
+struct ScrollView_Horizontal_SafeAreaInset: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScrollView_Horizontal_SafeAreaInset()
+}

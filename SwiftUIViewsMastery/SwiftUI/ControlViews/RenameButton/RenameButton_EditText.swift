6e4b95fbd92142cb66941f8980ed8ea05aca4730
Alex <oleksii.pidhornyi@gmail.com>
2025-04-28 13:13:21 +0200

    add rename button edit text

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/RenameButton/RenameButton_EditText.swift
@@ -0,0 +1,18 @@
+//
+//  RenameButton_EditText.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 28.04.2025.
+//
+
+import SwiftUI
+
+struct RenameButton_EditText: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RenameButton_EditText()
+}

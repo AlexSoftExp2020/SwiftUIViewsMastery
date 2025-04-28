9978c7df837be722153c02b99987685ce34e433e
Alex <oleksii.pidhornyi@gmail.com>
2025-04-28 13:42:22 +0200

    add scroll view horizontal

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/ScrollView/Scrollview_Horizontal.swift
@@ -0,0 +1,18 @@
+//
+//  Scrollview_Horizontal.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 28.04.2025.
+//
+
+import SwiftUI
+
+struct Scrollview_Horizontal: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Scrollview_Horizontal()
+}

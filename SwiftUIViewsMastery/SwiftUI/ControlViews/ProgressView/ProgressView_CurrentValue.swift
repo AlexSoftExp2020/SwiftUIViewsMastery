9387c9836ae66e69a9aca343cd2a8567d00fdd7f
Alex <oleksii.pidhornyi@gmail.com>
2025-04-28 12:47:42 +0200

    extend progress view current value

--- SwiftUIViewsMastery/SwiftUI/ControlViews/ProgressView/ProgressView_CurrentValue.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/ProgressView/ProgressView_CurrentValue.swift
@@ -8,8 +8,36 @@
 import SwiftUI
 
 struct ProgressView_CurrentValue: View {
+    @State private var progress = 0.2
+    @State private var current = 20
+    @State private var total = 100
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 40) {
+            HeaderView("ProgressView",
+                       subtitle: "Current Value Label",
+                       desc: "You can also the currentValueLabel parameter to indicate progress values.",
+                       back: .blue,
+                       textColor: .white)
+            
+            ProgressView(value: progress) {
+                Text("Fetching Records")
+            } currentValueLabel: {
+                Text("\(current) out of \(total)")
+            }
+            .padding()
+            Text("Allows customizations:")
+            ProgressView(value: progress) {
+                Text("Fetching Records")
+            } currentValueLabel: {
+                Text("\(current) out of \(total)")
+                    .font(.title3)
+                    .italic()
+                    .foregroundStyle(.green)
+            }
+            .padding()
+        }
+        .font(.title)
     }
 }
 

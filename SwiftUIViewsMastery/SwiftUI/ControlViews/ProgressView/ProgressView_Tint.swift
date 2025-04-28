9677d841457683a8943248d327a9868149cee613
Alex <oleksii.pidhornyi@gmail.com>
2025-04-28 13:10:25 +0200

    extend progress view tint

--- SwiftUIViewsMastery/SwiftUI/ControlViews/ProgressView/ProgressView_Tint.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/ProgressView/ProgressView_Tint.swift
@@ -8,8 +8,36 @@
 import SwiftUI
 
 struct ProgressView_Tint: View {
+    @State private var progress = 0.75
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            HeaderView("ProgressView",
+                       subtitle: "Tint",
+                       desc: "Starting in iOS 15, you use the tint modifier to change the color of the progress views.")
+            ProgressView(value: progress) {
+                HStack {
+                    Spacer()
+                    Text("Progress (\(Int(progress * 100))%)")
+                        .font(.title)
+                        .foregroundStyle(.white)
+                    Spacer()
+                }
+            }
+            .tint(.mint)
+            .padding()
+            .background(RoundedRectangle(cornerRadius: 16)
+                .fill(Color.yellow)
+                .shadow(radius: 10, y: 16))
+            .padding(.horizontal)
+            
+            Text("For the circular progress view, you need to set the color through the style modifier:")
+                .padding(.horizontal)
+            
+            ProgressView()
+                .tint(.red)
+        }
+        .font(.title)
     }
 }
 

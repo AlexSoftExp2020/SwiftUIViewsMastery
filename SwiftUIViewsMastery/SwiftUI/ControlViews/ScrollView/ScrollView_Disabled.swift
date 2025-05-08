f924ee31ab0ec8c9270c72cadd2b293f7927f13b
Alex <oleksii.pidhornyi@gmail.com>
2025-05-08 21:38:35 +0200

    add scroll view disabled

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/ScrollView/ScrollView_Disabled.swift
@@ -0,0 +1,18 @@
+//
+//  ScrollView_Disabled.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 08.05.2025.
+//
+
+import SwiftUI
+
+struct ScrollView_Disabled: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScrollView_Disabled()
+}

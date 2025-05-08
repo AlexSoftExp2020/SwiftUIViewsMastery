c72ccbfd374e18e03c464364aed1e04c9e71398b
Alex <oleksii.pidhornyi@gmail.com>
2025-05-08 23:10:22 +0200

    add navigation searchable cancel

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Searchable/Navigation_Searchable_Cancel.swift
@@ -0,0 +1,18 @@
+//
+//  Navigation_Searchable_Cancel.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 08.05.2025.
+//
+
+import SwiftUI
+
+struct Navigation_Searchable_Cancel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Navigation_Searchable_Cancel()
+}

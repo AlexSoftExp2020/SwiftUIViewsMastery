6a58c61ab3e38d9a4691b6a6386511214f39fe7c
Alex <oleksii.pidhornyi@gmail.com>
2025-05-08 21:49:05 +0200

    extend scroll view disabled

--- SwiftUIViewsMastery/SwiftUI/ControlViews/ScrollView/ScrollView_Disabled.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/ScrollView/ScrollView_Disabled.swift
@@ -8,8 +8,30 @@
 import SwiftUI
 
 struct ScrollView_Disabled: View {
+    @State private var disableScroll = false
+    
+    var items = [Color.green,
+                 Color.blue,
+                 Color.purple,
+                 Color.pink,
+                 Color.yellow,
+                 Color.orange]
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView(showsIndicators: true) {
+            ForEach(items, id: \.self) { item in
+                RoundedRectangle(cornerRadius: 15)
+                    .fill(item)
+                    .frame(height: 200)
+                    .padding(.horizontal)
+            }
+        }
+        .scrollDisabled(disableScroll)
+        .safeAreaInset(edge: .bottom) {
+            Toggle("Disable Scrolling", isOn: $disableScroll)
+                .padding()
+                .background(.regularMaterial)
+        }
+        .font(.title)
     }
 }
 

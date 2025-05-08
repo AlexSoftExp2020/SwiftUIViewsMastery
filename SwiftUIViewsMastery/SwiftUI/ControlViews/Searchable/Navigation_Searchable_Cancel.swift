844ea65da80e43162a76a007d77435f683c1f6b7
Alex <oleksii.pidhornyi@gmail.com>
2025-05-08 23:16:32 +0200

    extend navigation searchable cancel

--- SwiftUIViewsMastery/SwiftUI/ControlViews/Searchable/Navigation_Searchable_Cancel.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Searchable/Navigation_Searchable_Cancel.swift
@@ -8,8 +8,39 @@
 import SwiftUI
 
 struct Navigation_Searchable_Cancel: View {
+    private let locations = ["Milton", "Milltown", "Millerville", "Milwaukee", "Millcreek", "Milargo", "Milano"]
+    @State private var searchResults:[String] = []
+    @State private var locationSearch = ""
+    @State private var destination = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack(spacing: 20.0) {
+                Text("Where do you want to go?")
+                Text(destination)
+                    .fontWeight(.bold)
+                Spacer()
+                HeaderView("",
+                           subtitle: "Searchable",
+                           desc: "Use searchable to add a text field to the navigation bar drawer.")
+            }
+            .font(.title)
+            .navigationTitle(Text("Navigation"))
+        }
+        .searchable(text: $locationSearch) {
+            ForEach(searchResults, id: \.self) { name in
+                Button(name) {
+                    destination = name
+                    locationSearch = ""
+                    hideKeyboard()
+                }
+            }
+        }
+        .onChange(of: locationSearch) { _, location in
+            searchResults = locations.filter { name in
+                name.hasPrefix(locationSearch)
+            }
+        }
     }
 }
 

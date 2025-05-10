9e6e0d749f132dd61c2ea4ab2ca725726af54b61
Alex <oleksii.pidhornyi@gmail.com>
2025-05-10 23:48:17 +0200

    add searchable subview

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Searchable/SearchableSubview.swift
@@ -0,0 +1,18 @@
+//
+//  SearchableSubview.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 10.05.2025.
+//
+
+import SwiftUI
+
+struct SearchableSubview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchableSubview()
+}

048f8615582ca05a5c02cbd9b69e130f3e2d2af2
Alex <oleksii.pidhornyi@gmail.com>
2025-05-10 19:26:59 +0200

    add searchable dismiss search

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Searchable/Searchable_DismissSearch.swift
@@ -0,0 +1,18 @@
+//
+//  Searchable_DismissSearch.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 10.05.2025.
+//
+
+import SwiftUI
+
+struct Searchable_DismissSearch: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Searchable_DismissSearch()
+}

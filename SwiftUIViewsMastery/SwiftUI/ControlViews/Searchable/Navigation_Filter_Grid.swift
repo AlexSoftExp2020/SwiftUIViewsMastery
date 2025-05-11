33ec424f24eb19e883ba65de51053bf7b2ad54c7
Alex <oleksii.pidhornyi@gmail.com>
2025-05-11 22:14:05 +0200

    add navigation filter grid

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Searchable/Navigation_Filter_Grid.swift
@@ -0,0 +1,18 @@
+//
+//  Navigation_Filter_Grid.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 11.05.2025.
+//
+
+import SwiftUI
+
+struct Navigation_Filter_Grid: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Navigation_Filter_Grid()
+}

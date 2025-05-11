8c48ed15ae605536b0e521c869973de12225304e
Alex <oleksii.pidhornyi@gmail.com>
2025-05-11 22:12:54 +0200

    extend navigation searchable filter

--- SwiftUIViewsMastery/SwiftUI/ControlViews/Searchable/Navigation_Searchable_Filter.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Searchable/Navigation_Searchable_Filter.swift
@@ -8,8 +8,38 @@
 import SwiftUI
 
 struct Navigation_Searchable_Filter: View {
+    private let names = ["Chris",
+                         "Paul",
+                         "Scott",
+                         "Donny",
+                         "Antoine",
+                         "Denise",
+                         "Sean"]
+    @State private var filteredNames: [String] = []
+    @State private var nameSearch = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack(spacing: 20.0) {
+                List(nameSearch.isEmpty ? names : filteredNames, id: \.self) { name in
+                    Text(name)
+                        .font(.title)
+                }
+                HeaderView("",
+                           subtitle: "Searchable - Filter",
+                           desc: "Use searchable to filter an existing list.")
+                .font(.title)
+            }
+            .navigationTitle(Text("Navigation"))
+        }
+        .searchable(text: $nameSearch,
+                    placement: .navigationBarDrawer(displayMode: .always),
+                    prompt: Text("Filter"))
+        .onChange(of: nameSearch) { _, name in
+            filteredNames = names.filter { name in
+                name.hasPrefix(nameSearch)
+            }
+        }
     }
 }
 

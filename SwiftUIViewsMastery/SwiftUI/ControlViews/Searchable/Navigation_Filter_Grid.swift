773af379d2086aa6ba45cd85cdb58cb429faee29
Alex <oleksii.pidhornyi@gmail.com>
2025-05-11 22:45:53 +0200

    extend navigation filter grid

--- SwiftUIViewsMastery/SwiftUI/ControlViews/Searchable/Navigation_Filter_Grid.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Searchable/Navigation_Filter_Grid.swift
@@ -8,8 +8,21 @@
 import SwiftUI
 
 struct Navigation_Filter_Grid: View {
+    @State private var devs: [Developer] = []
+    @State private var searchText = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                ScrollView {
+                    LazyVGrid(columns: [GridItem(.flexible()),
+                                        GridItem(.flexible()),
+                                        GridItem(.flexible())]) {
+                        //ForEach(..., content: <#T##(Int) -> Content#>)
+                    }
+                }
+            }
+        }
     }
 }
 

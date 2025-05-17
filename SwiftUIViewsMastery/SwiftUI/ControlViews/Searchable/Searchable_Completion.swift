f5542959c2731673138fe9b1ac24800cd58ea286
Alex <oleksii.pidhornyi@gmail.com>
2025-05-17 21:28:13 +0200

    add searchable completion

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Searchable/Searchable_Completion.swift
@@ -0,0 +1,18 @@
+//
+//  Searchable_Completion.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 17.05.2025.
+//
+
+import SwiftUI
+
+struct Searchable_Completion: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Searchable_Completion()
+}

0cef46781ae240f03902ff2d81f4ee02811cdc31
Alex <oleksii.pidhornyi@gmail.com>
2025-05-20 23:45:49 +0200

    extend secure field customizations

--- SwiftUIViewsMastery/SwiftUI/ControlViews/SecureField/SecureField_Customizations.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/SecureField/SecureField_Customizations.swift
@@ -8,8 +8,33 @@
 import SwiftUI
 
 struct SecureField_Customizations: View {
+    @State private var userName = ""
+    @State private var password = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Use a ZStack to put a RoundedRextangle behind a SecureField witha plain textfieldStyle.")
+        
+        ZStack {
+            RoundedRectangle(cornerRadius: 8)
+                .foregroundStyle(.purple)
+            TextField("user name", text: $userName)
+                .foregroundStyle(Color.white)
+                .padding(.horizontal)
+        }
+        .frame(height: 40)
+        .padding(.horizontal)
+        
+        Text("Or overlay the SecureField on top of another view or shape.")
+        
+        RoundedRectangle(cornerRadius: 8)
+            .foregroundStyle(.purple)
+            .overlay {
+                SecureField("password", text: $password)
+                    .foregroundStyle(Color.white)
+                    .padding(.horizontal)
+            }
+            .frame(height: 40)
+            .padding(.horizontal)
     }
 }
 

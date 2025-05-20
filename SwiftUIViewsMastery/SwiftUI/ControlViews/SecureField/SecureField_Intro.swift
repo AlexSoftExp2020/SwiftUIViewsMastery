ac7f26210f7374dd2005bc53ea1e45635cc42857
Alex <oleksii.pidhornyi@gmail.com>
2025-05-20 23:22:05 +0200

    add secure field intro

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/SecureField/SecureField_Intro.swift
@@ -0,0 +1,18 @@
+//
+//  SecureField_Intro.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 20.05.2025.
+//
+
+import SwiftUI
+
+struct SecureField_Intro: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SecureField_Intro()
+}

a2de5d606e278e5ead5d0a195cbbe2634c618d67
Alex <oleksii.pidhornyi@gmail.com>
2025-05-20 23:59:55 +0200

    extend secure field customization layers

--- SwiftUIViewsMastery/SwiftUI/ControlViews/SecureField/SecureField_CustomizationLayers.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/SecureField/SecureField_CustomizationLayers.swift
@@ -8,8 +8,33 @@
 import SwiftUI
 
 struct SecureField_CustomizationLayers: View {
+    @State private var userName = ""
+    @State private var password = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            Text("SeureField")
+                .font(.largeTitle)
+            Text("Customization Layers")
+                .font(.title)
+                .foregroundStyle(.gray)
+            Text("You can also add a background to the SecureField. It's all the same idea: adjust the layers.")
+            
+            SecureField("password", text: $password)
+                .foregroundStyle(Color.white)
+                .frame(height: 40)
+                .padding(.horizontal)
+                .background {
+                    Capsule()
+                        .foregroundStyle(.purple)
+                }
+                .padding(.horizontal)
+            
+            Image("SecureFieldLayers")
+            Text("The highlighted layer in that image is the actual text field layer of the view.")
+                .font(.title)
+                .padding(.horizontal)
+        }
     }
 }
 

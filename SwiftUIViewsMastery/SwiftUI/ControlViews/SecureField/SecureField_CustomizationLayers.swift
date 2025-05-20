44e799a8b429829dbf845093b8443f3d62348b38
Alex <oleksii.pidhornyi@gmail.com>
2025-05-20 23:47:29 +0200

    add secure field customization layers

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/SecureField/SecureField_CustomizationLayers.swift
@@ -0,0 +1,18 @@
+//
+//  SecureField_CustomizationLayers.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 20.05.2025.
+//
+
+import SwiftUI
+
+struct SecureField_CustomizationLayers: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SecureField_CustomizationLayers()
+}

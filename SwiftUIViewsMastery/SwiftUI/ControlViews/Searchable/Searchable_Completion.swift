590f39e2a341acbf77ebb61809bc1becb919a014
Alex <oleksii.pidhornyi@gmail.com>
2025-05-20 23:17:48 +0200

    extend searchable completion

--- SwiftUIViewsMastery/SwiftUI/ControlViews/Searchable/Searchable_Completion.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Searchable/Searchable_Completion.swift
@@ -8,8 +8,31 @@
 import SwiftUI
 
 struct Searchable_Completion: View {
+    @State private var locationSearch = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack(spacing: 20.0) {}
+            .searchable(text: $locationSearch) {
+                Group {
+                    HStack {
+                        Image(systemName: "house.circle")
+                            .font(.largeTitle)
+                        Text("Home")
+                    }
+                    .searchCompletion("123 Main Street, Salt Lake Cite, Utah")
+                    HStack {
+                        Image(systemName: "building.2.crop.circle")
+                            .font(.largeTitle)
+                        Text("Work")
+                    }
+                    .searchCompletion("456 State Street, Salt Lake City, Utah")
+                }
+                .font(.title2)
+                .tint(.primary)
+                .padding(.vertical)
+            }
+        }
     }
 }
 

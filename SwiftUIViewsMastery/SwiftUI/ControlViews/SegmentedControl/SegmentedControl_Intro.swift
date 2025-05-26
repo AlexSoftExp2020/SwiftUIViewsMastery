a88f82369fcb8b284ef854a0ac71d73dc46c4cb7
Alex <oleksii.pidhornyi@gmail.com>
2025-05-26 18:44:49 +0200

    extend segmented control intro

--- SwiftUIViewsMastery/SwiftUI/ControlViews/SegmentedControl/SegmentedControl_Intro.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/SegmentedControl/SegmentedControl_Intro.swift
@@ -8,8 +8,29 @@
 import SwiftUI
 
 struct SegmentedControl_Intro: View {
+    @State private var dayNight = "day"
+    @State private var tab = 1
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            Text("Segmented Control with an @State variable that will control whick segment is selected. The state variable will match each segment's tag value.")
+            
+            Picker("", selection: $dayNight) {
+                Text("Day").tag("day")
+                Text("Night").tag("night")
+            }
+            .pickerStyle(.segmented)
+            .padding()
+            
+            Text("With Images:")
+            
+            Picker("", selection: $tab) {
+                Image(systemName: "sun.min").tag(0)
+                Image(systemName: "moon").tag(1)
+            }
+            .pickerStyle(.segmented)
+            .padding()
+        }
     }
 }
 

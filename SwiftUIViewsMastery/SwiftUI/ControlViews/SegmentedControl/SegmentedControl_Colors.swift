f5c62cef5e497bac8ee535c2bf478c45505e0b80
Alex <oleksii.pidhornyi@gmail.com>
2025-05-26 19:17:22 +0200

    extend segmented control colors

--- SwiftUIViewsMastery/SwiftUI/ControlViews/SegmentedControl/SegmentedControl_Colors.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/SegmentedControl/SegmentedControl_Colors.swift
@@ -8,8 +8,35 @@
 import SwiftUI
 
 struct SegmentedControl_Colors: View {
+    @State private var selection = 2
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            Text("Segmented Control (Picker)").font(.largeTitle)
+            Text("Colors")
+                .font(.title).foregroundStyle(.gray)
+            Text("You can change the color of segmented controls by using the background modifier.")
+            Text("When you add a color, notice the corners:")
+            Picker("", selection: $selection) {
+                Text("One").tag(1)
+                Text("Two").tag(2)
+                Text("Three").tag(3)
+            }
+            .pickerStyle(.segmented)
+            .background(Color.pink)
+            .padding(.horizontal)
+            
+            Text("Adding a corner radius should handle it:")
+            Picker("", selection: $selection) {
+                Text("One").tag(1)
+                Text("Two").tag(2)
+                Text("Three").tag(3)
+            }
+            .pickerStyle(.segmented)
+            .background(Color.pink)
+            .cornerRadius(8)
+            .padding(.horizontal)
+        }
     }
 }
 

49bf460eca434826d741257e9ea51e1c3d8e1f53
Alex <oleksii.pidhornyi@gmail.com>
2025-05-26 19:26:30 +0200

    add share link intro

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/ShareLink/ShareLink_Intro.swift
@@ -0,0 +1,18 @@
+//
+//  ShareLink_Intro.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 26.05.2025.
+//
+
+import SwiftUI
+
+struct ShareLink_Intro: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShareLink_Intro()
+}

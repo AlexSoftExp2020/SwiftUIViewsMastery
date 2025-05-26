86221861f08e24f7fac96dd970573996c0d98ccb
Alex <oleksii.pidhornyi@gmail.com>
2025-05-26 19:50:13 +0200

    add share link customize

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/ShareLink/ShareLink_Customize.swift
@@ -0,0 +1,18 @@
+//
+//  ShareLink_Customize.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 26.05.2025.
+//
+
+import SwiftUI
+
+struct ShareLink_Customize: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShareLink_Customize()
+}

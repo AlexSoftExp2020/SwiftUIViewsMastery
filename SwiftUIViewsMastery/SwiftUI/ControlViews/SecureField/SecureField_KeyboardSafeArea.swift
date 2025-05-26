c4a23949214025020ef3dc15782a6afc804d4d5b
Alex <oleksii.pidhornyi@gmail.com>
2025-05-26 18:31:40 +0200

    extend secure field keyboard safe area

--- SwiftUIViewsMastery/SwiftUI/ControlViews/SecureField/SecureField_KeyboardSafeArea.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/SecureField/SecureField_KeyboardSafeArea.swift
@@ -8,8 +8,30 @@
 import SwiftUI
 
 struct SecureField_KeyboardSafeArea: View {
+    @State private var userName = ""
+    @State private var password = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            Spacer()
+            Image("Logo")
+            Spacer()
+            
+            HeaderView("SecureField",
+                       subtitle: "Keyboard Safe Area",
+                       desc: "SecureFields will automatic move into view when the keyboard appears. The keyboard adjusts the bottom safe area so it will not cover views.",
+                       back: .purple,
+                       textColor: .white)
+            
+            TextField("user name", text: $userName)
+                .textFieldStyle(.roundedBorder)
+                .padding(.horizontal)
+            
+            SecureField("password", text: $password)
+                .textFieldStyle(.roundedBorder)
+                .padding(.horizontal)
+        }
+        .font(.title)
     }
 }
 

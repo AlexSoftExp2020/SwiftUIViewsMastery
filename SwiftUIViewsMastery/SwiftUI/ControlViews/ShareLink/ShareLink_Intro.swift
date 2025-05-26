080b7268ccd88e067eecf32f4c60b9c0a2fc8ae4
Alex <oleksii.pidhornyi@gmail.com>
2025-05-26 19:43:36 +0200

    extend share link intro

--- SwiftUIViewsMastery/SwiftUI/ControlViews/ShareLink/ShareLink_Intro.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/ShareLink/ShareLink_Intro.swift
@@ -9,7 +9,37 @@ import SwiftUI
 
 struct ShareLink_Intro: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            Section {
+                ShareLink(item: "Hello and welcome!")
+                ShareLink("Share Text", item: "Hello and welcome!")
+                ShareLink(item: "Hello and welcome") {
+                    Label("Share Text", systemImage: "square.and.arrow.up.circle")
+                }
+                ShareLink("Share Text",
+                          item: "Hello and welcome",
+                          subject: Text("Subject"),
+                          message: Text("This is the message"),
+                          preview: SharePreview(Text("Share message"),
+                                                image: Image(systemName: "doc.richtext"),
+                                                icon: Image(systemName: "doc.richtext")))
+            }
+            Section("URL") {
+                ShareLink("Share URL", item: URL(string: "bigmountainstudio.com")!)
+            }
+            Section("Collections") {
+                ShareLink("Share Collections", items: ["Milk", "Eggs", "Bread"])
+            }
+            Section("Photo") {
+                ShareLink(item: Image(systemName: "globe.americas.fill"),
+                          preview: SharePreview("The Globe",
+                                                image: Image(systemName: "globe.americas.fill"))) {
+                    Label("Share Photo", systemImage: "globe.americas.fill")
+                }
+            }
+        }
+        .headerProminence(.increased)
+        .font(.title)
     }
 }
 

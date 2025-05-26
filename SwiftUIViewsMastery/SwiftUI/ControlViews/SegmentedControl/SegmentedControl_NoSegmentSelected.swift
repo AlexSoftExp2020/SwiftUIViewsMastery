12b963b4fc28d9c003de36e19f3cb4328dd2f3c2
Alex <oleksii.pidhornyi@gmail.com>
2025-05-26 18:57:26 +0200

    extend segmented control no segment  selected

--- SwiftUIViewsMastery/SwiftUI/ControlViews/SegmentedControl/SegmentedControl_NoSegmentSelected.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/SegmentedControl/SegmentedControl_NoSegmentSelected.swift
@@ -8,8 +8,32 @@
 import SwiftUI
 
 struct SegmentedControl_NoSegmentSelected: View {
+    @State private var selection = 0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            Text("Segmented Control (Picker)").font(.largeTitle)
+            Text("No segment Selected")
+                .font(.title)
+                .foregroundStyle(.gray)
+            Text("This segmented control will have nothing selected because the default state variable does not match any of the segment tag values.")
+            Text("How many meals do you eat?")
+                .foregroundStyle(.gray)
+                .font(.title)
+            
+            Picker("", selection: $selection) {
+                Text("One").tag(1)
+                Text("Two").tag(2)
+                Text("Three").tag(3)
+                Text("More").tag(4)
+            }
+            .pickerStyle(.segmented)
+            .background(RoundedRectangle(cornerRadius: 8)
+                .stroke(Color.red, lineWidth: selection == 0 ? 1 : 0))
+            .padding()
+            
+            Text("The red outline will go away once a selection is made.")
+        }
     }
 }
 

60813e234e0632120256fc784d27994abfb5dd94
Alex <oleksii.pidhornyi@gmail.com>
2025-05-26 18:33:31 +0200

    add segmented control intro

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/SegmentedControl/SegmentedControl_Intro.swift
@@ -0,0 +1,18 @@
+//
+//  SegmentedControl_Intro.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 26.05.2025.
+//
+
+import SwiftUI
+
+struct SegmentedControl_Intro: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SegmentedControl_Intro()
+}

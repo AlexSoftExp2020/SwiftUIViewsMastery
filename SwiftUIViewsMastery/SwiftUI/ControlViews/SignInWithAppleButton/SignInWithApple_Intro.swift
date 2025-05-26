cb0be7e57d5aa753803365e8079249742645f7fc
Alex <oleksii.pidhornyi@gmail.com>
2025-05-26 20:07:48 +0200

    add sign in with apple intro

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/SignInWithAppleButton/SignInWithApple_Intro.swift
@@ -0,0 +1,19 @@
+//
+//  SignInWithApple_Intro.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii on 26.05.2025.
+//
+
+import SwiftUI
+import AuthenticationServices
+
+struct SignInWithApple_Intro: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignInWithApple_Intro()
+}

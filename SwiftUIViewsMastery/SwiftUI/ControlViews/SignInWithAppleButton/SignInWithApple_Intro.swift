0d40a3686aa9b888b3a1cbf83facefbc46c88ae3
AlexLuxoftPoland <oleksii.pidhornyi@dxc.com>
2025-10-08 12:07:04 +0200

    extend sign in with apple intro

--- SwiftUIViewsMastery/SwiftUI/ControlViews/SignInWithAppleButton/SignInWithApple_Intro.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/SignInWithAppleButton/SignInWithApple_Intro.swift
@@ -10,7 +10,27 @@ import AuthenticationServices
 
 struct SignInWithApple_Intro: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 15) {
+            HeaderView("SignInWithAppleButton",
+                       subtitle: "Introduction",
+                       desc: "This view allows you to display Apple's sign in, continue and sign up buttons.")
+            VStack {
+                Text("Sign In")
+                SignInWithAppleButton(.signIn) { request in } onCompletion: { result in }
+                    .frame(height: 50)
+
+                Text("Continue")
+                SignInWithAppleButton(.continue) { request in } onCompletion: { result in }
+                    .frame(height: 50)
+                
+                Text("Sign Up")
+                SignInWithAppleButton(.signUp) { request in } onCompletion: { result in }
+                    .frame(height: 50)
+                    .shadow(radius: 8, y: 12)
+            }
+            .padding(.horizontal)
+        }
+        .font(.title)
     }
 }
 

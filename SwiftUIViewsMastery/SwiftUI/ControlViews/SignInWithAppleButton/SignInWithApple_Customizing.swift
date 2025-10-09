bee3e427345a25f4fafd6e0aaa4a375415066dbb
AlexLuxoftPoland <oleksii.pidhornyi@dxc.com>
2025-10-09 15:11:33 +0200

    add sign in with apple customizing

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/SignInWithAppleButton/SignInWithApple_Customizing.swift
@@ -0,0 +1,18 @@
+//
+//  SignInWithApple_Customizing.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii Pidhornyi on 09/10/2025.
+//
+
+import SwiftUI
+
+struct SignInWithApple_Customizing: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignInWithApple_Customizing()
+}

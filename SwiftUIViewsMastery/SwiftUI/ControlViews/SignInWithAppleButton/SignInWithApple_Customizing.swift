e06f5d3724cbdbde498edfd06ef2e8e4d8ef09a0
AlexLuxoftPoland <oleksii.pidhornyi@dxc.com>
2026-04-13 20:30:58 +0200

    extend sign in with apple customizing

--- SwiftUIViewsMastery/SwiftUI/ControlViews/SignInWithAppleButton/SignInWithApple_Customizing.swift
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/SignInWithAppleButton/SignInWithApple_Customizing.swift
@@ -6,10 +6,37 @@
 //
 
 import SwiftUI
+import _AuthenticationServices_SwiftUI
 
 struct SignInWithApple_Customizing: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color("ColorBackground")
+                .ignoresSafeArea()
+            VStack {
+                Text("black")
+                SignInWithAppleButton(.signIn,
+                                      onRequest: { request in },
+                                      onCompletion: { result in })
+                .signInWithAppleButtonStyle(.black)
+                .frame(height: 50)
+                Text("whiteOutline")
+                SignInWithAppleButton(.continue,
+                                      onRequest: { request in },
+                                      onCompletion: { result in })
+                .signInWithAppleButtonStyle(.whiteOutline)
+                .frame(height: 50)
+                Text("white")
+                SignInWithAppleButton(.signUp,
+                                      onRequest: { request in },
+                                      onCompletion: { result in })
+                .signInWithAppleButtonStyle(.white)
+                .frame(height: 50)
+                .shadow(radius: 8, y: 12)
+            }
+            .padding(.horizontal)
+        }
+        .font(.title)
     }
 }
 

4bb63379f4fd38c4c4a6607b02663f5e9fc47054
AlexLuxoftPoland <oleksii.pidhornyi@dxc.com>
2026-05-21 16:36:47 +0200

    add stepper introduction

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/Stepper/Stepper_Intro.swift
@@ -0,0 +1,46 @@
+//
+//  Stepper_Intro.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii Pidhornyi on 21/05/2026.
+//
+
+import SwiftUI
+
+struct Stepper_Intro: View {
+    @State private var stepperValue =  1
+    @State private var values = [0,1]
+
+    var body: some View {
+        VStack(spacing: 20) {
+            Text("Stepper")
+                .font(.largeTitle)
+            Text("Introduction")
+                .font(.title).foregroundStyle(.gray)
+            Text("The Stepper can be found to a variable like this:")
+            Stepper(value: $stepperValue) {
+                Text("Bound Stepper: \(stepperValue)")
+            }.padding(.horizontal)
+            Divider()
+            Image(systemName: "bolt.fill")
+                .font(.title).foregroundStyle(.yellow)
+            Text("Or you can run code on the increment and decrement events:")
+                .frame(maxWidth: .infinity).padding()
+                .background(Color.blue).foregroundStyle(Color.white)
+                .font(.title)
+            Stepper(onIncrement: {self.values.append(self.values.count)},
+                    onDecrement: {self.values.removeLast()}) {
+                Text("onIcrement and onDecrement")
+            }.padding(.horizontal)
+            HStack {
+                ForEach(values, id: \.self) { value in
+Image(systemName: "\(value).circle.fill")
+                }
+            }.font(.title).foregroundStyle(.green)
+        }
+    }
+}
+
+#Preview {
+    Stepper_Intro()
+}

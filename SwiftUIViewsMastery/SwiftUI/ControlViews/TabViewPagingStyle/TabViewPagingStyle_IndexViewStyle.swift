bece1af9c264444c99b0c0e1886c2439ff062b1d
AlexLuxoftPoland <oleksii.pidhornyi@dxc.com>
2026-07-24 21:55:34 +0200

    add tab view paging style index view style

---
+++ SwiftUIViewsMastery/SwiftUI/ControlViews/TabViewPagingStyle/TabViewPagingStyle_IndexViewStyle.swift
@@ -0,0 +1,36 @@
+//
+//  TabViewPagingStyle_IndexViewStyle.swift
+//  SwiftUIViewsMastery
+//
+//  Created by Oleksii Pidhornyi on 24/07/2026.
+//
+
+import SwiftUI
+
+struct TabViewPagingStyle_IndexViewStyle: View {
+    var body: some View {
+        TabView {
+            VStack(spacing: 20) {
+                HeaderView("TabView", subtitle: "Index View Style", desc: "You couldn't see the index dots on the bottom of the screen because it was white-on-white.")
+                Text("PAGE 1")
+                Spacer()
+                DescView("To make the index dots show up better, use the indexViewStyle modifier.")
+                Image(systemName: "arrow.down.circle")
+                    .padding(.bottom, 45)
+            }
+            VStack {
+                Text("PAGE 2")
+            }
+            VStack {
+                Text("PAGE 3")
+            }
+        }
+        .font(.title)
+        .tabViewStyle(.page)
+        .indexViewStyle(.page(backgroundDisplayMode: .always)) // always show dots
+    }
+}
+
+#Preview {
+    TabViewPagingStyle_IndexViewStyle()
+}
